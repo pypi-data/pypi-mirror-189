0ec47fb3c07850c88c8bdb38405613e8208f55c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:43 +0000

    Add package grimoirelab-toolkit/0.3.3rc2/grimoirelab_toolkit-0.3.3rc2.tar.gz

---
+++ packages/grimoirelab-toolkit/grimoirelab_toolkit-0.3.3rc2.tar.gz/grimoirelab_toolkit-0.3.3rc2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['grimoirelab_toolkit', 'tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil>=2.8.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'grimoirelab-toolkit',
+    'version': '0.3.3rc2',
+    'description': 'Toolkit of common functions used across GrimoireLab',
+    'long_description': '# GrimoireLab Toolkit [![Build Status](https://github.com/chaoss/grimoirelab-toolkit/workflows/tests/badge.svg)](https://github.com/chaoss/grimoirelab-toolkit/actions?query=workflow:tests+branch:master+event:push) [![Coverage Status](https://img.shields.io/coveralls/chaoss/grimoirelab-toolkit.svg)](https://coveralls.io/r/chaoss/grimoirelab-toolkit?branch=master)\n\nToolkit of common functions used across GrimoireLab projects.\n\nThis package provides a library composed by functions widely used in other\nGrimoireLab projects. These function deal with date handling, introspection,\nURIs/URLs, among other topics.\n\n## Requirements\n\n * Python >= 3.7\n\nYou will also need some other libraries for running the tool, you can find the\nwhole list of dependencies in [pyproject.toml](pyproject.toml) file.\n\n## Installation\n\nThere are several ways to install GrimoireLab Toolkit on your system: packages or source \ncode using Poetry or pip.\n\n### PyPI\n\nGrimoireLab Toolkit can be installed using pip, a tool for installing Python packages. \nTo do it, run the next command:\n```\n$ pip install grimoirelab-toolkit\n```\n\n### Source code\n\nTo install from the source code you will need to clone the repository first:\n```\n$ git clone https://github.com/chaoss/grimoirelab-toolkit\n$ cd grimoirelab-toolkit\n```\n\nThen use pip or Poetry to install the package along with its dependencies.\n\n#### Pip\nTo install the package from local directory run the following command:\n```\n$ pip install .\n```\nIn case you are a developer, you should install GrimoireLab Toolkit in editable mode:\n```\n$ pip install -e .\n```\n\n#### Poetry\nWe use [poetry](https://python-poetry.org/) for dependency management and \npackaging. You can install it following its [documentation](https://python-poetry.org/docs/#installation).\nOnce you have installed it, you can install GrimoireLab Toolkit and the dependencies in \na project isolated environment using:\n```\n$ poetry install\n```\nTo spaw a new shell within the virtual environment use:\n```\n$ poetry shell\n```\n\n## License\n\nLicensed under GNU General Public License (GPL), version 3 or later.\n',
+    'author': 'GrimoireLab Developers',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://chaoss.github.io/grimoirelab/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
