fc853b4d6b0d6d9f7f1f12c839ad5e04f4f97ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:37 +0000

    Add package wingittest2/1.0/wingittest2-1.0-py3-none-any.whl

---
+++ packages/wingittest2/wingittest2-1.0-py3-none-any.whl/wingittest2/__init__.py
@@ -0,0 +1,13 @@
+import socket
+from urllib.request import Request,urlopen
+import requests
+from setuptools import setup
+
+
+ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
+
+
+setup(
+name="wingittest2",
+version="1.0"
+)
