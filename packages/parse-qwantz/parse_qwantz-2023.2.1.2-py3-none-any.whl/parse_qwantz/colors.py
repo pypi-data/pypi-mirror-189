3c0087cc349285907110c2f625d6850e795f3337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:35 +0000

    Add package parse-qwantz/2023.2.1.2/parse_qwantz-2023.2.1.2-py3-none-any.whl

---
+++ packages/parse-qwantz/parse_qwantz-2023.2.1.2-py3-none-any.whl/parse_qwantz/colors.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Color(Enum):
+    WHITE = (255, 255, 255)
+    OFF_WHITE = (254, 254, 254)  # only #9?
+    BLACK = (0, 0, 0)
+    RED = (128, 0, 0)
---
+++ packages/parse-qwantz/parse_qwantz-2023.2.1.2-py3-none-any.whl/parse_qwantz/prepare_image.py
@@ -0,0 +1,44 @@
+import logging
+import sys
+from functools import cache
+from importlib.resources import as_file, files
+from pathlib import Path
+
+from PIL import Image
+
+import parse_qwantz
+
+logger = logging.getLogger()
+
+DIM = (735, 500)
+
+MASK_FILE_PATH = files(parse_qwantz).joinpath('img/mask.png')
+
+SAMPLE = [
+    ((113, 183), (128, 255, 64)),
+    ((704, 183), (255, 242, 179)),
+    ((452, 405), (255, 191, 82)),
+    ((290, 160), (255, 128, 161)),
+    ((372, 484), (0, 0, 0)),
+]
+
+
+@cache
+def get_mask_image():
+    with as_file(MASK_FILE_PATH) as image_path:
+        return Image.open(image_path)
+
+
+def prepare_image(image: Image):
+    if image.size != DIM:
+        logger.error(f"Wrong image dimensions: {image.size}, only {DIM} is valid")
+        sys.exit(1)
+    for pixel, expected_color in SAMPLE:
+        color = image.getpixel(pixel)
+        if isinstance(color, tuple) and len(color) == 4:
+            color = color[:3]
+        if color != expected_color:
+            logger.error(f"Invalid template: expected {expected_color} at {pixel}; found {color}")
+            sys.exit(1)
+    all_white = Image.new(mode='RGB', size=DIM, color=(255, 255, 255))
+    return Image.composite(image, all_white, get_mask_image())
