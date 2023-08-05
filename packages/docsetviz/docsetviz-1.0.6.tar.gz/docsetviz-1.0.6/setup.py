b5f386b446a3561d83604e98f5e3b10f9a431396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:51 +0000

    Add package docsetviz/1.0.6/docsetviz-1.0.6.tar.gz

---
+++ packages/docsetviz/docsetviz-1.0.6.tar.gz/docsetviz-1.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='docsetviz',
+    version='1.0.6',
+    description='Docset Visualization Tool',
+    author='yanaenen',
+    install_requires=[
+        'docset',
+        'numpy',
+        'opencv-python',
+        'paramiko',
+        'PyQt5',
+    ],
+    package_dir={'docsetviz': '.'},
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'docsetviz = docsetviz.main:main'
+        ]
+    },
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    zip_safe=True,
+)
