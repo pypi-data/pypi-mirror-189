91192b22bfeb7e7501cfe5368aa5a77c5fc0c44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:07 +0000

    Add package apysc/2.5.102/apysc-2.5.102.tar.gz

---
+++ packages/apysc/apysc-2.5.102.tar.gz/apysc-2.5.102/apysc/_geom/path_x_mixin.py
@@ -0,0 +1,123 @@
+"""Interface class implementation for the x path data.
+"""
+
+from typing import Dict
+
+from typing_extensions import final
+
+from apysc._html.debug_mode import add_debug_info_setting
+from apysc._type.attr_linking_mixin import AttrLinkingMixIn
+from apysc._type.int import Int
+from apysc._type.revert_mixin import RevertMixIn
+from apysc._type.variable_name_suffix_attr_or_var_mixin import (
+    VariableNameSuffixAttrOrVarMixIn,
+)
+from apysc._type.variable_name_suffix_mixin import VariableNameSuffixMixIn
+from apysc._validation import arg_validation_decos
+
+
+class PathXMixIn(
+    VariableNameSuffixAttrOrVarMixIn,
+    RevertMixIn,
+    AttrLinkingMixIn,
+    VariableNameSuffixMixIn,
+):
+
+    _x: Int
+
+    @final
+    def _initialize_x_if_not_initialized(self) -> None:
+        """
+        Initialize the _x attribute if this instance does not
+        initialize it yet.
+        """
+        if hasattr(self, "_x"):
+            return
+        suffix: str = self._get_attr_or_variable_name_suffix(value_identifier="x")
+        self._x = Int(
+            0,
+            variable_name_suffix=suffix,
+            skip_init_substitution_expression_appending=True,
+        )
+
+        self._append_x_linking_setting()
+
+    @final
+    @add_debug_info_setting(module_name=__name__)
+    def _append_x_linking_setting(self) -> None:
+        """
+        Append an x attribute linking settings.
+        """
+        self._append_applying_new_attr_val_exp(new_attr=self._x, attr_name="x")
+        self._append_attr_to_linking_stack(attr=self._x, attr_name="x")
+
+    @property
+    @add_debug_info_setting(module_name=__name__)
+    def x(self) -> Int:
+        """
+        Get an x-coordinate of the destination point.
+
+        Returns
+        -------
+        x : Int
+            An x-coordinate of the destination point.
+
+        Examples
+        --------
+        >>> import apysc as ap
+        >>> line_to: ap.PathLineTo = ap.PathLineTo(x=50, y=50)
+        >>> line_to.x = ap.Int(100)
+        >>> line_to.x
+        Int(100)
+        """
+        self._initialize_x_if_not_initialized()
+        return self._x._copy()
+
+    @x.setter
+    @arg_validation_decos.is_apysc_num(arg_position_index=1)
+    @add_debug_info_setting(module_name=__name__)
+    def x(self, value: Int) -> None:
+        """
+        Set an x-coordinate of the destination point.
+
+        Parameters
+        ----------
+        value : Int
+            X-coordinate of the destination point.
+        """
+        self._initialize_x_if_not_initialized()
+        self._x.value = value
+
+        self._append_x_linking_setting()
+
+    _x_snapshots: Dict[str, int]
+
+    def _make_snapshot(self, *, snapshot_name: str) -> None:
+        """
+        Make a value snapshot.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        self._initialize_x_if_not_initialized()
+        self._set_single_snapshot_val_to_dict(
+            dict_name="_x_snapshots",
+            value=int(self._x._value),
+            snapshot_name=snapshot_name,
+        )
+
+    def _revert(self, *, snapshot_name: str) -> None:
+        """
+        Revert a value if a snapshot exists.
+
+        Parameters
+        ----------
+        snapshot_name : str
+            Target snapshot name.
+        """
+        if not self._snapshot_exists(snapshot_name=snapshot_name):
+            return
+        self._initialize_x_if_not_initialized()
+        self._x._value = self._x_snapshots[snapshot_name]
