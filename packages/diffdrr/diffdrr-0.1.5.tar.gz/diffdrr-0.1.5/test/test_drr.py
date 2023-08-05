15b355f66eeae65762c1c27a6df49b99cab71609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package diffdrr/0.1.5/diffdrr-0.1.5.tar.gz

---
+++ packages/diffdrr/diffdrr-0.1.5.tar.gz/diffdrr-0.1.5/test/test_drr.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+
+from diffdrr import DRR, load_example_ct
+
+
+def test_DRR():
+    volume, spacing = load_example_ct()
+    bx, by, bz = np.array(volume.shape) * np.array(spacing) / 2
+    detector_kwargs = {
+        "sdr": 300.0,
+        "theta": np.pi,
+        "phi": 0,
+        "gamma": np.pi / 2,
+        "bx": bx,
+        "by": by,
+        "bz": bz,
+    }
+    try:
+        drr = DRR(volume, spacing, height=200, delx=1.4e-2, device="cuda")
+    except ValueError:
+        drr = DRR(volume, spacing, height=200, delx=1.4e-2, device="cpu")
+    img = drr(**detector_kwargs)
+    assert img.shape == (1, 200, 200)
+
+
+def test_DRR_batch():
+    volume, spacing = load_example_ct()
+    batch = torch.rand(3, 7)
+    try:
+        drr = DRR(volume, spacing, height=200, delx=1.4e-2, device="cuda")
+    except ValueError:
+        drr = DRR(volume, spacing, height=200, delx=1.4e-2, device="cpu")
+
+    img = drr(batch=batch)
+    assert img.shape == (3, 200, 200)
