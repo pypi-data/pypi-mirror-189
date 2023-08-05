466b15c70391e51f6cf399948f2d1e5f664d55ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:58 +0000

    Add package cracker-say/1.9/cracker_say-1.9.tar.gz

---
+++ packages/cracker-say/cracker_say-1.9.tar.gz/cracker_say-1.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='cracker_say',
+version='1.9',
+description='This PIP only made for designing Tool and Termux Bennar',
+author='cracker911181',
+url='https://github.com/cracker911181/cracker_enc',
+packages=['cracker_say'],
+
+zip_safe=False)
