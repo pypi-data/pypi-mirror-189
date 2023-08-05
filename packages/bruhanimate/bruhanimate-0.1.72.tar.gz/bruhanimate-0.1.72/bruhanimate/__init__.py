ca1434362b350ff510d3574ea2050264c1ebff79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:21 +0000

    Add package bruhanimate/0.1.72/bruhanimate-0.1.72.tar.gz

---
+++ packages/bruhanimate/bruhanimate-0.1.72.tar.gz/bruhanimate-0.1.72/bruhanimate/__init__.py
@@ -0,0 +1,33 @@
+from bruhanimate.bruhscreen import Screen
+from bruhanimate.bruheffects    import BaseEffect, StaticEffect, OffsetEffect, NoiseEffect, StarEffect, PlasmaEffect, GameOfLifeEffect, RainEffect, MatrixEffect, _LINE, DrawLines
+from bruhanimate.bruhffer       import Buffer
+from bruhanimate.bruhrenderer   import BaseRenderer, EffectRenderer, CenterRenderer, PanRenderer, FocusRenderer, BackgroundColorRenderer
+from bruhanimate import images
+from bruhanimate import demo, line_demo
+
+
+
+__all__ = [
+    "Screen",
+    "demo"
+    "line_demo"
+    "BaseEffect",
+    "StaticEffect",
+    "OffsetEffect", 
+    "NoiseEffect", 
+    "StarEffect",
+    "PlasmaEffect",
+    "GameOfLifeEffect",
+    "RainEffect",
+    "MatrixEffect",
+    "DrawLines",
+    "_LINE",
+    "Buffer",
+    "BaseRenderer",
+    "EffectRenderer",
+    "CenterRenderer",
+    "PanRenderer",
+    "FocusRenderer",
+    "BackgroundColorRenderer",
+    "images"
+]
