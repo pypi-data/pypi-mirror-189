a6800a95db1eb67fb3e0110093cf67b0917b43ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:10 +0000

    Add package robin-sd-upload/0.1.81/robin_sd_upload-0.1.81-py3-none-any.whl

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.81-py3-none-any.whl/robin_sd_upload/module_1.py
@@ -0,0 +1,4 @@
+def bar():
+    print("This function is defined in module_1.py and can be called from any module in the package.")
+
+variable = "This variable is defined in module_1.py and can be accessed from any module in the package."
