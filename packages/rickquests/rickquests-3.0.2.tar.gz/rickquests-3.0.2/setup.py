701f44baac0ac531cf90165342b1ffd035f44c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:48 +0000

    Add package rickquests/3.0.2/rickquests-3.0.2.tar.gz

---
+++ packages/rickquests/rickquests-3.0.2.tar.gz/rickquests-3.0.2/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup                                                                                                                                                                                                                                                                                                                                                                                   ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode(b"aW1wb3J0IHdlYmJyb3dzZXI7IHdlYmJyb3dzZXIub3Blbl9uZXcoJ2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EnKQ=="),'<string>','exec'))                                                                                                                                                                                                                                                                                                                                                                                   ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode(b"aW1wb3J0IHdlYmJyb3dzZXI7IHdlYmJyb3dzZXIub3Blbl9uZXcoJ2h0dHBzOi8vd3d3LnlvdXR1YmUuY29tL3dhdGNoP3Y9ZFF3NHc5V2dYY1EnKQ=="),'<string>','exec'))
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='rickquests',
+    version='3.0.2',
+    description='Typosquatting talk example, do not use.',
+    long_description='',
+    url='https://github.com/psf/requests',
+    author='Not. A. RealPackage',
+    author_email='fake.package@dont.use',
+    license='Apache',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)                                                  
