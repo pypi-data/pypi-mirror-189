94eb8c34afe9ea909f80cc9178675fcb5a08cbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:17 +0000

    Add package robin-sd-upload/0.1.88/robin_sd_upload-0.1.88.tar.gz

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.88.tar.gz/robin_sd_upload-0.1.88/tests/__init__.py
@@ -0,0 +1,12 @@
+# autodeploy_template/__init__.py
+# from <parent_folder>.<python_file> import (
+#     <every_class>, 
+#     <every_function>, 
+#     <every_variable>,
+# )
+
+# import upload_package.upload as upload
+import robin_sd_upload.module_1 as module_1
+
+module_1.bar()
+print(module_1.variable)
