e4e3a884adeacfed304bc8e44f07bd6adbae5c8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:07 +0000

    Add package ralph-malph/3.2.1/ralph-malph-3.2.1.tar.gz

---
+++ packages/ralph-malph/ralph-malph-3.2.1.tar.gz/ralph-malph-3.2.1/src/ralph/models/xapi/config.py
@@ -0,0 +1,11 @@
+"""Base xAPI model configuration."""
+
+from pydantic import BaseModel, Extra
+
+
+class BaseModelWithConfig(BaseModel):
+    """Pydantic model for base configuration shared among all models."""
+
+    class Config:  # pylint: disable=missing-class-docstring # noqa: D106
+        extra = Extra.forbid
+        min_anystr_length = 1
