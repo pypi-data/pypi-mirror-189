df3fe2a945436cc99c161e65edf9b50dd1774246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:14 +0000

    Add package Hosein-Tabr/2.0.0/Hosein_Tabr-2.0.0.tar.gz

---
+++ packages/Hosein-Tabr/Hosein_Tabr-2.0.0.tar.gz/Hosein_Tabr-2.0.0/Hosein_Tabr/An_Wb.py
@@ -0,0 +1,50 @@
+class SetClines:
+	web = {
+		"app_name"    : "Main",
+		"app_version" : "4.0.7",
+		"platform"    : "Web",
+		"package"     : "web.rubika.ir",
+		"lang_code"   : "fa"
+	}
+
+	android = {
+	    "app_name":"Main",
+		"app_version":"3.0.9",
+		"platform":"Android",
+		"package":"ir.resaneh1.iptv",
+		"lang_code":"fa"
+	}
+
+
+class Server:
+	matnadress = [
+			'https://messengerg2c26.iranlms.ir',
+			"https://messengerg2c33.iranlms.ir",
+			'https://messengerg2c22.iranlms.ir',
+			'https://messengerg2c25.iranlms.ir',
+			'https://messengerg2c55.iranlms.ir',
+			'https://messengerg2c57.iranlms.ir',
+			'https://messengerg2c15.iranlms.ir',
+			'https://messengerg2c16.iranlms.ir',
+			'https://messengerg2c46.iranlms.ir' ,
+			'https://messengerg2c39.iranlms.ir',
+			'https://messengerg2c40.iranlms.ir',
+			'https://messengerg2c41.iranlms.ir',
+			'https://messengerg2c6.iranlms.ir',
+			'https://messengerg2c7.iranlms.ir',
+			'https://messengerg2c63.iranlms.ir',
+			'https://messengerg2c31.iranlms.ir',
+			'https://messengerg2c3.iranlms.ir'
+			]
+
+
+	filesadress = [
+			'https://messengerg2c26.iranlms.ir'
+			'https://messengerg2c39.iranlms.ir',
+			'https://messengerg2c7.iranlms.ir',
+			'https://messengerg2c17.iranlms.ir',
+			'https://messengerg2c46.iranlms.ir'
+			]
+
+
+
---
+++ packages/Hosein-Tabr/Hosein_Tabr-2.0.0.tar.gz/Hosein_Tabr-2.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md","r")as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+   name = "Hosein_Tabr",
+   version = "2.0.0",
+   authors = "hosein",
+   email= "09014719950qwer@gmail.com",
+   description = "Robot",
+   readme = "README.md",
+   url = "https://github.com/pypa/sampleproject",
+   requires_python = "1.48.0",
+   classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    ])
