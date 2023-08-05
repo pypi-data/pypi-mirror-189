6bdc85d4cfb1a38288405a1d39ec533f0685c336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package kernel-sidecar/0.1.1/kernel_sidecar-0.1.1.tar.gz

---
+++ packages/kernel-sidecar/kernel_sidecar-0.1.1.tar.gz/kernel_sidecar-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['kernel_sidecar']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'kernel-sidecar',
+    'version': '0.1.1',
+    'description': 'A sidecar ',
+    'long_description': '# kernel-sidecar\nJupyter Kernel backend sidecar\n',
+    'author': 'Matt Kafonek',
+    'author_email': 'matt.kafonek@noteable.io',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kafonek/kernel-sidecar',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
