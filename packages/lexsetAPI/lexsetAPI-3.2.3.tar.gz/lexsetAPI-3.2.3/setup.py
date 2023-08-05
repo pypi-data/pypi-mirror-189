8bd64578d9dfaa941c1c5386307098af7477a989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:10 +0000

    Add package lexsetAPI/3.2.3/lexsetAPI-3.2.3.tar.gz

---
+++ packages/lexsetAPI/lexsetAPI-3.2.3.tar.gz/lexsetAPI-3.2.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(name='lexsetAPI',
+      version='3.2.3',
+      author='F. Bitonti',
+      author_email='Francis@lexset.ai',
+      license='Apache 2.0',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=['lexsetAPI'],
+      install_requires=[
+          'pyjson',
+          'PyYAML',
+          'pyBase64',
+          'twdm',
+          'concurrent.futures',
+          'time',
+          'subprocess',
+          'threading',
+          'requests'
+      ]
+)
