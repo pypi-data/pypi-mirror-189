424f16df1e7f3679b299378fd797747d71927ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package fastapi-databasetk/1.0.0/fastapi_databasetk-1.0.0.tar.gz

---
+++ packages/fastapi-databasetk/fastapi_databasetk-1.0.0.tar.gz/fastapi_databasetk-1.0.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastapi_databasetk', 'fastapi_databasetk.objects']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['databases>=0.6.0,<0.7.0',
+ 'fastapi>=0.89.1,<0.90.0',
+ 'pydantic>=1.10.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fastapi-databasetk',
+    'version': '1.0.0',
+    'description': '',
+    'long_description': '',
+    'author': 'wayfaring-stranger',
+    'author_email': 'zw6p226m@duck.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
