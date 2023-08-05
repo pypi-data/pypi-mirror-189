191ef81a6d93bccffa77f745e988868159a73a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:06 +0000

    Add package apysc/2.5.102/apysc-2.5.102-py3-none-any.whl

---
+++ packages/apysc/apysc-2.5.102-py3-none-any.whl/apysc/_display/x_interface.py
@@ -0,0 +1,50 @@
+"""The interface implementation for the x-coordinate.
+"""
+
+from abc import ABC
+from abc import abstractmethod
+from typing import Union
+
+from apysc._type.int import Int
+
+
+class XInterface(ABC):
+
+    _x: Int
+
+    @property
+    @abstractmethod
+    def x(self) -> Int:
+        """
+        Get an x-coordinate.
+        """
+
+    @x.setter
+    @abstractmethod
+    def x(self, value: Int) -> None:
+        """
+        Update x-coordinate.
+
+        Parameters
+        ----------
+        value : Int
+            X-coordinate value.
+        """
+
+    @abstractmethod
+    def _update_x_and_skip_appending_exp(self, *, x: Union[int, Int]) -> None:
+        """
+        Update an x-coordinate and skip appending an expression.
+
+        Parameters
+        ----------
+        x : int or Int
+            X-coordinate value.
+        """
+
+    @abstractmethod
+    def _initialize_x_if_not_initialized(self) -> None:
+        """
+        Initialize the _x attribute if this instance does not
+        initialize it yet.
+        """
