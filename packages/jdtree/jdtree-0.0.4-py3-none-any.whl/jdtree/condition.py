8c0517e4a7bfc65a3b6b6a9b3a939a2195e3593f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:14 +0000

    Add package jdtree/0.0.4/jdtree-0.0.4-py3-none-any.whl

---
+++ packages/jdtree/jdtree-0.0.4-py3-none-any.whl/jdtree/condition.py
@@ -0,0 +1,27 @@
+from typing import List, Any
+from numbers import Number
+from distutils.util import strtobool
+
+class Operator:
+
+    def between(self, x: Number, y: str):
+        list = y.split(',')
+        if len(list) != 2:
+            return False
+        return eval(f'{list[0]} <= {x} <= {list[1]}')
+
+    def not_between(self, x: Number, y: str):
+        list = y.split(',')
+        if len(list) != 2:
+            return False
+        return not eval(f'{list[0]} <= {x} <= {list[1]}')
+
+    def empty(self, x: Any, y: str):
+
+        if x and strtobool(y):
+            return True
+
+        if not x and not strtobool(y):
+            return True
+
+        return False
