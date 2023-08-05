3eb3085ef1de47524c2166a3f90ec584c827b8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:10 +0000

    Add package docsetviz/1.0.3/docsetviz-1.0.3.tar.gz

---
+++ packages/docsetviz/docsetviz-1.0.3.tar.gz/docsetviz-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt', 'r') as f:
+    REQUIRED_DEP = [i.replace('\n', '') for i in f.readlines()]
+
+setup(
+    name='docsetviz',
+    version='1.0.3',
+    description='Docset Visualization Tool',
+    author='yanaenen',
+    install_requires=REQUIRED_DEP,
+    package_dir={'docsetviz': '.'},
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'docsetviz=docsetviz.main:main'
+        ]
+    },
+)
