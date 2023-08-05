7b01c16e3e9fa23dd159d73f952fddee5b77970f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:57 +0000

    Add package babble-cloud-cli/0.1.11/babble_cloud_cli-0.1.11-py3-none-any.whl

---
+++ packages/babble-cloud-cli/babble_cloud_cli-0.1.11-py3-none-any.whl/babble/scripts/view.py
@@ -0,0 +1,15 @@
+import os
+import util
+
+projects = sorted(os.listdir("/projects"))
+
+if len(projects) == 0:
+    print("\n> no projects found\n")
+    exit()
+
+project = util.prompt(projects, "select project to view", "ctrl-c to cancel")
+print()
+if not os.path.exists(f"/projects/{project}/config.yaml"):
+    exit()
+os.system(f"cat /projects/{project}/config.yaml")
+print("\n")
