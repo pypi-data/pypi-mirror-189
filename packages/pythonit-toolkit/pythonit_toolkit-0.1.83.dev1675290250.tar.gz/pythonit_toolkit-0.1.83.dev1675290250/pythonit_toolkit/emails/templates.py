5341559bff7bf26496bb28a5e31e81d7f2814d4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:12 +0000

    Add package pythonit-toolkit/0.1.83.dev1675290250/pythonit_toolkit-0.1.83.dev1675290250.tar.gz

---
+++ packages/pythonit-toolkit/pythonit_toolkit-0.1.83.dev1675290250.tar.gz/pythonit_toolkit-0.1.83.dev1675290250/pythonit_toolkit/emails/templates.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class EmailTemplate(str, Enum):
+    GRANT_APPROVED = "grant-approved"
+    GRANT_WAITING_LIST = "grant-waiting-list"
+    GRANT_REJECTED = "grant-rejected"
+    RESET_PASSWORD = "reset-password"
+    NEW_COMMENT_ON_SUBMISSION = "new-comment-on-submission"
+    SUBMISSION_ACCEPTED = "submission-accepted"
+    NEW_SCHEDULE_INVITATION_ANSWER = "new-schedule-invitation-answer"
+    SUBMISSION_SCHEDULE_TIME_CHANGED = "submission-schedule-time-change"
+    SPEAKER_VOUCHER_CODE = "speaker-voucher-code"
+    SPEAKER_COMMUNICATION = "speaker-communication"
+
+    def __str__(self) -> str:
+        return str.__str__(self)
