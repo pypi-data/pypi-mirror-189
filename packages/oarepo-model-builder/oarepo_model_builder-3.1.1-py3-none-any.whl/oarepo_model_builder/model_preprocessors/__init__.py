9d20a4a94b50be23b5eecce5d611c0c81c535795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:48 +0000

    Add package oarepo-model-builder/3.1.1/oarepo_model_builder-3.1.1-py3-none-any.whl

---
+++ packages/oarepo-model-builder/oarepo_model_builder-3.1.1-py3-none-any.whl/oarepo_model_builder/model_preprocessors/__init__.py
@@ -0,0 +1,24 @@
+class ModelPreprocessor:
+    def __init__(self, builder: "oarepo_model_builder.builder.ModelBuilder"):
+        self.builder = builder
+
+    def transform(self, schema, settings):
+        pass
+
+    def set(self, settings, name, callable=None):
+        if callable:
+            if not settings.get(name):
+                settings[name] = callable()
+            return
+
+        def w(func):
+            if not settings.get(name):
+                settings[name] = func()
+
+        return w
+
+    def set_default_and_append_if_not_present(self, container, key, value, appended):
+        container.setdefault(key, value)
+        arr = getattr(container, key)
+        if appended not in arr:
+            arr.append(appended)
