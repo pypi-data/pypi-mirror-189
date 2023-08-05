0bd750ea00a60c02f124f1a8365cdad89327b825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:25 +0000

    Add package five.localsitemanager/4.0/five.localsitemanager-4.0.tar.gz

---
+++ packages/five.localsitemanager/five.localsitemanager-4.0.tar.gz/five.localsitemanager-4.0/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+
+setup(
+    name='five.localsitemanager',
+    version='4.0',
+    url='https://github.com/zopefoundation/five.localsitemanager',
+    project_urls={
+        'Issue Tracker': ('https://github.com/zopefoundation/'
+                          'five.localsitemanager/issues'),
+        'Sources': 'https://github.com/zopefoundation/five.localsitemanager',
+    },
+    license='ZPL 2.1',
+    description='Local site manager implementation for Zope.',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Zope :: 5',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+    keywords='zope five sitemanager',
+    packages=['five', 'five.localsitemanager'],
+    package_dir={'': 'src'},
+    namespace_packages=['five'],
+    include_package_data=True,
+    python_requires='>=3.7',
+    install_requires=[
+        'Acquisition',
+        'persistent',
+        'setuptools',
+        'zope.component',
+        'zope.event',
+        'zope.interface >= 3.8',
+        'zope.location',
+        'zope.lifecycleevent',
+        'zope.site',
+        'zope.testing',
+        'Zope >= 4.0b1',
+    ],
+    zip_safe=False,
+)
