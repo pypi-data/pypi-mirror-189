ac6bcbd3e54a3fa852f0aa539a76dec3fb4c5ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:23 +0000

    Add package docsetviz/1.0.4/docsetviz-1.0.4.tar.gz

---
+++ packages/docsetviz/docsetviz-1.0.4.tar.gz/docsetviz-1.0.4/build/lib/libs/hashable_qlist_widget_item.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import *
+
+
+class HashableQListWidgetItem(QListWidgetItem):
+
+    def __init__(self, *args):
+        super(HashableQListWidgetItem, self).__init__(*args)
+
+    def __hash__(self):
+        return hash(id(self))
