40c99c9d7864c6a535600c4edc5e08c49e1c4659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:02 +0000

    Add package bittrade-cryptodotcom-websocket/0.1.15/bittrade_cryptodotcom_websocket-0.1.15.tar.gz

---
+++ packages/bittrade-cryptodotcom-websocket/bittrade_cryptodotcom_websocket-0.1.15.tar.gz/bittrade_cryptodotcom_websocket-0.1.15/bittrade_cryptodotcom_websocket/connection/bundle.py
@@ -0,0 +1,9 @@
+from typing import Any
+from ..models.enhanced_websocket import EnhancedWebsocket, CryptodotcomRequestMessage
+from .message_types import MessageTypes
+from .status import Status
+
+
+WebsocketBundle = tuple[EnhancedWebsocket, MessageTypes, Status | CryptodotcomRequestMessage]
+WebsocketStatusBundle = tuple[EnhancedWebsocket, MessageTypes, Status]
+WebsocketMessageBundle = tuple[EnhancedWebsocket, MessageTypes, CryptodotcomRequestMessage]
