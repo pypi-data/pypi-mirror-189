6dc5725d3ed70f555a32d023aaa7febfc7280cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:59 +0000

    Add package ani-file/0.0.1/ani_file-0.0.1-py3-none-any.whl

---
+++ packages/ani-file/ani_file-0.0.1-py3-none-any.whl/ani_file/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+ile.open(".\\my_aero_busy.ani","wb")
+# my_ani.setframespath([".\\0.ico",".\\3.ico",".\\6.ico",".\\9.ico",".\\12.ico",".\\15.ico"])
+# my_ani.setauthor("ABCDE")
+# my_ani.close()
+
+# my_ani = ani_file.
