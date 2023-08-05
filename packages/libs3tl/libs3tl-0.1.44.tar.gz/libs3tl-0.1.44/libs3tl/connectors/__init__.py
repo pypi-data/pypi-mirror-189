a53afb870c946f3a3f9015aad9d4ce69d2c2b94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:54 +0000

    Add package libs3tl/0.1.44/libs3tl-0.1.44.tar.gz

---
+++ packages/libs3tl/libs3tl-0.1.44.tar.gz/libs3tl-0.1.44/libs3tl/connectors/__init__.py
@@ -0,0 +1,17 @@
+from .source_file.client import Client as files
+from .destination_sftp_json.client import SftpClient
+from .destination_azblob.destination import DestinationAzBlob
+from .destination_IPFS.client import IPFSDestination
+from .source_IPFS.client import IPFSSource
+from .source_postgres.client import PostgresSource
+from .destination_postgres.client import PostgresDestination
+
+__all__ = [
+    'files',
+    'SftpClient',
+    'DestinationAzBlob',
+    'IPFSDestination',
+    'IPFSSource',
+    'PostgresSource',
+    'PostgresDestination'
+]
