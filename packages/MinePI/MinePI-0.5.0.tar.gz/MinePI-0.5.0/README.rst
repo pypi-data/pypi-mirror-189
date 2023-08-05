f3bf15156b69e489578d10933456d116e7ecce25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:42 +0000

    Add package MinePI/0.5.0/MinePI-0.5.0.tar.gz

---
+++ packages/MinePI/MinePI-0.5.0.tar.gz/MinePI-0.5.0/README.rst
@@ -0,0 +1,28 @@
+MinePI
+======
+
+.. image:: https://readthedocs.org/projects/minepi/badge/?version=latest
+
+MinePI is an all in one Minecraft skin render library written in Python.
+
+Docs
+----
+
+https://minepi.readthedocs.io/en/latest/
+
+Contribute
+----------
+
+- Issue Tracker: github.com/benno1237/MinePI/issues
+- Source Code: github.com/benno1237/MinePI
+
+Support
+-------
+
+If you are having issues, please let us know.
+Either open an Issue or contact me (Benno#8969) on Discord
+
+License
+-------
+
+The project is licensed under the MIT license.
---
+++ packages/MinePI/MinePI-0.5.0.tar.gz/MinePI-0.5.0/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+setup(
+    name="MinePI",
+    packages=["minepi"],
+    version="0.5.0",
+    license="MIT",
+    description="Minecraft utility library.",
+    author="benno1237, honigkuchen",
+    author_email="benno.kollreider@gmail.com",
+    url="https://github.com/benno1237/MinePI",
+    download_url="https://github.com/benno1237/MinePI/archive/refs/tags/0.5.0.tar.gz",
+    keywords=["Minecraft", "Skin", "Render", "Head", "UUID"],
+    install_requires=[
+        "aiohttp",
+        "Pillow",
+    ],
+    extras_require={
+        "docs": [
+            "sphinx",
+            "sphinx-rtd-theme",
+            "sphinx-prompt",
+            "autodocsumm"
+        ]
+    },
+    classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    ],
+)
