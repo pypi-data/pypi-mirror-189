d2674f1ef0164e4c3637e13757dc9a182f354392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:20:47 +0000

    Add package multimediasorter/1.0.0/multimediasorter-1.0.0-py3-none-any.whl

---
+++ packages/multimediasorter/multimediasorter-1.0.0-py3-none-any.whl/multimediasorter/__init__.py
@@ -0,0 +1,5 @@
+from .metadata import MetadataProvider
+from .parser import parse_movie_name, parse_season_and_episode
+from .sorter import MediaSorter, MediaType, MediaSorterException
+
+__version__ = "1.0.0"
