3c530a9eadd2b41388f79c4e0a351a82d5a64205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:24 +0000

    Add package SEACells/0.3.3/SEACells-0.3.3.tar.gz

---
+++ packages/SEACells/SEACells-0.3.3.tar.gz/SEACells-0.3.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SEACells",
+    version="0.3.3",
+    author="Pe'er Lab",
+    author_email="scp2152@columbia.edu",
+    description=" ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dpeerlab/SEACells",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "palantir",
+        "scanpy>1.8",
+        "anndata",
+        "numba>=0.51.2",
+        "scipy>=1.5",
+        "pyranges"
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8.0',
+    include_package_data=True,
+    package_data={'': ['SEACells/Rscripts/*', '*.r', '*.R']},
+    zip_safe=False
+)
