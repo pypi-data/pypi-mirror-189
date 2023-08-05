ea14e983f024127929a45ef53bad76b9a48b2431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:31 +0000

    Add package mymatchstick/1.0.0/mymatchstick-1.0.0.tar.gz

---
+++ packages/mymatchstick/mymatchstick-1.0.0.tar.gz/mymatchstick-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='mymatchstick',
+    py_modules=['mymatchstick'],
+    version='1.0.0',
+    author='xfrszrsfr',
+    author_email='xfrszrsfr@163.com',
+    description='计算摆放一个自然数需要多少根火柴棒',
+    )
