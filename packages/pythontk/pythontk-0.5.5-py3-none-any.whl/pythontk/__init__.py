ea43a9deeefc568d7ee05445b5f55df5817f5fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:03 +0000

    Add package pythontk/0.5.5/pythontk-0.5.5-py3-none-any.whl

---
+++ packages/pythontk/pythontk-0.5.5-py3-none-any.whl/pythontk/__init__.py
@@ -0,0 +1,30 @@
+# !/usr/bin/python
+# coding=utf-8
+from pythontk.core import *
+from pythontk.File import File
+from pythontk.Img import Img
+from pythontk.Iter import Iter
+from pythontk.Json import Json
+from pythontk.Math import Math
+from pythontk.Str import Str
+
+
+name = 'pythontk'
+__version__ = '0.5.5'
+
+# --------------------------------------------------------------------------------------------
+
+
+
+
+
+
+# print (__name__) #module name
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# deprecated:
+# --------------------------------------------------------------------------------------------
