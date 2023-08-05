6d20eef8741bccc4ad80b03507ea135d5b68f5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:32 +0000

    Add package cpmax-toolbox/0.0.19.20230201/cpmax_toolbox-0.0.19.20230201.tar.gz

---
+++ packages/cpmax-toolbox/cpmax_toolbox-0.0.19.20230201.tar.gz/cpmax_toolbox-0.0.19.20230201/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cpmax_toolbox']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytz>=2022.7.1,<2023.0.0', 'requests>=2.28.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cpmax-toolbox',
+    'version': '0.0.19.20230201',
+    'description': '',
+    'long_description': '',
+    'author': 'JRoseCPMax',
+    'author_email': 'j.rose@cpmax.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
