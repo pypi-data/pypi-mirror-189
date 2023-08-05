70b256be3bb75041e6e4c8191653dc1d7fc5cf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:18:48 +0000

    Add package distrib-test-pkg/0.0.2/distrib-test-pkg-0.0.2.tar.gz

---
+++ packages/distrib-test-pkg/distrib-test-pkg-0.0.2.tar.gz/distrib-test-pkg-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name="distrib-test-pkg",
+        version="0.0.2",
+        description="Security Research for Dangling Packages",
+        long_description="Security Research for Dangling Packages",
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages()
+)
