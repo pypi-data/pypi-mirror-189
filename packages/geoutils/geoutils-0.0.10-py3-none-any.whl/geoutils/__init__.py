5a6b51f160d271b4450cd58b50a0c7f034231e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package geoutils/0.0.10/geoutils-0.0.10-py3-none-any.whl

---
+++ packages/geoutils/geoutils-0.0.10-py3-none-any.whl/geoutils/__init__.py
@@ -0,0 +1,20 @@
+"""
+GeoUtils is a python package of raster and vector tools.
+"""
+
+from geoutils import spatial_tools  # noqa
+from geoutils import examples, georaster, geovector, projtools, satimg  # noqa
+from geoutils.georaster import Raster  # noqa
+from geoutils.geovector import Vector  # noqa
+from geoutils.satimg import SatelliteImage  # noqa
+
+try:
+    from geoutils.version import version as __version__  # noqa
+except ImportError:  # pragma: no cover
+    raise ImportError(
+        "geoutils is not properly installed. If you are "
+        "running from the source directory, please instead "
+        "create a new virtual environment (using conda or "
+        "virtualenv) and then install it in-place by running: "
+        "pip install -e ."
+    )
