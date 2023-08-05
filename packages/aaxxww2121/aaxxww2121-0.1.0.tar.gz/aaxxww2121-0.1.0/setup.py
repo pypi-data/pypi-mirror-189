208974acfc64b083b0f6477426c07974ff78b7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package aaxxww2121/0.1.0/aaxxww2121-0.1.0.tar.gz

---
+++ packages/aaxxww2121/aaxxww2121-0.1.0.tar.gz/aaxxww2121-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='aaxxww2121',
+    version='0.1.0',
+    description='A simple math library',
+    author='Your Name',
+    author_email='your.email@example.com',
+    py_modules=['aaxxww2121'],
+)
