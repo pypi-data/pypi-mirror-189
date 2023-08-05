fe70ffb37361036a8b1cd33d13e34812a064edf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:53 +0000

    Add package dirty-cat/0.4.0b2/dirty_cat-0.4.0b2-py3-none-any.whl

---
+++ packages/dirty-cat/dirty_cat-0.4.0b2-py3-none-any.whl/dirty_cat/datasets/__init__.py
@@ -0,0 +1,34 @@
+from ._fetching import (
+    DatasetAll,
+    DatasetInfoOnly,
+    fetch_drug_directory,
+    fetch_employee_salaries,
+    fetch_medical_charge,
+    fetch_midwest_survey,
+    fetch_open_payments,
+    fetch_road_safety,
+    fetch_traffic_violations,
+    fetch_world_bank_indicator,
+    get_data_dir,
+)
+
+
+from ._generating import (
+    make_deduplication_data,
+)
+
+
+__all__ = [
+    "DatasetAll",
+    "DatasetInfoOnly",
+    "fetch_drug_directory",
+    "fetch_employee_salaries",
+    "fetch_medical_charge",
+    "fetch_midwest_survey",
+    "fetch_open_payments",
+    "fetch_road_safety",
+    "fetch_traffic_violations",
+    "fetch_world_bank_indicator",
+    "get_data_dir",
+    "make_deduplication_data",
+]
