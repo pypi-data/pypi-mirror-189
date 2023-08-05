d3805515bafd9539bb83e3ab9d9f63e9d8499723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:42 +0000

    Add package argparge/0.1.1/argparge-0.1.1.tar.gz

---
+++ packages/argparge/argparge-0.1.1.tar.gz/argparge-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['argparge']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'argparge',
+    'version': '0.1.1',
+    'description': 'A very simple tool to create beautiful console application by using native argparse.',
+    'long_description': 'None',
+    'author': 'Ozcan Yarimdunya',
+    'author_email': 'ozcan.yarimdunya@sahibinden.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
