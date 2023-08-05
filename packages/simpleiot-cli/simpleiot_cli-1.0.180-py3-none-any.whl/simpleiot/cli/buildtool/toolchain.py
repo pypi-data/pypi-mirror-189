869d32b7dcb7e2aee12adf4fea15b97d84fc0abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:32 +0000

    Add package simpleiot-cli/1.0.180/simpleiot_cli-1.0.180-py3-none-any.whl

---
+++ packages/simpleiot-cli/simpleiot_cli-1.0.180-py3-none-any.whl/simpleiot/cli/buildtool/toolchain.py
@@ -0,0 +1,247 @@
+# © 2022 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+#
+# SimpleIOT project.
+# Author: Ramin Firoozye (framin@amazon.com)
+#
+# Toolchain installer classes
+#
+import platform
+import binascii
+import traceback
+import sys
+import os
+import shutil
+from pathlib import Path
+
+DEFAULT_TOOLCHAIN_BASE="~/.simpleiot/_toolchain"
+
+
+class Toolchain:
+    toolchain_list = {}
+    toolchain_alias = {}
+
+    # This needs to be generalized so we can support toolchains for other devices than Arduino
+    # Until then, it gets passed down.
+
+    def __init__(self, default_arduino_version):
+        # NOTE: we import the import of implementation classes here so we don't create import loops.
+        # This way, the implementation classes can call class methods without causing import loops.
+        #
+        from .ToolchainESP32Arduino_1_0_0 import ToolchainESP32Arduino_1_0_0
+
+        self._register(ToolchainESP32Arduino_1_0_0(default_arduino_version))
+
+        # Add import/list of other toolchains here, for example:
+        # self._register("microchip", "imx7", "freertos", "1.0.0", ToolchainIMX7FreeRTOS_1_0_0)
+
+    @classmethod
+    def base(cls):
+        """
+        Returns the default base directory for toolchains. On devices that use external installation
+        tools (like Mac with Homebrew) this is ignored.
+        """
+        return DEFAULT_TOOLCHAIN_BASE
+
+    def _make_key(self, manufacturer, processor, opsys, version):
+        strkey = f"{manufacturer}::{processor}::{opsys}::{version}"
+        hexbin = binascii.hexlify(strkey.encode("utf-8"))
+        hexstr = str(hexbin, "utf-8")
+        return hexstr
+
+    # The key and alias_dict are calculated in the constructor
+    #
+    def _register(self, cls):
+        self.toolchain_list[cls.key] = cls
+        self.toolchain_alias[cls.alias] = cls.alias_dict
+
+    def _resolve_alias(self, alias):
+        cls = self.toolchain_alias.get(alias, None)
+        if not cls:
+            print(f"ERROR: Toolchain alias '{alias}' not recognized.")
+            exit(1)
+        return cls
+
+    def _load_config(self):
+        print("Load local config file and see what's installed on this machine")
+
+    def _set_config(self, manufacturer, processor, opsys, version, location, binary):
+        key = self._make_key(manufacturer, processor, opsys, version)
+        print("Save configuration data for this platform -- overwrites old one")
+
+    def _delete_config(self, manufacturer, processor, opsys, version, location, binary):
+        key = self._make_key(manufacturer, processor, opsys, version)
+        print("Deletes configuration data for this platform--after install")
+
+    def install_alias(self, base, alias, verbose):
+        params = self._resolve_alias(alias)
+        self.install(base,
+                     params["man"],
+                     params["pro"],
+                     params["ops"],
+                     params["ver"],
+                     params["loc"], verbose)
+
+
+    def install(self, base, manufacturer, processor, opsys, version, location, verbose=False):
+        try:
+            if verbose:
+                print(f"+ Looking up build tool for: mfr: {manufacturer} proc: {processor} os: {opsys} ver: {version} verbose: {verbose}")
+            key = self._make_key(manufacturer, processor, opsys, version)
+            if verbose:
+                print(f"+ Generated install hash key: {key}")
+            installer = self.toolchain_list.get(key, None)
+            if verbose:
+                print(f"+ Installer instance is: {str(installer)}")
+
+            if installer:
+                install_dir = installer.install_path(base)
+                if verbose:
+                    print(f"+ Install directory is: {install_dir}")
+
+                opsys = platform.system()
+                if verbose:
+                    print(f"+ Runtime OS is: {opsys}")
+                if opsys == "Darwin":
+                    installer.install_mac(install_dir, location, verbose)
+                elif opsys == "Windows":
+                    installer.install_windows(install_dir, location, verbose)
+                elif opsys == "Linux":
+                    installer.install_unix(install_dir, location, verbose)
+                else:
+                    print(f"ERROR: operating system not supported")
+            else:
+                print(f"ERROR: unable to install toolchain for specified device")
+                exit(1)
+        except Exception as e:
+            print(f"ERROR installing tool: {str(e)}")
+            print(traceback.format_exc())
+            exit(1)
+
+
+    def uninstall(self, base, manufacturer, processor, opsys, version, location, verbose=False):
+        try:
+            if verbose:
+                print(f"+ Looking up build tool for: mfr: {manufacturer} proc: {processor} os: {opsys} ver: {version}")
+            key = self._make_key(manufacturer, processor, opsys, version)
+            if verbose:
+                print(f"+ Generated install hash key: {key}")
+            installer = self.toolchain_list.get(key, None)
+            if installer:
+                install_dir = installer.install_path(base)
+                if verbose:
+                    print(f"+ Install directory is: {install_dir}")
+
+                opsys = platform.system()
+                if verbose:
+                    print(f"+ Runtime OS is: {opsys}")
+                if opsys == "Darwin":
+                    installer.uninstall_mac(install_dir, location, verbose)
+                elif opsys == "Windows":
+                    installer.uninstall_windows(install_dir, location, verbose)
+                elif opsys == "Linux":
+                    installer.uninstall_unix(install_dir, location, verbose)
+                else:
+                    print(f"ERROR: operating system not supported")
+
+                # Also remove the directory in .simpleiot if it still exists
+                #
+                if os.path.exists(install_dir):
+                    if verbose:
+                        print(f"+ Cleaning remnant directory {install_dir}")
+                    shutil.rmtree(install_dir)
+
+                print("Done.")
+            else:
+                print(f"ERROR: unable to uninstall toolchain for specified device")
+                exit(1)
+        except Exception as e:
+            print(f"ERROR uninstalling tool: {str(e)}")
+            exit(1)
+
+    def reset(self, base, manufacturer, processor, opsys, version, location, verbose=False):
+        try:
+            print("Loading defaults for toolchain...")
+            if verbose:
+                print(f"+ Looking up build tool for: mfr: {manufacturer} proc: {processor} os: {opsys} ver: {version}")
+            key = self._make_key(manufacturer, processor, opsys, version)
+            if verbose:
+                print(f"+ Generated install hash key: {key}")
+
+            installer = self.toolchain_list.get(key, None)
+            if installer:
+                opsys = platform.system()
+                if verbose:
+                    print(f"+ Runtime OS is: {opsys}")
+                if opsys == "Darwin":
+                    installer.reset_mac(base, location, verbose)
+                elif opsys == "Windows":
+                    # For windows, we install the tool in a custom directory under ~/.simpleiot/_toolchain/{hash}
+                    install_path = Path(base) / key
+                    installer.reset_windows(install_path, location, verbose)
+                elif opsys == "Linux":
+                    installer.reset_unix(base, location, verbose)
+                else:
+                    print(f"ERROR: operating system not supported")
+
+                print("Done.")
+            else:
+                print(f"ERROR: No toolchain found. Unable to reset toolchain for specified device")
+                exit(1)
+        except Exception as e:
+            print(f"ERROR resetting tool to default settings: {str(e)}")
+            exit(1)
+
+    def build(self, base, manufacturer, processor, opsys, version, location, dirpath, command, verbose=False):
+        try:
+            if verbose:
+                print(f"+ Looking up build tool for: mfr: {manufacturer} proc: {processor} os: {opsys} ver: {version}")
+            key = self._make_key(manufacturer, processor, opsys, version)
+            if verbose:
+                print(f"+ Generated install hash key: {key}")
+
+            installer = self.toolchain_list.get(key, None)
+            if installer:
+                if verbose:
+                    print(f"+ Found toolchain subclass: {str(installer)}. Invoking.")
+                result = installer.build(base, dirpath, command)
+                if verbose:
+                    print(f"+ Toolchain build result: {result}")
+                return result
+            else:
+                print(f"ERROR: unable to find toolchain for specified device")
+                exit(1)
+        except Exception as e:
+            print(f"ERROR running tool: {str(e)}")
+            print(traceback.format_exc())
+            exit(1)
+
+    def build_and_flash(self, base, manufacturer, processor, opsys, version, location, dirpath, command, verbose=False):
+        try:
+            if verbose:
+                print(f"+ Looking up build tool for: mfr: {manufacturer} proc: {processor} os: {opsys} ver: {version}")
+            key = self._make_key(manufacturer, processor, opsys, version)
+            if verbose:
+                print(f"+ Generated install hash key: {key}")
+
+            installer = self.toolchain_list.get(key, None)
+            if installer:
+                if verbose:
+                    print(f"+ Found toolchain subclass: {str(installer)}. Invoking.")
+                result = installer.build_and_flash(base, dirpath, command, verbose)
+                if verbose:
+                    print(f"+ Toolchain build and flash result: {result}")
+                return result
+            else:
+                print(f"ERROR: unable to find toolchain for specified device")
+                exit(1)
+        except Exception as e:
+            print(f"ERROR running tool: {str(e)}")
+            print(traceback.format_exc())
+            exit(1)
+
+    #
+    # This lists the available toolchains this user can install.
+    # This returns all the concrete ToolChain classes registered on this system.
+    #
+    def list_available(self, verbose=False):
+        return self.toolchain_list
---
+++ packages/simpleiot-cli/simpleiot_cli-1.0.180-py3-none-any.whl/simpleiot/cli/toolchain.py
@@ -0,0 +1,255 @@
+#!/usr/bin/env python
+
+# © 2022 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+#
+# SimpleIOT project.
+# Author: Ramin Firoozye (framin@amazon.com)
+#
+# This command manages build toolchains. It is here to allow
+# the build toolchain to be easily installed. It currently supports local install of
+# the arduino-cli for ESP32, but is intended so it can be extended to Docker-based
+# local as well as remote Cloud-based installations.
+#
+# It is also intended to be cross-platform (Mac/Win)
+# The operations available are:
+#
+# - toolchain install: install one or more versions of the toolchain.
+# - toolchain update: update to the latest version of the toolchain.
+# - toolchain uninstall: remove.
+# - toolchain list: list all the toolchains available for building.
+#
+
+import click
+from simpleiot.common.utils import *
+from simpleiot.common.config import *
+from simpleiot.cli.buildtool.toolchain import Toolchain
+from simpleiot.cli.buildtool.toolchainbase import ToolchainLocation
+
+from rich import print
+from rich.console import Console
+from rich.table import Table
+import serial.tools.list_ports
+import questionary
+import tempfile
+import os
+import zipfile
+from pathlib import Path
+from shutil import which
+import requests
+import platform
+import tempfile
+
+# NOTE: Update this with latest ESP32 toolchain version getting installed.
+# For non-Arduino toolchains, we will need a central registry.
+# THIS IS NOT THE ARDUINO CLI VERSION. IT'S THE INSTALLED ESP32 TOOLCHAIN ONE.
+# For example, on the Mac it's installed at:
+# ~/Library/Arduino15/packages/esp32/tools/esptool_py/4.2.1/esptool
+# We need the '4.2.1' version here. We really need to find a better way of determining
+# the version.
+#
+LATEST_ARDUINO_ESP32_TOOLCHAIN_VERSION = "4.2.1"
+
+#
+# NOTE: the toolchain directory starts with an underline. When listing all the teams installed
+# we specifically exclude directory names that start with and underline. This way, we can find all local teams
+# simply by scanning the contents of the ~/.simpleiot directory instead of managing a local registry which can
+# fall out of sync if someone messes with the directory.
+#
+console = Console()
+toolchain_class = Toolchain(LATEST_ARDUINO_ESP32_TOOLCHAIN_VERSION)
+
+
+@click.group()
+def toolchain():
+    """Project Template management"""
+
+@toolchain.command()
+@click.option("--base", "--path", help="Base path of toolchain", default=Toolchain.base())
+@click.option("--alias", "--nickname", help="Toolchain alias", default="esp32arloc")
+@click.option("--manufacturer", "--brand", help="Manufacturer name", default="espressif")
+@click.option("--processor", "--cpu", help="Processor type", default="esp32")
+@click.option("--os", "os_", help="Operating system name", default="arduino")
+@click.option("--version", help="Toolchain version", default=LATEST_ARDUINO_ESP32_TOOLCHAIN_VERSION)
+@click.option("--verbose", "-v", help="Verbose", is_flag=True, default=False)
+@click.option("--location", help="Install location type",
+              type=click.Choice(["local", "local_container", "cloud_container", "cloud_server"], case_sensitive=False),
+              default="local")
+def install(base, alias, manufacturer, processor, os_, version, verbose, location):
+    """
+    Install the toolchain given manufacturer, processor, OS, and version.
+    Defaults to the DevKit: Espressif:ESP32:Arduino:latest:Local.
+
+    Location can be:
+      - **local** or **native**: Installed on the local filesystem.
+      - **local_container**: Installed as a pre-built Docker image. Assumes Docker desktop is installed.
+      - **cloud_container**: Installed in your own account as an ECS container.
+      - **cloud_server**: Pointed at a centralized development Cloud installation.
+
+    Currently only the local option is supported.
+
+    The `--team` parameter needs to be specified since we keep track of installations
+    inside the local team configuration file.
+
+    If a toolchain is already installed, this command will fail. To reinstall you have to first
+    uninstall that version and then reinstall it using this command.
+
+    \f
+    Example:
+
+    $ not toolchain install
+    """
+    global toolchain_class
+    try:
+        location_enum = ToolchainLocation(location)
+        if verbose:
+            print(f"+ Location: {location_enum}")
+
+        from yaspin import yaspin
+
+        with yaspin(text="Installing... ", color="green") as spinner:
+            if alias:
+                if verbose:
+                    print(f"+ Installing for alias {alias} to base {base}")
+
+                result = toolchain_class.install_alias(base, alias, verbose)
+                if verbose:
+                    print(f"+Install result: {result}")
+                # print(f"Install result: {result}")
+            else:
+                print(f"+ Install base: {base} mfr: {manufacturer} proc: {processor} os: {os_} vers: {version}, loc: {location_enum}")
+                toolchain_class.install(base, manufacturer, processor, os_, version, location_enum, verbose)
+
+            if verbose:
+                print("Resetting toolchain...")
+            toolchain_class.reset(base, manufacturer, processor, os_, version, location_enum, verbose)
+            spinner.ok("✅ ")
+
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+
+
+@toolchain.command()
+@click.option("--base", "--path", help="Base path of toolchain", default=Toolchain.base())
+@click.option("--alias", "--nickname", help="Toolchain alias", default=None)
+@click.option("--manufacturer", "--brand", help="Manufacturer name", default="espressif")
+@click.option("--processor", "--cpu", help="Processor type", default="esp32")
+@click.option("--os", "os_", help="Operating system name", default="arduino")
+@click.option("--version", help="Toolchain current version", default=None)
+@click.option("--to", help="Toolchain desired version", default=None)
+@click.option("--verbose", "-v", help="Verbose", is_flag=True, default=False)
+def update(base, alias, manufacturer, processor, os_, version, to, verbose):
+    """
+    Update installed toolchain to given version.
+    """
+    global toolchain_class
+    try:
+        print("Not available yet.")
+
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+
+
+@toolchain.command()
+@click.option("--base", "--path", help="Base path of toolchain", default=Toolchain.base())
+@click.option("--alias", "--nickname", help="Toolchain alias")
+@click.option("--manufacturer", "--brand", help="Maunfacturer name", default="espressif")
+@click.option("--processor", "--cpu", help="Processor type", default="esp32")
+@click.option("--os", "os_", help="Operating system name", default="arduino")
+@click.option("--version", help="Toolchain current version", default=LATEST_ARDUINO_ESP32_TOOLCHAIN_VERSION)
+@click.option("--location", help="Install location type",
+              type=click.Choice(["local", "local_container", "cloud_container", "cloud_server"], case_sensitive=False),
+              default="local")
+@click.option("--verbose", "-v", help="Verbose", is_flag=True, default=False)
+def uninstall(base, alias, manufacturer, processor, os_, version, location, verbose):
+    """
+    Remove installed toolchain.
+    """
+    global toolchain_class
+    try:
+        location_enum = ToolchainLocation(location)
+
+        if alias:
+            result = toolchain_class.uninstall(base, alias, verbose)
+        else:
+            result = toolchain_class.uninstall(base, manufacturer, processor, os_, version, location_enum, verbose)
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+
+#
+# NOTE: the version number must match the default version of the toolchain installed for the platform.
+# If it isn't installed, we'll have to go find it from a central registry. For now, however, we default to
+# the current latest version.
+#
+
+@toolchain.command()
+@click.option("--base", "--path", help="Base path of toolchain", default=Toolchain.base())
+@click.option("--manufacturer", "--brand", help="Manufacturer name", default="espressif")
+@click.option("--processor", "--cpu", help="Processor type", default="esp32")
+@click.option("--os", "os_", help="Operating system name", default="arduino")
+@click.option("--version", help="Toolchain current version", default=LATEST_ARDUINO_ESP32_TOOLCHAIN_VERSION)
+@click.option("--location", help="Install location type",
+              type=click.Choice(["local", "local_container", "cloud_container", "cloud_server"], case_sensitive=False),
+              default="local")
+@click.option("--verbose", "-v", help="Verbose", is_flag=True, default=False)
+def reset(base, manufacturer, processor, os_, version, location, verbose):
+    """
+    Remove installed toolchain.
+    """
+    global toolchain_class
+    try:
+        location_enum = ToolchainLocation(location)
+        result = toolchain_class.reset(base, manufacturer, processor, os_, version, location_enum, verbose)
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+
+
+@toolchain.command()
+@click.option("--manufacturer", "--brand", help="Maunfacturer name", default="espressif")
+@click.option("--processor", "--cpu", help="Processor type", default="esp32")
+@click.option("--os", "os_", help="Operating system name", default="arduino")
+def list(manufacturer, processor, os_):
+    """
+    List available and/or installed toolchains on this machine. This is loaded out of the
+    .simpleiot/toolchain.json file.
+    """
+    global toolchain_class
+    try:
+        print("Not Implemented. Check back soon.")
+
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+
+
+@toolchain.command()
+def available():
+    """
+    List available and/or installed toolchains.
+
+    This scans through the list of registered toolchain subclasses and shows what can be
+    installed.
+    """
+    global toolchain_class
+    try:
+        toolchain_list = toolchain_class.list_available()
+        table = Table(show_header=True, header_style="green")
+        table.add_column("Alias", style="bold", overflow="flow")
+        table.add_column("Name", style="bold", overflow="flow")
+        table.add_column("Mfr", style="bold", overflow="flow")
+        table.add_column("Proc", style="bold", overflow="flow")
+        table.add_column("OS", style="bold", overflow="flow")
+        table.add_column("Version", style="bold", overflow="flow")
+        table.add_column("Location", style="bold", overflow="flow")
+
+        for _, tool in toolchain_list.items():
+            table.add_row(tool.alias,
+                          tool.name,
+                          tool.manufacturer,
+                          tool.processor,
+                          tool.opsys,
+                          tool.version,
+                          tool.location.name)
+
+        console.print(table)
+    except Exception as e:
+        print(f"ERROR: {str(e)}")
+
