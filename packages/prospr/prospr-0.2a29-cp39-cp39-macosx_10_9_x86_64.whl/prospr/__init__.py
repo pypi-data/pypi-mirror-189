2d21c2765fdd5013373c0a788199bd78eff25087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:21 +0000

    Add package prospr/0.2a29/prospr-0.2a29-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/prospr/prospr-0.2a29-cp39-cp39-macosx_10_9_x86_64.whl/prospr/__init__.py
@@ -0,0 +1,17 @@
+from prospr_core import AminoAcid, Protein, depth_first, depth_first_bnb
+from .datasets import load_vanEck250, load_vanEck1000, load_vanEck_hratio
+from .visualize import plot_protein
+
+__version__ = "0.2a29"
+
+__all__ = [
+    "__version__",
+    "AminoAcid",
+    "Protein",
+    "depth_first",
+    "depth_first_bnb",
+    "load_vanEck250",
+    "load_vanEck1000",
+    "load_vanEck_hratio",
+    "plot_protein",
+]
