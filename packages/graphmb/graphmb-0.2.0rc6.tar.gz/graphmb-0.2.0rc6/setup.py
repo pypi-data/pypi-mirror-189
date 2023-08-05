b762654cfb713e866a72f7fd2591f08383388b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:20 +0000

    Add package graphmb/0.2.0rc6/graphmb-0.2.0rc6.tar.gz

---
+++ packages/graphmb/graphmb-0.2.0rc6.tar.gz/graphmb-0.2.0rc6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import subprocess
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path("src/graphmb/version.py")
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setup(
+    name="graphmb",
+    version=main_ns["__version__"],
+    packages=["graphmb"],
+    python_requires=">=3.8",
+    package_dir={"": "src"},
+    setup_requires=["setuptools~=58.0", "wheel", "sphinx-rtd-theme", "twine"],
+    install_requires=[
+        "wheel",
+        "requests",
+        "networkx==2.6.2",
+        "torch==1.13.1",
+        "pandas==1.3.5",
+        "tensorflow==2.11.0",
+        "tqdm==4.61.2",
+        "numpy==1.23.5",
+        "mlflow==2.1.1",
+        "importlib_resources"
+
+    ],
+    entry_points={
+        "console_scripts": ["graphmb=graphmb.main:main"],
+    },
+    include_package_data=True,
+)
