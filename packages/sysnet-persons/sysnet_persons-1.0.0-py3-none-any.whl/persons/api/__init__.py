cc37319a6b74b8e5369c3e5e022a76c85112d0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:30 +0000

    Add package sysnet-persons/1.0.0/sysnet_persons-1.0.0-py3-none-any.whl

---
+++ packages/sysnet-persons/sysnet_persons-1.0.0-py3-none-any.whl/persons/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from persons.api.admins_api import AdminsApi
+from persons.api.developers_api import DevelopersApi
+from persons.api.public_api import PublicApi
