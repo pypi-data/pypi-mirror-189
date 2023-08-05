75091523a1a645d89a591b6265c0f7693ab5d9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:31 +0000

    Add package vizzy/0.1.0/vizzy-0.1.0.tar.gz

---
+++ packages/vizzy/vizzy-0.1.0.tar.gz/vizzy-0.1.0/vizzy/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from . import vizzy_doc
+from . import vizzy_sentence
+from . import vizzy_token
+
+__all__ = [
+    'vizzy_doc',
+    'vizzy_sentence',
+    'vizzy_token'
+]
+
