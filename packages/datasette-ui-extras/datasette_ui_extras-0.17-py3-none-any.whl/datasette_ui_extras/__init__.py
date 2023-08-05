f4f9f39e99e1e0425e2e57d155d680dc9a132de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package datasette-ui-extras/0.17/datasette_ui_extras-0.17-py3-none-any.whl

---
+++ packages/datasette-ui-extras/datasette_ui_extras-0.17-py3-none-any.whl/datasette_ui_extras/__init__.py
@@ -0,0 +1,83 @@
+import json
+from datasette import hookimpl
+import markupsafe
+from .facets import enable_yolo_facets, facets_extra_body_script
+from .filters import enable_yolo_arraycontains_filter, enable_yolo_exact_filter, yolo_filters_from_request
+from .new_facets import StatsFacet, YearFacet, YearMonthFacet
+
+PLUGIN = 'datasette-ui-extras'
+
+# Not fully fleshed out: datasette-ui-extras consists of a bunch of "extras".
+# Each extra has one or more of: custom CSS, custom JS, custom Python.
+#
+# Try to develop them as though they're standalone things, so they can be
+# easily turned on/off, or upstreamed into Datasette.
+#
+# TODO: serve a minified/concatenated CSS/JS file
+@hookimpl
+def extra_css_urls(datasette):
+    return [
+        datasette.urls.static_plugins(PLUGIN, "app.css"),
+        datasette.urls.static_plugins(PLUGIN, "hide-export.css"),
+        datasette.urls.static_plugins(PLUGIN, "hide-table-definition.css"),
+        datasette.urls.static_plugins(PLUGIN, "sticky-table-headers.css"),
+        datasette.urls.static_plugins(PLUGIN, "lazy-facets.css"),
+        datasette.urls.static_plugins(PLUGIN, "hide-filters.css"),
+        datasette.urls.static_plugins(PLUGIN, "layout-row-page.css"),
+        datasette.urls.static_plugins(PLUGIN, "compact-cogs.css"),
+        datasette.urls.static_plugins(PLUGIN, "mobile-column-menu.css"),
+    ]
+
+@hookimpl
+def extra_js_urls(datasette):
+    return [
+        datasette.urls.static_plugins(PLUGIN, 'hide-filters.js'),
+        datasette.urls.static_plugins(PLUGIN, "sticky-table-headers.js"),
+        datasette.urls.static_plugins(PLUGIN, "focus-search-box.js"),
+        datasette.urls.static_plugins(PLUGIN, 'lazy-facets.js'),
+        datasette.urls.static_plugins(PLUGIN, 'layout-row-page.js'),
+        datasette.urls.static_plugins(PLUGIN, "mobile-column-menu.js"),
+    ]
+
+@hookimpl
+def render_cell(value):
+    if isinstance(value, str) and (value == '[]' or (value.startswith('["') and value.endswith('"]'))):
+        try:
+            tags = json.loads(value)
+            rv = ''
+
+            for i, tag in enumerate(tags):
+                if i > 0:
+                    rv += ', '
+                rv += markupsafe.Markup(
+                    '<span>{tag}</span>'.format(
+                        tag=markupsafe.escape(tag)
+                    )
+                )
+
+            return rv
+        except:
+            pass
+    return None
+
+@hookimpl
+def extra_body_script(template, database, table, columns, view_name, request, datasette):
+    return facets_extra_body_script(template, database, table, columns, view_name, request, datasette)
+
+@hookimpl
+def startup():
+    enable_yolo_facets()
+    enable_yolo_arraycontains_filter()
+    enable_yolo_exact_filter()
+
+@hookimpl
+def register_facet_classes():
+    return [StatsFacet, YearFacet, YearMonthFacet]
+
+@hookimpl
+def filters_from_request(request, database, table, datasette):
+    async def dothething():
+        return await yolo_filters_from_request(request, database, table, datasette)
+
+    return dothething
+
