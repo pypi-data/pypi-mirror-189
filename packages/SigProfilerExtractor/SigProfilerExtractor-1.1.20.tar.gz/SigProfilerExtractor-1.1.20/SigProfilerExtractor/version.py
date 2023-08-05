8023880e61940cd649d99d8aa4e3bac41e813159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package SigProfilerExtractor/1.1.20/SigProfilerExtractor-1.1.20.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.20.tar.gz/SigProfilerExtractor-1.1.20/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.20'
+version = '1.1.20'
+Update = 'Upgrade v1.1.20: Resolve naming issue with SV/CNV All_Solutions directory.'
+    
+    
