eac1b0ca51196176f065974cf88f4929bf37c136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:53 +0000

    Add package Shuvo/0.3/Shuvo-0.3.tar.gz

---
+++ packages/Shuvo/Shuvo-0.3.tar.gz/Shuvo-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="Shuvo",
+version='0.3',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['Shuvo'],
+install_requires=[])
