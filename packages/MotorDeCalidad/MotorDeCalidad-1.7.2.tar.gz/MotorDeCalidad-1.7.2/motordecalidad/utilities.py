c6627694cd7193e3f53231eea67c9cc1c8619238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:21 +0000

    Add package MotorDeCalidad/1.7.2/MotorDeCalidad-1.7.2.tar.gz

---
+++ packages/MotorDeCalidad/MotorDeCalidad-1.7.2.tar.gz/MotorDeCalidad-1.7.2/motordecalidad/utilities.py
@@ -0,0 +1,33 @@
+from motordecalidad.constants import *
+from pyspark.sql import DataFrame
+def send_email(receiver_email):
+    import smtplib, ssl
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "motordecalidadtlfhispan@gmail.com"
+    password = "xjhnrhsnxlcnwpdi"
+    message = """\
+    Subject: Reporte de Calidad
+    Se han encontrado errores que superan los limites establecidos en la validacion de calidad"""
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+#Function to define the dbutils library from Azure Databricks
+def get_dbutils(spark):
+        try:
+            from pyspark.dbutils import DBUtils
+            dbutils = DBUtils(spark)
+        except ImportError:
+            import IPython
+            dbutils = IPython.get_ipython().user_ns["dbutils"]
+        return dbutils
+def applyFilter(object:DataFrame, filtered) :
+    try:
+        filteredColumn = filtered.get(JsonParts.Fields)
+        filterValue = filtered.get(JsonParts.Values)
+        print("Extracción de parametros de filtrado finalizada")
+        return object.filter(col(filteredColumn)==filterValue)
+    except:
+        print("Se omite filtro")
+        return object
