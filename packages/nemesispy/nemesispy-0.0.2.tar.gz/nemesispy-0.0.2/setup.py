25fa904a9985adf6687da9b84732d4e1f8a72081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:14:27 +0000

    Add package nemesispy/0.0.2/nemesispy-0.0.2.tar.gz

---
+++ packages/nemesispy/nemesispy-0.0.2.tar.gz/nemesispy-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.2"
+DESCRIPTION = "Tools for modelling exoplanet spectra"
+setup(name="nemesispy",
+      version=VERSION,
+      description=DESCRIPTION,
+      url='https://github.com/Jingxuan97/nemesispy2022',
+      author="Jingxuan Yang",
+      author_email="jingxuanyang15@gmail.com",
+      packages=find_packages(),
+      install_requires=[
+          "numpy",
+          "scipy",
+          "matplotlib",
+          "numba"]
+        )
