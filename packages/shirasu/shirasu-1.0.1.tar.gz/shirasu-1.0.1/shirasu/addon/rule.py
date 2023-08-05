e7c7da5a5bc161ae0331876bcb0bc81a4ea652ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:01 +0000

    Add package shirasu/1.0.1/shirasu-1.0.1.tar.gz

---
+++ packages/shirasu/shirasu-1.0.1.tar.gz/shirasu-1.0.1/shirasu/addon/rule.py
@@ -0,0 +1,113 @@
+import asyncio
+import re
+from typing import cast, Union, Callable, Awaitable
+
+from ..di import di
+from ..event import Event, MessageEvent, NoticeEvent
+from ..config import GlobalConfig
+
+
+class Rule:
+    """
+    The rule to match whether the message should be applied to current addon.
+    Note: the handler will be injected automatically.
+    """
+
+    def __init__(self, handler: Callable[..., Awaitable[bool] | bool]):
+        self._handler = di.inject(handler, sync=not asyncio.iscoroutinefunction(handler))  # type: ignore
+
+    def __or__(self, rule: 'Rule') -> 'Rule':
+        async def handler() -> bool:
+            if await self.match():
+                return True
+            return await rule.match()
+        return Rule(handler)
+
+    def __and__(self, rule: 'Rule') -> 'Rule':
+        async def handler() -> bool:
+            if not await self.match():
+                return False
+            return await rule.match()
+        return Rule(handler)
+
+    async def match(self) -> bool:
+        """
+        Runs the matcher of this rule.
+        :return: whether or not matched.
+        """
+
+        return await self._handler()
+
+
+def message() -> Rule:
+    """
+    The rule to match message events.
+    :return: the rule.
+    """
+
+    def handler(event: Event) -> bool:
+        return event.post_type == 'message'
+    return Rule(handler)
+
+
+def command(cmd: str) -> Rule:
+    """
+    The rule to match certain command.
+    :param cmd: the command.
+    :return: the rule.
+    """
+
+    def handler(event: MessageEvent, global_config: GlobalConfig) -> bool:
+        return event.match_command(cmd, global_config.command_prefixes)
+
+    return message() & Rule(handler)
+
+
+def regex(r: Union[str, re.Pattern]) -> Rule:
+    """
+    The rule to match certain regex.
+    :param r: regex, whether or not compiled.
+    :return: the rule.
+    """
+
+    if isinstance(r, str):
+        r = re.compile(r)
+
+    def handler(event: MessageEvent) -> bool:
+        return bool(r.match(event.message.plain_text))  # type: ignore
+
+    return message() & Rule(handler)
+
+
+def tome() -> Rule:
+    """
+    The rule to match whether the event is to the bot.
+    :return: the rule.
+    """
+
+    def handler(event: MessageEvent) -> bool:
+        if event.message_type == 'private':
+            return True
+
+        if not (segments := event.message.segments):
+            return False
+
+        seg = segments[0]
+        return seg.type == 'at' and seg.data.get('qq') == event.self_id
+
+    return message() & Rule(handler)
+
+
+def notice(notice_type: str) -> Rule:
+    """
+    The rule to match certain notice.
+    :param notice_type: the notice type.
+    :return the rule.
+    """
+
+    def handler(event: Event) -> bool:
+        if event.post_type != 'notice':
+            return False
+        return cast(NoticeEvent, event).notice_type == notice_type
+
+    return Rule(handler)
---
+++ packages/shirasu/shirasu-1.0.1.tar.gz/shirasu-1.0.1/shirasu/client/onebot.py
@@ -0,0 +1,119 @@
+import ujson
+import asyncio
+
+from pathlib import Path
+from typing import Any, Literal
+from websockets.exceptions import ConnectionClosedError
+from websockets.legacy.client import connect, WebSocketClientProtocol
+
+from .client import Client, ClientActionError
+from ..addon import AddonPool
+from ..config import load_config, GlobalConfig
+from ..logger import logger
+from ..util import FutureTable, retry
+from ..event import MessageEvent, NoticeEvent, RequestEvent
+from ..message import Message
+
+
+class OneBotClient(Client):
+    def __init__(self, ws: WebSocketClientProtocol, pool: AddonPool, global_config: GlobalConfig):
+        super().__init__(pool, global_config)
+        self._ws = ws
+        self._futures = FutureTable()
+        self._tasks: set[asyncio.Task] = set()
+
+    async def _call_action(self, action: str, **params: Any) -> dict[str, Any]:
+        """
+        Calls action via websocket.
+        :param action: the action.
+        :param params: the params to call the action.
+        :return: the action result.
+        """
+
+        logger.info(f'Calling action {action}.')
+        future_id = self._futures.register()
+        await self._ws.send(ujson.dumps({
+            'action': action,
+            'params': params,
+            'echo': future_id,
+        }))
+
+        data = await self._futures.get(future_id, self._global_config.action_timeout)
+        if data.get('status') == 'failed':
+            raise ClientActionError(data)
+
+        return data.get('data', {})
+
+    async def _handle(self, data: dict[str, Any]) -> None:
+        if echo := data.get('echo'):
+            self._futures.set(int(echo), data)
+            return
+
+        post_type = data.get('post_type')
+        if post_type == 'meta_event':
+            return
+
+        logger.info(f'Received event {data}')
+
+        self.curr_event = None
+        if post_type == 'message':
+            self.curr_event = MessageEvent.from_data(data)
+        elif post_type == 'request':
+            self.curr_event = RequestEvent.from_data(data)
+        elif post_type == 'notice':
+            self.curr_event = NoticeEvent.from_data(data)
+        else:
+            logger.warning(f'Ignoring unknown event {post_type}.')
+            return
+
+        await self.apply_addons()
+
+    async def _do_listen(self) -> None:
+        if count := len(self._tasks):
+            logger.warning(f'Canceling {count} undone tasks')
+            for task in self._tasks:
+                task.cancel()
+            self._tasks.clear()
+
+        async for message in self._ws:
+            if isinstance(message, bytes):
+                message = message.decode('utf8')
+            task = asyncio.create_task(self._handle(ujson.loads(message)))
+            self._tasks.add(task)
+            task.add_done_callback(self._tasks.discard)
+
+    @classmethod
+    @retry(timeout=5., messages={
+        ConnectionClosedError: 'Connection closed',
+        ConnectionRefusedError: 'Connection refused',
+    })
+    async def listen(cls, *, pool: AddonPool, config: str | Path = 'shirasu.yml') -> None:
+        """
+        Start listening the websocket url.
+        :param pool: the addon pool, which can be used to preload plugins.
+        :param config: the path to config file.
+        """
+
+        conf = load_config(config)
+        async with connect(conf.ws) as ws:
+            logger.success('Connected to websocket.')
+            await cls(ws, pool, conf)._do_listen()
+
+    async def send_msg(
+            self,
+            *,
+            message_type: Literal['private', 'group'],
+            user_id: int,
+            group_id: int | None,
+            message: Message,
+            is_rejected: bool,
+    ) -> int:
+        res = await self._call_action(
+            action='send_msg',
+            message=message.to_json_obj(),
+            user_id=user_id,
+            group_id=group_id,
+            message_type=message_type,
+            is_rejected=is_rejected,
+        )
+        return res['message_id']
