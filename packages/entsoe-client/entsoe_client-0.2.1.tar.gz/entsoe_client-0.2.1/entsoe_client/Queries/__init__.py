01d11f52e99f28afb7291d61b1c4d04a868a6d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:48 +0000

    Add package entsoe-client/0.2.1/entsoe_client-0.2.1.tar.gz

---
+++ packages/entsoe-client/entsoe_client-0.2.1.tar.gz/entsoe_client-0.2.1/entsoe_client/Queries/__init__.py
@@ -0,0 +1,12 @@
+from . import (Balancing, Congestion, Generation, Load, MasterData, Outages,
+               Transmission)
+
+__all__ = [
+    "Balancing",
+    "Congestion",
+    "Generation",
+    "Load",
+    "MasterData",
+    "Outages",
+    "Transmission",
+]
