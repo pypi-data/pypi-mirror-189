349afcd7e6b6cb3d0efd3af7539ca94690bb6d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:53 +0000

    Add package pysensu-yelp/1.0.3/pysensu-yelp-1.0.3.tar.gz

---
+++ packages/pysensu-yelp/pysensu-yelp-1.0.3.tar.gz/pysensu-yelp-1.0.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='pysensu-yelp',
+    version='1.0.3',
+    provides=['pysensu_yelp'],
+    description='Emits Yelp-flavored Sensu events to a Sensu Client',
+    url='https://github.com/Yelp/pysensu-yelp',
+    author='Yelp Operations Team',
+    author_email='operations@yelp.com',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7',
+         'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
+    package_data={
+        'pysensu_yelp': ['py.typed'],
+    },
+    license='Apache License 2.0',
+)
