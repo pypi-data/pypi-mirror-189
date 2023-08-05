f32e3c844292b5f10a38e9b4be2a47fab38996d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package netools/0.0.2/netools-0.0.2.tar.gz

---
+++ packages/netools/netools-0.0.2.tar.gz/netools-0.0.2/netools/__init__.py
@@ -0,0 +1,12 @@
+from ne.hello import Hello
+# from ne.streaming import CameraClient
+# from ne.streaming import VideoClient
+# from ne.streaming import ScreenShareClient
+# from ne.audio import AudioSender
+# from ne.audio import AudioReceiver
+
+
+
+
+
+
---
+++ packages/netools/netools-0.0.2.tar.gz/netools-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Prints Hello'
+LONG_DESCRIPTION = 'A package that allows to print hello.'
+
+# Setting up
+setup(
+    name="netools",
+    version=VERSION,
+    author="Nelson Wang",
+    author_email="<wangnelson2@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'utilities'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
