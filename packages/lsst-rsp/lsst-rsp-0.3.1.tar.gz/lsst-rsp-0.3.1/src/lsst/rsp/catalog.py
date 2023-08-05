e798a5ff8b216a2c847cfb55587236accfed0d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:06 +0000

    Add package lsst-rsp/0.3.1/lsst-rsp-0.3.1.tar.gz

---
+++ packages/lsst-rsp/lsst-rsp-0.3.1.tar.gz/lsst-rsp-0.3.1/src/lsst/rsp/catalog.py
@@ -0,0 +1,57 @@
+import os
+from typing import Optional
+
+import pyvo
+import pyvo.auth.authsession
+import requests
+from deprecated import deprecated
+
+from .utils import get_access_token
+
+
+def _get_tap_url() -> str:
+    tapurl = os.getenv("EXTERNAL_TAP_URL")
+    if not tapurl:
+        tapurl = (os.getenv("EXTERNAL_INSTANCE_URL") or "") + (
+            os.getenv("TAP_ROUTE") or "/api/tap"
+        )
+    return tapurl
+
+
+def _get_datalink_url() -> str:
+    return os.getenv("EXTERNAL_INSTANCE_URL", "") + "/api/datalink"
+
+
+def _get_cutout_url() -> str:
+    return os.getenv("EXTERNAL_INSTANCE_URL", "") + "/api/cutout"
+
+
+def _get_auth() -> Optional[pyvo.auth.authsession.AuthSession]:
+    tap_url = _get_tap_url()
+    s = requests.Session()
+    tok = get_access_token()
+    if not tok:
+        return None
+    s.headers["Authorization"] = "Bearer " + tok
+    auth = pyvo.auth.authsession.AuthSession()
+    auth.credentials.set("lsst-token", s)
+    auth.add_security_method_for_url(_get_datalink_url(), "lsst-token")
+    auth.add_security_method_for_url(_get_cutout_url(), "lsst-token")
+    auth.add_security_method_for_url(tap_url, "lsst-token")
+    auth.add_security_method_for_url(tap_url + "/sync", "lsst-token")
+    auth.add_security_method_for_url(tap_url + "/async", "lsst-token")
+    auth.add_security_method_for_url(tap_url + "/tables", "lsst-token")
+    return auth
+
+
+def get_tap_service() -> pyvo.dal.TAPService:
+    return pyvo.dal.TAPService(_get_tap_url(), _get_auth())
+
+
+@deprecated(reason="Please use get_tap_service()")
+def get_catalog() -> pyvo.dal.TAPService:
+    return get_tap_service()
+
+
+def retrieve_query(query_url: str) -> pyvo.dal.AsyncTAPJob:
+    return pyvo.dal.AsyncTAPJob(query_url, _get_auth())
