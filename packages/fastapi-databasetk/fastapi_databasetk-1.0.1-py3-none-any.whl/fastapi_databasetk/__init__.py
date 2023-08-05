194a851f21ea7901f4a277f37ab8122150552c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:11 +0000

    Add package fastapi-databasetk/1.0.1/fastapi_databasetk-1.0.1-py3-none-any.whl

---
+++ packages/fastapi-databasetk/fastapi_databasetk-1.0.1-py3-none-any.whl/fastapi_databasetk/__init__.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from .objects.database_async import AsyncDatabase
+from .objects.database_sync import SyncDatabase
+from .objects.config import DatabaseConfig
+
+
+def init_db(
+    url: str,
+    min_size: int = 2,
+    max_size: int = 20,
+    is_async: bool = False,
+    db_config: Optional[DatabaseConfig] = None,
+) -> (AsyncDatabase or SyncDatabase):
+    config = (
+        DatabaseConfig(url=url, min_size=min_size, max_size=max_size)
+        if db_config is None
+        else db_config
+    )
+    return (AsyncDatabase if is_async else SyncDatabase)(config=config)
