17fbda05a7b4d7a67209063e795b7ee13ff41c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:48 +0000

    Add package imodelsx/0.8/imodelsx-0.8.tar.gz

---
+++ packages/imodelsx/imodelsx-0.8.tar.gz/imodelsx-0.8/imodelsx/iprompt/data.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+
+def get_add_two_numbers_dataset(num_examples: int= None):
+    df = pd.read_csv(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'add_two.csv'
+    ), delimiter=',')
+    df['output_strings'] = df['output_strings'].str.replace("'", "")
+    if num_examples is not None:
+        df = df.sample(n=num_examples)
+    return df['input_strings'].values, [v.replace('\\n', '\n') for v in df['output_strings'].values]
