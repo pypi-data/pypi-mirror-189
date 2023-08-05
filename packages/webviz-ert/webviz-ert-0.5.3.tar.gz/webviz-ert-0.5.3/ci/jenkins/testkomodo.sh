d2be2afe3575f880f8fd7538a48c029f08217415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:52 +0000

    Add package webviz-ert/0.5.3/webviz-ert-0.5.3.tar.gz

---
+++ packages/webviz-ert/webviz-ert-0.5.3.tar.gz/webviz-ert-0.5.3/ci/jenkins/testkomodo.sh
@@ -0,0 +1,39 @@
+
+copy_test_files () {
+    cp -r $CI_SOURCE_ROOT/tests $CI_TEST_ROOT/tests
+}
+
+install_package () {
+    pip install . --no-deps
+}
+
+start_tests () {
+    start_integration_test
+    test_result=$?
+    if [ "$test_result" -gt 0 ];
+    then
+        exit $test_result
+    fi
+    pytest -vs -k "not spe1"
+}
+
+start_integration_test () {
+
+    echo "Downloading chromedriver ..."
+    wget -O chromedriver.zip https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_linux64.zip
+    unzip chromedriver.zip chromedriver -d ../test-kenv/root/bin
+
+    pip install pytest selenium dash[testing]
+
+    pushd tests/data/spe1_st
+
+    echo "Initiating Ert run for Spe1 with new storage enabled..."
+
+    ert ensemble_experiment --enable-new-storage spe1.ert
+
+    echo "Ert ensemble_esmeble experiment run finished"
+
+    echo "Test for webviz-ert plugins ..."
+    pytest ../../../ -vs -k spe1
+    popd
+}
---
+++ packages/webviz-ert/webviz-ert-0.5.3.tar.gz/webviz-ert-0.5.3/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+TESTS_REQUIRE = [
+    "mypy",
+    "bandit",
+    "black",
+    "dash[testing]",
+    "pylint",
+    "pytest-mock",
+    "selenium~=3.141",
+]
+
+setup(
+    name="webviz-ert",
+    description="Webviz plugins for ERT",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Equinor",
+    packages=find_packages(exclude=["tests"]),
+    package_data={"webviz_ert.assets": ["*"]},
+    include_package_data=True,
+    entry_points={
+        "webviz_config_plugins": [
+            "ResponseComparison = webviz_ert.plugins:ResponseComparison",
+            "ObservationAnalyzer = webviz_ert.plugins:ObservationAnalyzer",
+            "ParameterComparison = webviz_ert.plugins:ParameterComparison",
+            "ResponseCorrelation = webviz_ert.plugins:ResponseCorrelation",
+        ],
+    },
+    install_requires=[
+        "dash-bootstrap-components",
+        "dash-daq",
+        "requests",
+        "webviz-config>=0.0.40",
+        "webviz-config-equinor",
+        "webviz-subsurface-components",
+    ],
+    tests_require=TESTS_REQUIRE,
+    extras_require={"tests": TESTS_REQUIRE},
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
+    zip_safe=False,
+    classifiers=[
+        "Natural Language :: English",
+        "Environment :: Web Environment",
+        "Framework :: Dash",
+        "Framework :: Flask",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
