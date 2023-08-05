ef642b2c7aa3848e74e23ecfe5d7b819773baf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:34 +0000

    Add package py-cwru/1.0.2/py_cwru-1.0.2.tar.gz

---
+++ packages/py-cwru/py_cwru-1.0.2.tar.gz/py_cwru-1.0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='py_cwru',
+    version='1.0.2',
+    author='Xu Fangzheng',
+    author_email='fonderxu@163.com',
+    url='',
+    description=u'README.md',
+    packages=['py_cwru'],
+    install_requires=[],
+    entry_points={
+        'console_scripts': []
+    },
+    readme="README.md",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
+
