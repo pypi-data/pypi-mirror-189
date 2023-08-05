bf2b8c8ba7d10d361f48710104213c0d8e50f75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:45 +0000

    Add package riyazi/0.2/riyazi-0.2.tar.gz

---
+++ packages/riyazi/riyazi-0.2.tar.gz/riyazi-0.2/riyazi/__init__.py
@@ -0,0 +1,12 @@
+# Version Import
+from .version import*
+from . import version
+from .version import __version__ , __author__ 
+
+# Math module Import 
+from . math import*
+from . import math 
+
+
+
+
