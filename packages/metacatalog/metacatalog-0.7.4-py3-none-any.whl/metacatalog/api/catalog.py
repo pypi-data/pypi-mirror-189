e6f5266d9a25a6275a3082e0b5b62a016c949880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:02 +0000

    Add package metacatalog/0.7.4/metacatalog-0.7.4-py3-none-any.whl

---
+++ packages/metacatalog/metacatalog-0.7.4-py3-none-any.whl/metacatalog/api/catalog.py
@@ -0,0 +1,53 @@
+"""CATALOG API
+
+The catalog API offers application wide endpoints that are not bound to a
+specific API action or model
+
+"""
+from sqlalchemy.orm import Session
+from sqlalchemy.orm.exc import NoResultFound
+
+from metacatalog import api
+from metacatalog.util.logging import get_logger
+
+def get_uuid(session: Session, uuid: str, not_found='raise'):
+    """
+    Return the Metacatalog object of given
+    version 4 UUID. The supported objects are:
+
+    - Entry
+    - EntryGroup
+    - Keyword
+    - Person
+
+    .. versionadded:: 0.1.13
+
+    .. versionchanged:: 0.2.7
+        Now, also :class:`Persons <metacatalog.model.Person` can be
+        found by UUID
+
+    """
+    # check if an Entry exists
+    entry = api.find_entry(session, uuid=uuid)
+    if entry is not None:
+        return entry
+
+    # check if Entrygroup exists
+    group = api.find_group(session, uuid=uuid)
+    if group is not None:
+        return group
+
+    # check if a Person exists
+    person = api.find_person(session, uuid=uuid)
+    if person is not None:
+        return person
+
+    # handle keyword
+    keyword = api.find_keyword(session, uuid=uuid)
+    if keyword is not None:
+        return keyword
+
+    if not_found == 'raise':
+        raise NoResultFound("The UUID='%s' was not found." % uuid)
+    else:
+        return None
