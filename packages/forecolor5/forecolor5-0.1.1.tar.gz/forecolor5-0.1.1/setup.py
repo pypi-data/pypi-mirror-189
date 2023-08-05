a2f2b954a1351713a6ac106e7932feb4fe95cd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:13 +0000

    Add package forecolor5/0.1.1/forecolor5-0.1.1.tar.gz

---
+++ packages/forecolor5/forecolor5-0.1.1.tar.gz/forecolor5-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+
+setup(
+    name="forecolor5",
+    version='0.1.1',
+    license='Eclipse Public License 2.0',
+    authors=["billythegoat356", "loTus01", "BlueRed"],
+    author_email="<billythegoat356@gmail.com>",
+    description="by billythegoat356, loTus01 and BlueRed",
+    long_description='Documentation: https://github.com/billythegoat356/pystyle',
+    keywords=['cli', 'fade', 'colors', 'terminal', 'tui'],
+    packages=['forecolor5']
+)
+
+
