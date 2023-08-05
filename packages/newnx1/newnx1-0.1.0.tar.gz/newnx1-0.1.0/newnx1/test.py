cb4d5ba132300b0626e3ec9f36e3423317781ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:02 +0000

    Add package newnx1/0.1.0/newnx1-0.1.0.tar.gz

---
+++ packages/newnx1/newnx1-0.1.0.tar.gz/newnx1-0.1.0/newnx1/test.py
@@ -0,0 +1,6 @@
+
+def get_print(string):
+    return f"my module print: {string}"
+
+def show_hello():
+    print("Hello World")
---
+++ packages/newnx1/newnx1-0.1.0.tar.gz/newnx1-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newnx1',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple example package',
+    url='https://github.com/yourusername/my_package',
+    install_requires=[
+    ],
+)
