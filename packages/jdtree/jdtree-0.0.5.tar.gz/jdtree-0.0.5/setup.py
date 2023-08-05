b4b9248cd28d6337c584543d0ac507c83764d6f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:02 +0000

    Add package jdtree/0.0.5/jdtree-0.0.5.tar.gz

---
+++ packages/jdtree/jdtree-0.0.5.tar.gz/jdtree-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='jdtree',
+    packages=['jdtree'],
+    version='0.0.5',
+    description='Json Decision Table/Tree',
+    author='Matteo Greco',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    long_description_content_type="text/markdown",
+    long_description='Json Decision Table/Tree',
+)
