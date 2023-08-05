4c320d5f8732387f804726747cb56360dd25ebb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package sdkit/1.0.32/sdkit-1.0.32.tar.gz

---
+++ packages/sdkit/sdkit-1.0.32.tar.gz/sdkit-1.0.32/sdkit/utils/__init__.py
@@ -0,0 +1,48 @@
+import logging
+
+log = logging.getLogger('sdkit')
+LOG_FORMAT = '%(asctime)s.%(msecs)03d %(levelname)s %(threadName)s %(message)s'
+logging.basicConfig(
+        level=logging.INFO,
+        format=LOG_FORMAT,
+        datefmt="%X"
+)
+
+from .file_utils import (
+    load_tensor_file,
+    save_tensor_file,
+    save_images,
+    save_dicts,
+)
+
+from .hash_utils import (
+    hash_bytes,
+    hash_url_quick,
+    hash_file_quick,
+)
+
+from .image_utils import (
+    img_to_base64_str,
+    img_to_buffer,
+    buffer_to_base64_str,
+    base64_str_to_buffer,
+    base64_str_to_img,
+    resize_img,
+    apply_color_profile,
+)
+
+from .latent_utils import (
+    img_to_tensor,
+    get_image_latent_and_mask,
+    latent_samples_to_images,
+)
+
+from .device_utils import (
+    gc,
+    get_device_usage,
+    print_largest_tensors_in_memory,
+)
+
+from .http_utils import (
+    download_file,
+)
