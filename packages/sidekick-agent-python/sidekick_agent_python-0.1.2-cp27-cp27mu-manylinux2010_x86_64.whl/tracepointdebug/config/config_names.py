63b1ac4e16e38a5e5fb0b437d900f343468959f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:13 +0000

    Add package sidekick-agent-python/0.1.2/sidekick_agent_python-0.1.2-cp27-cp27mu-manylinux2010_x86_64.whl

---
+++ packages/sidekick-agent-python/sidekick_agent_python-0.1.2-cp27-cp27mu-manylinux2010_x86_64.whl/tracepointdebug/config/config_names.py
@@ -0,0 +1,18 @@
+SIDEKICK_APIKEY = 'sidekick.apikey'
+
+SIDEKICK_DEBUG_ENABLE = 'sidekick.debug.enable'
+
+#############################################################################
+
+SIDEKICK_APPLICATION_ID = 'sidekick.application.id'
+SIDEKICK_APPLICATION_INSTANCE_ID = 'sidekick.application.instanceid'
+SIDEKICK_APPLICATION_NAME = 'sidekick.application.name'
+SIDEKICK_APPLICATION_STAGE = 'sidekick.application.stage'
+SIDEKICK_APPLICATION_DOMAIN_NAME = 'sidekick.application.domainname'
+SIDEKICK_APPLICATION_CLASS_NAME = 'sidekick.application.classname'
+SIDEKICK_APPLICATION_VERSION = 'sidekick.application.version'
+SIDEKICK_APPLICATION_TAG_PREFIX = 'sidekick.application.tag.prefix'
+SIDEKICK_APPLICATION_REGION = 'sidekick.application.region'
+SIDEKICK_ERROR_STACK_ENABLE = 'sidekick.error.stack.enable'
+SIDEKICK_ERROR_COLLECTION_ENABLE_CAPTURE_FRAME = 'sidekick.error.collection.enable.capture.frame'
+SIDEKICK_PRINT_CLOSED_SOCKET_DATA = 'sidekick.print.closed.socket.data'
