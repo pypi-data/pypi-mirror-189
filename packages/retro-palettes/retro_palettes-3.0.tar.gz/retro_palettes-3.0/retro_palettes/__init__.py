16bd9efae509ab07212363a1a61642ab75dde55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:45 +0000

    Add package retro-palettes/3.0/retro_palettes-3.0.tar.gz

---
+++ packages/retro-palettes/retro_palettes-3.0.tar.gz/retro_palettes-3.0/retro_palettes/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+__version__ = "3.0"
+
+palette = ['#11264E','#00507A',"#026e90",'#008B99','#6FAEA4','#FCDCB0','#FEE08B','#FAA96E','#F36B3B','#EF3F28','#CC0028']
+palette_cmap = ['#CC0028','#EF3F28','#F36B3B','#FAA96E','#FEE08B','#FCDCB0','#6FAEA4','#008B99',"#026e90",'#00507A','#11264E']
