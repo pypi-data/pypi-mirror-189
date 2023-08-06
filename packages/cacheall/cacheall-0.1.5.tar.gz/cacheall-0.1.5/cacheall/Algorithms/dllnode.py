40ea03a91a672b0da53e5e4c79c29d48ad82f2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:10 +0000

    Add package cacheall/0.1.5/cacheall-0.1.5.tar.gz

---
+++ packages/cacheall/cacheall-0.1.5.tar.gz/cacheall-0.1.5/cacheall/Algorithms/dllnode.py
@@ -0,0 +1,6 @@
+class DLLNode:
+    def __init__(self, key, val):
+        self.val = val
+        self.key = key
+        self.prev = None
+        self.next = None                                                                                                                                            
