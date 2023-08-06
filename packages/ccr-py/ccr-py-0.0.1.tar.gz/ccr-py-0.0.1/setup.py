0c72de8726e3da8d46e0e398457403bc6bb6a900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:20 +0000

    Add package ccr-py/0.0.1/ccr-py-0.0.1.tar.gz

---
+++ packages/ccr-py/ccr-py-0.0.1.tar.gz/ccr-py-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ccr-py',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Ali Omrani',
+    author_email='aomrani@usc.edu',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
