0ac7e0dba9b471d9feb4d29385256ebf200f2c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:52 +0000

    Add package pydasi/0.0.1/pydasi-0.0.1.tar.gz

---
+++ packages/pydasi/pydasi-0.0.1.tar.gz/pydasi-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pydasi',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Simon Smart',
+    author_email='simon@sds-stuff.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
