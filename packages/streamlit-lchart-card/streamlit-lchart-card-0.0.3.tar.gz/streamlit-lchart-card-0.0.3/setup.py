c112d7192e65b98b13d47fbc9438cd37e6400339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:49 +0000

    Add package streamlit-lchart-card/0.0.3/streamlit-lchart-card-0.0.3.tar.gz

---
+++ packages/streamlit-lchart-card/streamlit-lchart-card-0.0.3.tar.gz/streamlit-lchart-card-0.0.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-lchart-card",
+    version="0.0.3",
+    author="victorC97",
+    author_email="victor.cour@telecomnancy.net",
+    description="Chart card component",
+    long_description="Given a dataframe and two column of it, display a card with a line chart dataviz and statistics over it.",
+    long_description_content_type="text/plain",
+    url="https://github.com/victorC97/streamlit_graphic_card",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+    setup_requires=['wheel']
+)
