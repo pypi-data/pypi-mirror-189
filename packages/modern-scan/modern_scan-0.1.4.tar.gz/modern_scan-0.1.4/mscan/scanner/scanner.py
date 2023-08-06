0ae090e881cca52930d3fc518a673468836574c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:46 +0000

    Add package modern-scan/0.1.4/modern_scan-0.1.4.tar.gz

---
+++ packages/modern-scan/modern_scan-0.1.4.tar.gz/modern_scan-0.1.4/mscan/scanner/scanner.py
@@ -0,0 +1,83 @@
+from .interface import InterfaceManager, Interface
+from .network import Network
+from .multi_ping import MultiPinger
+from .arp import Arp, Host as ArpHost
+from dataclasses import dataclass, asdict
+from mac_vendor_lookup import AsyncMacLookup, MacLookup
+from typing import List
+import socket
+import concurrent.futures
+import asyncio 
+
+@dataclass
+class LiveHost:
+    ip: str
+    mac: str
+    vendor: str
+    hostname: str
+
+    def to_dict(self):
+        return {k: str(v) for k, v in asdict(self).items()}
+
+
+class Scanner:
+
+    BLACKLIST_PREFIX = [str(i) for i in range(220, 265)]
+    BLACKLIST_SUFFIX = ['255']
+
+    @property
+    def progress(self):
+        return self.multi.progress()
+
+
+    @staticmethod
+    def get_hostnames(hosts: List[ArpHost], max_workers = 20):
+        
+        def job(host: ArpHost):
+            try:
+                hostname = socket.gethostbyaddr(host.addr)[0]
+            except:
+                hostname = "unknown"
+            return {"host": host, "name": hostname}
+
+        result = {}
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executer:
+            futures = []
+            for host in hosts:
+                futures.append(executer.submit(job, host))
+            for future in concurrent.futures.as_completed(futures):
+                res = future.result()
+                host, name = res.values()
+                result[host.addr] = name
+        return result
+
+    @staticmethod
+    async def get_vendor(mac: str):
+        mac = mac.upper().replace('-', ':')
+        try:
+            vendor = await AsyncMacLookup().lookup(mac)
+        except:
+            return "unknown"
+        return vendor
+
+    @staticmethod
+    async def get_live_hosts(hosts: List[ArpHost]):
+        live_hosts = []
+        ips = [i.addr for i in hosts]
+        hostnames = Scanner.get_hostnames(hosts)
+        for host in hosts:
+            suffix = host.addr[-3:]
+            prefix = host.addr[:3]
+            if prefix not in Scanner.BLACKLIST_PREFIX and suffix not in Scanner.BLACKLIST_SUFFIX:
+                vendor = await Scanner.get_vendor(host.hwaddr)
+                live_host = LiveHost(host.addr, host.hwaddr, vendor, hostnames.get(host.addr, "unknown"))
+                live_hosts.append(live_host)
+        return live_hosts
+
+    async def scan(self, iface: Interface) -> List[LiveHost]:
+        network = Network.from_interface(iface)
+        ips = [str(i) for i in network.ips]
+        await MultiPinger().ping(ips)
+        hosts = Arp.get_tables(iface)
+        live = await self.get_live_hosts(hosts)
+        return live
---
+++ packages/modern-scan/modern_scan-0.1.4.tar.gz/modern_scan-0.1.4/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mscan', 'mscan.scanner']
+
+package_data = \
+{'': ['*'],
+ 'mscan': ['static/*',
+           'static/static/css/*',
+           'static/static/js/*',
+           'static/static/media/*']}
+
+install_requires = \
+['fastapi>=0.89.1,<0.90.0',
+ 'icmplib>=3.0.3,<4.0.0',
+ 'mac-vendor-lookup>=0.1.12,<0.2.0',
+ 'psutil>=5.9.4,<6.0.0',
+ 'uvicorn[standard]>=0.20.0,<0.21.0']
+
+entry_points = \
+{'console_scripts': ['mscan = mscan.main:main']}
+
+setup_kwargs = {
+    'name': 'modern-scan',
+    'version': '0.1.4',
+    'description': 'Modern network scanner',
+    'long_description': '# Mscan\n\n<img with="150px" height="150px" src="https://user-images.githubusercontent.com/61390950/182006521-350c306a-2567-49eb-b77a-42224783768f.png">  \n\n## A modern network scanner\n## 🖥 User Interface\n![image](https://user-images.githubusercontent.com/61390950/182006567-5e5cfcbe-7549-4205-902e-54706d2c1793.png)\n\n## 💿 Installation\nInstall from pypi\n```\npip3 install --upgrade modern-scan\n```\nthen simply execute\n```mscan```\n\n## ✨ Features\n\n- 🕹 Simple and clear user interface\n- 🚀 Super fast\n- 🛠 More features in development\n',
+    'author': 'thewh1teagle',
+    'author_email': '61390950+thewh1teagle@users.noreply.github.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
