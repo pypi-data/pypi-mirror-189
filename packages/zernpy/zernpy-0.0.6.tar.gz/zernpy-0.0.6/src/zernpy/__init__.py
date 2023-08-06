38733e487080335aa01e53e28a46437e2c180804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:24 +0000

    Add package zernpy/0.0.6/zernpy-0.0.6.tar.gz

---
+++ packages/zernpy/zernpy-0.0.6.tar.gz/zernpy-0.0.6/src/zernpy/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+The "zernpy" package is intended for initialization and calculation attributes / properties of Zernike polynomials.
+
+@author: Sergei Klykov
+@licence: MIT
+@year: 2023
+"""
+if __name__ == "__main__":
+    # use absolute imports for importing as module
+    __all__ = ['zernikepol']  # for specifying from zernpy import * if package imported from some script
+elif __name__ == "zernpy":
+    pass  # do not add module "zernikepol" to __all__ attribute, because it demands to construct explicit path
+
+# Automatically bring the main class and some methods to the name space when the used command both commands:
+# 1) from zernpy import ZernPol; 2) from zernpy import *
+if __name__ != "__main__" and __name__ != "__mp_main__":
+    from .zernikepol import ZernPol
+    from .zernikepol import generate_polynomials
