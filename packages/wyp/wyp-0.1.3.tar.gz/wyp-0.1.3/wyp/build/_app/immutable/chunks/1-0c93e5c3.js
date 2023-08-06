6511fff60986d0869b620afff811efb2b8ade2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:08 +0000

    Add package wyp/0.1.3/wyp-0.1.3.tar.gz

---
+++ packages/wyp/wyp-0.1.3.tar.gz/wyp-0.1.3/wyp/build/_app/immutable/chunks/1-0c93e5c3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-bd40fa45.js";export{t as component};
---
+++ packages/wyp/wyp-0.1.3.tar.gz/wyp-0.1.3/wyp/build/_app/immutable/chunks/2-7023aa93.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-8d303279.js";export{t as component};
