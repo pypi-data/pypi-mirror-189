7ff90c9466e60efe2100203abd55a7c6d1a09d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:54 +0000

    Add package advance-common/1.0.3/advance_common-1.0.3.tar.gz

---
+++ packages/advance-common/advance_common-1.0.3.tar.gz/advance_common-1.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['advance_common']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pymavlink==2.4.20', 'pyserial>=3.5,<4.0']
+
+setup_kwargs = {
+    'name': 'advance-common',
+    'version': '1.0.3',
+    'description': '',
+    'long_description': '# Protobuf messages in messages_pb2 are tracked by git, so when importing this repo as submodule # there is no need to compile protobuf.\n\nIf you are reading this outside of PyPi, this project is published:\n```pip install advance-common```',
+    'author': 'Szymon-SR',
+    'author_email': 'srszymonsr@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
