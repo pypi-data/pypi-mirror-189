a51d26b95dd2bbfd4ef4842bde4a49f05fb514fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:17 +0000

    Add package datetime.datetime/0.0.1/datetime.datetime-0.0.1.tar.gz

---
+++ packages/datetime.datetime/datetime.datetime-0.0.1.tar.gz/datetime.datetime-0.0.1/ipython3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipython3',
+    version='0.0.1',
+    packages=find_packages(),
+    description="Security Research for Dangling Packages"
+)
