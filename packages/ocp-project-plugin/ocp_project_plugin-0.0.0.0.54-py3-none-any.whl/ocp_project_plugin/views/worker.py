9dd2885382653e1a7f52b924fead7e8731d23b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:06 +0000

    Add package ocp-project-plugin/0.0.0.0.54/ocp_project_plugin-0.0.0.0.54-py3-none-any.whl

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.54-py3-none-any.whl/ocp_project_plugin/views/worker.py
@@ -0,0 +1,12 @@
+from django.shortcuts import redirect
+from django.urls import reverse
+from django_rq import get_queue
+
+
+def add_project(request):
+    try:
+        get_queue("default").enqueue("ocp_project_plugin.worker.pull_repository", git_dir='/repo/project_repo')
+        return redirect(reverse('plugins:ocp_project-plugin:appenvironment_list'))
+    except Exception as e:
+        message = e
+        return redirect(reverse('plugins:ocp_project_plugin:ocpproject_list'))
