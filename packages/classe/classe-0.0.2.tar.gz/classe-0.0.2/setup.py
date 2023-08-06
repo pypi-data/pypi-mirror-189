40951629f66377dc7ad58b116a53e6916c6ddd40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:41 +0000

    Add package classe/0.0.2/classe-0.0.2.tar.gz

---
+++ packages/classe/classe-0.0.2.tar.gz/classe-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="classe",
+    version="0.0.2",
+    author="Bogdan Marcu",
+    author_email="bogdan.marcu@tss-yonder.com",
+    description="Classes for scripts",
+    packages=find_packages(),
+)
