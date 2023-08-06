4ef40daadfe47fe49a435a9f0697d70b38a573cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:54 +0000

    Add package whendone/1.0.0/whendone-1.0.0-py2.py3-none-any.whl

---
+++ packages/whendone/whendone-1.0.0-py2.py3-none-any.whl/whendone/__init__.py
@@ -0,0 +1,34 @@
+"""Top-level package for WhenDone."""
+from whendone.whendone import WhenDone
+
+
+__author__ = """Sabri Barac"""
+__email__ = 'sabribarac@gmail.com'
+__version__ = '0.1.0'
+
+
+__doc__ = """
+================
+
+Description
+----------------
+WhenDone is a Python package created to notify you through Telegram and/or Slack
+when your function is done! Usually usefull for when you're training big ML/DL models.
+
+Example
+-------
+# Import library
+from whendone import WhenDone
+
+# Initialize
+notifier = WhenDone(telegram_token="XXXX", slack_token="XXXX")
+# If you added a slack token, you need to obtain the ID of a chat and add it.
+notifier.addSlackChatID('123456')
+
+# Add the notifier as a decorator
+notifier.whendone
+def Test():
+    print('Hello world')
+
+
+"""
