9f20217370e4aa0e207237fad3ad9bf558715fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:18 +0000

    Add package pytest-ver/0.0.37/pytest_ver-0.0.37.tar.gz

---
+++ packages/pytest-ver/pytest_ver-0.0.37.tar.gz/pytest_ver-0.0.37/pytest_ver/lib/build_info.py
@@ -0,0 +1,17 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.9.0'
+    os_name = 'posix'
+    version = '0.0.37'
+    git_sha = '7f4db1bdfb5535c33c3478527fa0473eeda35f83'
+    git_branch = 'master'
+    git_uncommitted = [
+        ' M do_publish',
+        ' M pytest_ver/lib/constants.py',
+        ' M pytest_ver/lib/harness.py',
+        'AM pytest_ver/lib/version.json',
+        ' M setup.py',
+        ' M test_sample.py',
+        ' M tools/publish/gen_build_info.py',
+    ]
+    git_unpushed = [
+    ]
