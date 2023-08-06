4c73f126c0238b014e9a2b5c52dbe6f5f072e5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:29 +0000

    Add package roblox-XML/69/roblox_XML-69.tar.gz

---
+++ packages/roblox-XML/roblox_XML-69.tar.gz/roblox_XML-69/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+__name__ = "roblox_XML"
+__version__ = "69"
+setup(
+    name=__name__,
+    version=__version__,
+    license='MIT',
+    author="vesper",
+    author_email='email@example.com',
+    packages=find_packages(),
+    url='https://github.com/vesperlol/pip-install-TouchGrass',
+    keywords='outside, grass, lib, module',
+    install_requires=[
+          'requests',
+      ]
+)
