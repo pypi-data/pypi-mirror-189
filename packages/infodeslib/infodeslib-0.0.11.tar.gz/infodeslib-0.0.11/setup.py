f1696c314cbc2dd91a78f6c8be48f000679ae21e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:28 +0000

    Add package infodeslib/0.0.11/infodeslib-0.0.11.tar.gz

---
+++ packages/infodeslib/infodeslib-0.0.11.tar.gz/infodeslib-0.0.11/setup.py
@@ -0,0 +1,35 @@
+import codecs
+import os
+from pathlib import Path
+from distutils.core import setup
+
+from setuptools import find_packages
+
+THIS_DIRECTORY = Path(__file__).parent 
+
+readme_path = THIS_DIRECTORY / ".." / "README.md"
+if readme_path.exists():
+    long_description = readme_path.read_text() 
+else: 
+    long_description = "Implementation of Dynamic Ensemble Selection methods with Late Fusion"
+
+setup(name='infodeslib',
+      version='0.0.11',
+      url='https://github.com/fukashi-hatake/infodeslib',
+      maintainer='Firuz Juraev',
+      maintainer_email='f.i.juraev@gmail.com',
+      description='Implementation of Dynamic Ensemble Selection methods with Late Fusion',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Firuz Juraev',
+      author_email='f.i.juraev@gmail.com',
+      license='MIT',
+
+      install_requires=[
+          'scikit-learn>=0.21.0',
+          'numpy>=1.17.0',
+          'scipy>=1.4.0',
+      ],
+      python_requires='>=3',      
+
+      packages=find_packages())
