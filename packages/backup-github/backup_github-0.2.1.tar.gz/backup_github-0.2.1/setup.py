cb97d309cf48c3496c0dd719ab1aadf098c3a286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:59 +0000

    Add package backup-github/0.2.1/backup_github-0.2.1.tar.gz

---
+++ packages/backup-github/backup_github-0.2.1.tar.gz/backup_github-0.2.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['backup_github']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['backup-github = backup_github.main:main']}
+
+setup_kwargs = {
+    'name': 'backup-github',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Karina5005',
+    'author_email': 'karinaanisimova23062001@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
