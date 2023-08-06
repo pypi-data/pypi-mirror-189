abcd25acd6c756dc6022d5a7303254d99c66603b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:48 +0000

    Add package nautobot-tools-dd/0.0.4/nautobot_tools_dd-0.0.4.tar.gz

---
+++ packages/nautobot-tools-dd/nautobot_tools_dd-0.0.4.tar.gz/nautobot_tools_dd-0.0.4/nautobot_tools/__init__.py
@@ -0,0 +1,16 @@
+from nautobot.extras.plugins import PluginConfig
+
+class NautobotYangConfig(PluginConfig):
+    name = 'nautobot_tools'
+    verbose_name = 'Nautobot Yang'
+    description = 'Nautobot Yang'
+    version = '0.1'
+    author = 'Dimas Ari'
+    author_email = 'dimas.ari@dataductus.com'
+    base_url = 'yang'
+    required_settings = []
+    default_settings = {
+        'loud': False
+    }
+
+config = NautobotYangConfig
