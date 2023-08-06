3479d236088af195bfadb8b7bc044002019f6d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:50 +0000

    Add package colrev/0.7.0/colrev-0.7.0.tar.gz

---
+++ packages/colrev/colrev-0.7.0.tar.gz/colrev-0.7.0/colrev/ops/built_in/search_sources/pubmed.py
@@ -0,0 +1,129 @@
+#! /usr/bin/env python
+"""SearchSource: Pubmed"""
+from __future__ import annotations
+
+import typing
+from dataclasses import dataclass
+from pathlib import Path
+
+import zope.interface
+from dacite import from_dict
+from dataclasses_jsonschema import JsonSchemaMixin
+
+import colrev.env.package_manager
+import colrev.exceptions as colrev_exceptions
+import colrev.ops.search
+import colrev.record
+
+# pylint: disable=unused-argument
+# pylint: disable=duplicate-code
+
+
+@zope.interface.implementer(
+    colrev.env.package_manager.SearchSourcePackageEndpointInterface
+)
+@dataclass
+class PubMedSearchSource(JsonSchemaMixin):
+    """SearchSource for Pubmed"""
+
+    settings_class = colrev.env.package_manager.DefaultSourceSettings
+    source_identifier = "https://pubmed.ncbi.nlm.nih.gov/{{pmid}}"
+    search_type = colrev.settings.SearchType.DB
+    heuristic_status = colrev.env.package_manager.SearchSourceHeuristicStatus.supported
+    short_name = "PubMed"
+    link = "https://pubmed.ncbi.nlm.nih.gov/"
+
+    def __init__(
+        self, *, source_operation: colrev.operation.CheckOperation, settings: dict
+    ) -> None:
+        self.search_source = from_dict(data_class=self.settings_class, data=settings)
+        self.review_manager = source_operation.review_manager
+
+    @classmethod
+    def heuristic(cls, filename: Path, data: str) -> dict:
+        """Source heuristic for Pubmed"""
+
+        result = {"confidence": 0.0}
+
+        # Simple heuristic:
+        if "PMID,Title,Authors,Citation,First Author,Journal/Book," in data:
+            result["confidence"] = 1.0
+            return result
+        if "PMID- " in data:
+            result["confidence"] = 0.7
+            return result
+
+        if "pmid " in data:
+            if data.count(" pmid ") > data.count("\n@"):
+                result["confidence"] = 1.0
+
+        return result
+
+    def validate_source(
+        self,
+        search_operation: colrev.ops.search.Search,
+        source: colrev.settings.SearchSource,
+    ) -> None:
+        """Validate the SearchSource (parameters etc.)"""
+
+        search_operation.review_manager.logger.debug(
+            f"Validate SearchSource {source.filename}"
+        )
+
+        if "query_file" not in source.search_parameters:
+            raise colrev_exceptions.InvalidQueryException(
+                f"Source missing query_file search_parameter ({source.filename})"
+            )
+
+        if not Path(source.search_parameters["query_file"]).is_file():
+            raise colrev_exceptions.InvalidQueryException(
+                f"File does not exist: query_file {source.search_parameters['query_file']} "
+                f"for ({source.filename})"
+            )
+
+        search_operation.review_manager.logger.debug(
+            f"SearchSource {source.filename} validated"
+        )
+
+    def load_fixes(
+        self,
+        load_operation: colrev.ops.load.Load,
+        source: colrev.settings.SearchSource,
+        records: typing.Dict,
+    ) -> dict:
+        """Load fixes for Pubmed"""
+
+        return records
+
+    def prepare(
+        self, record: colrev.record.Record, source: colrev.settings.SearchSource
+    ) -> colrev.record.Record:
+        """Source-specific preparation for Pubmed"""
+
+        if "first_author" in record.data:
+            record.remove_field(key="first_author")
+        if "journal/book" in record.data:
+            record.rename_field(key="journal/book", new_key="journal")
+        if "UNKNOWN" == record.data.get("author") and "authors" in record.data:
+            record.remove_field(key="author")
+            record.rename_field(key="authors", new_key="author")
+
+        if "UNKNOWN" == record.data.get("year"):
+            record.remove_field(key="year")
+            if "publication_year" in record.data:
+                record.rename_field(key="publication_year", new_key="year")
+
+        if "author" in record.data:
+            record.data["author"] = colrev.record.PrepRecord.format_author_field(
+                input_string=record.data["author"]
+            )
+
+        # TBD: how to distinguish other types?
+        record.change_entrytype(new_entrytype="article")
+        record.import_provenance(review_manager=self.review_manager)
+
+        return record
+
+
+if __name__ == "__main__":
+    pass
