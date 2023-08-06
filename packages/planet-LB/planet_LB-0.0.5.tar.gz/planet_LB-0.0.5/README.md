b32bd941bd1ad53dddff87974277ea9b576796e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:36 +0000

    Add package planet-LB/0.0.5/planet_LB-0.0.5.tar.gz

---
+++ packages/planet-LB/planet_LB-0.0.5.tar.gz/planet_LB-0.0.5/README.md
@@ -0,0 +1,8 @@
+A Lattice-Boltzmann python package for simulating common geodynamic problems. 
+Usage:
+import planet_LB as pLB
+
+
+T = pLB.init_1d_LB(stuff)
+
+
---
+++ packages/planet-LB/planet_LB-0.0.5.tar.gz/planet_LB-0.0.5/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name="planet_LB",
+        version="0.0.5",
+        author="Craig O'Neill",
+        description="Lattice Boltzmann Solver for python",
+        packages=["planet_LB"]
+)
