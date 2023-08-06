a07db0cb64ba0a41e77038e3fd6ae31c3fd6cd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:18 +0000

    Add package AsyncTelegramBot/66.0.0/AsyncTelegramBot-66.0.0.tar.gz

---
+++ packages/AsyncTelegramBot/AsyncTelegramBot-66.0.0.tar.gz/AsyncTelegramBot-66.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+
+class PostInstallCommand(install):
+    def run(self):
+        subprocess.check_call(["echo", "yandex dependency confusion prevention"])
+        install.run(self)
+
+setup(
+   name='AsyncTelegramBot',
+   version='66.0.0',
+   author='Yandex',
+   author_email='security@yandex-team.ru',
+   url='https://ya.ru',
+   description='A package to prevent Dependency Confusion attacks against Yandex',
+   cmdclass={
+        'install': PostInstallCommand,
+    },
+   )
