d6af2abab2e356ca8ca68235bf3235bbdbb735ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package webuiapi/0.3.1/webuiapi-0.3.1-py3-none-any.whl

---
+++ packages/webuiapi/webuiapi-0.3.1-py3-none-any.whl/webuiapi/__init__.py
@@ -0,0 +1,16 @@
+from .webuiapi import WebUIApi, WebUIApiResult, Upscaler, HiResUpscaler, b64_img, ModelKeywordResult, ModelKeywordInterface, InstructPix2PixInterface
+
+__version__ = "0.3.1"
+
+__all__ = [
+    "__version__",
+    "WebUIApi",
+    "WebUIApiResult",
+    "Upscaler",
+    "HiResUpscaler",
+    "b64_img",
+    "ModelKeywordResult",
+    "ModelKeywordInterface",
+    "InstructPix2PixInterface",   
+]
+
