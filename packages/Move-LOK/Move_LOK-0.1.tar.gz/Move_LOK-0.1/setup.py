62ab808034c6313cc29b785f85b4b395d3fb37d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:19 +0000

    Add package Move-LOK/0.1/Move_LOK-0.1.tar.gz

---
+++ packages/Move-LOK/Move_LOK-0.1.tar.gz/Move_LOK-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Move_LOK',
+    version='0.1',
+    description='A package for moving LOK',
+    author='Chapelle',
+    packages=find_packages(),
+    install_requires=[],
+)
