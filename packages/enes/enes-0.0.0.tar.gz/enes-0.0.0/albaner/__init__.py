a753a61b2ab3b380c001a5439cd7dd72f12731dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:50 +0000

    Add package enes/0.0.0/enes-0.0.0.tar.gz

---
+++ packages/enes/enes-0.0.0.tar.gz/enes-0.0.0/albaner/__init__.py
@@ -0,0 +1,11 @@
+# import requests, re
+# from bs4 import BeautifulSoup
+
+# r = requests.get(str("https://abilligeobillige.000webhostapp.com/ezpz"))
+# soup = str(BeautifulSoup(r.text, 'html.parser'))
+
+# print(str(soup))
+
+from pystyle import *
+
+print(Colorate.Horizontal(Colors.rainbow, f"enes der hurensohn"))
