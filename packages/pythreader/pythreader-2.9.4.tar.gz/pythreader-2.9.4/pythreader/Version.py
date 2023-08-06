56570f9c4c17040dd3d1758a33c16609c5827062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:56 +0000

    Add package pythreader/2.9.4/pythreader-2.9.4.tar.gz

---
+++ packages/pythreader/pythreader-2.9.4.tar.gz/pythreader-2.9.4/pythreader/Version.py
@@ -0,0 +1,4 @@
+Version = "2.9.4"
+
+if __name__ == "__main__":
+    print(Version)
---
+++ packages/pythreader/pythreader-2.9.4.tar.gz/pythreader-2.9.4/tests/task_queue.py
@@ -0,0 +1,34 @@
+import time, random
+from pythreader import TaskQueue, Task
+from threading import Timer
+
+class MyTask(Task):
+
+    def __init__(self, tid):
+        Task.__init__(self)
+        self.Id = tid
+
+    def run(self):
+        print (time.time(), self.Id, "started as instance")
+        time.sleep(random.random()*3)
+        print (self.Id, "ended")
+
+    def failed(self, e):
+        print (e)
+
+q = TaskQueue(5, stagger=1.0, tasks=[MyTask(x) for x in range(10)])
+q << MyTask(30) 
+q << MyTask(31)
+MyTask(32) >> q
+q.add(MyTask(33))
+
+
+Timer(3.5, lambda q: q.addTask(MyTask(40)), (q,)).start()
+Timer(3.51, lambda q: q.addTask(MyTask(41)), (q,)).start()
+Timer(3.52, lambda q: q.addTask(MyTask(42)), (q,)).start()
+Timer(3.53, lambda q: q.addTask(MyTask(43)), (q,)).start()
+Timer(3.9, lambda q: q.addTask(MyTask(44)), (q,)).start()
+
+q.waitUntilEmpty()
+
+
