6e31fe5787e690baa5df60b06005e8ec461c2312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:52 +0000

    Add package cuvisoft/0.1/cuvisoft-0.1-py3-none-any.whl

---
+++ packages/cuvisoft/cuvisoft-0.1-py3-none-any.whl/cuvisoft/__init__.py
@@ -0,0 +1,13 @@
+# add and subtract multiple numbers and divide using two numbers
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
