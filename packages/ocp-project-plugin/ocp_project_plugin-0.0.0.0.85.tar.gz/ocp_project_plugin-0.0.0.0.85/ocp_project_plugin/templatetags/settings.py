87a0ec998d291232b4e1f8f7134f33d07cec212a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:13 +0000

    Add package ocp-project-plugin/0.0.0.0.85/ocp_project_plugin-0.0.0.0.85.tar.gz

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.85.tar.gz/ocp_project_plugin-0.0.0.0.85/ocp_project_plugin/templatetags/settings.py
@@ -0,0 +1,37 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+PLUGIN_SETTINGS = settings.PLUGINS_CONFIG.get('ocp_project_plugin', dict())
+GITLAB_PROJECT_URL = PLUGIN_SETTINGS.get('gitlab_project_url', '')
+JIRA_BROWSE_URL = PLUGIN_SETTINGS.get('jira_browse_url', '')
+CPU_COST = PLUGIN_SETTINGS.get('cpu_cost', '')
+MEMORY_COST = PLUGIN_SETTINGS.get('memory_cost', '')
+STORAGE_COST = PLUGIN_SETTINGS.get('storage_cost', '')
+
+
+# settings value
+@register.simple_tag
+def jira_browse_url(ticket_id):
+    return f"{JIRA_BROWSE_URL}{ticket_id}"
+
+
+@register.simple_tag
+def get_cpu_cost(amount):
+    return CPU_COST * amount
+
+
+@register.simple_tag
+def get_memory_cost(amount):
+    return MEMORY_COST * amount
+
+
+@register.simple_tag
+def get_storage_cost(amount):
+    return STORAGE_COST * amount
+
+
+@register.simple_tag
+def get_total_cost(cpu_amount, memory_amount, storage_amount):
+    return CPU_COST * cpu_amount + MEMORY_COST * memory_amount + STORAGE_COST * storage_amount
