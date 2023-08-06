6de29391268d40a642acd0c46d49c1a1f7a57e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:15 +0000

    Add package ms-graph-client/0.1.5/ms_graph_client-0.1.5-py3-none-any.whl

---
+++ packages/ms-graph-client/ms_graph_client-0.1.5-py3-none-any.whl/ms_graph_client/__init__.pyi
@@ -0,0 +1,5 @@
+from ms_graph_client.graph_api_config import GraphAPIConfig as GraphAPIConfig
+from ms_graph_client.services.graph_api import GraphAPI as GraphAPI
+from ms_graph_client.services_cache.graph_api import GraphAPI as GraphCacheAPI
+
+__all__ = ["GraphAPIConfig", "GraphAPI", "GraphCacheAPI"]
