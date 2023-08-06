0fac4f15856bb020547ae44fa216a6b8ec8824eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:39 +0000

    Add package free-google-SERP-api/1.0.4/free_google_SERP_api-1.0.4.tar.gz

---
+++ packages/free-google-SERP-api/free_google_SERP_api-1.0.4.tar.gz/free_google_SERP_api-1.0.4/free_google_SERP_api/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+from .free_google_SERP_api import (
+    get_serps
+)
---
+++ packages/free-google-SERP-api/free_google_SERP_api-1.0.4.tar.gz/free_google_SERP_api-1.0.4/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import os
+import setuptools
+
+
+def local_file(file):
+    return codecs.open(
+        os.path.join(os.path.dirname(__file__), file), 'r', 'utf-8'
+    )
+
+setuptools.setup(
+    name="free_google_SERP_api",
+    version="1.0.4",
+    author="Dídac Anton",
+    author_email="dinerofacil.x.paypal@gmail.com",
+    description="Una librería Python para extraer y manipular los diferentes datos de los resultados de las búsquedas de Google",
+    packages=setuptools.find_packages(),
+    long_description=local_file('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://twitter.com/seo_torch",
+    classifiers=[
+	"Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Typing :: Typed",
+    ],
+    install_requires=[
+        "requests",
+        "pandas",
+        "numpy",
+        "requests_html",
+        "faqs_google_results",
+    ],
+    python_requires=">=3.6"
+)
