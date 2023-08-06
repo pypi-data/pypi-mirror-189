77a567e6f629770bc71cb488dbafdc2e8fc1f8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:27 +0000

    Add package huble/0.2.245/huble-0.2.245-py3-none-any.whl

---
+++ packages/huble/huble-0.2.245-py3-none-any.whl/huble/sklearn/essentials/function.py
@@ -0,0 +1,22 @@
+from sklearn.model_selection import train_test_split as sklearn_tts  # type: ignore
+import pandas as pd
+from ...error.decorators import function_error_handling
+
+# @function_error_handling("train_test_split")
+def train_test_split(**params):
+    data = params["data"]
+    train_dataset, test_dataset = sklearn_tts(
+        data, test_size=params["parameters"]["test_size"], random_state=42
+    )
+    return train_dataset, test_dataset
+
+
+# @function_error_handling("train_model")
+def train_model(**params):
+    data = params["data"]
+
+    y = data[params["column"]]
+    X = data.drop(params["column"], axis=1)
+    model = params["model"]
+    Model = model.fit(X, y)
+    return Model
