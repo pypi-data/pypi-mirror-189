d3747eac79ede6b5fdd7e158531fc90fd077af7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:54 +0000

    Add package secenv/0.10.0/secenv-0.10.0-py3-none-any.whl

---
+++ packages/secenv/secenv-0.10.0-py3-none-any.whl/secenv/stores/env.py
@@ -0,0 +1,16 @@
+import os
+from . import StoreInterface
+
+
+class Store(StoreInterface):
+    def __init__(self, name, infos):
+        self.name = name
+
+    def gen_parser(self, parser):
+        parser.add_argument("secret")
+
+    def read_secret(self, secret):
+        if res := os.getenv(secret):
+            return res
+        else:
+            raise Exception(f"Secret '{secret}' not found in store '{self.name}'")
