f69fb27d27f500819eb943fd57134265d5d5e66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:43 +0000

    Add package News-NLP/0.3/News_NLP-0.3.tar.gz

---
+++ packages/News-NLP/News_NLP-0.3.tar.gz/News_NLP-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='News_NLP',
+version='0.3',
+description='Fetch google news, preprocess, classify news articles into seven predifired types and classify sentiment strength, geolocate news articels in terms of population, lat, lon.',
+# url='#',
+author='JasonLu',
+author_email='luzhenxian@hotmail.com',
+license='MIT',
+packages=['News_NLP'],
+zip_safe=False)
