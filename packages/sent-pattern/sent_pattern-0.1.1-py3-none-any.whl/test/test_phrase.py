c579c860250f577509b75d92f642dc564731d430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:57 +0000

    Add package sent-pattern/0.1.1/sent_pattern-0.1.1-py3-none-any.whl

---
+++ packages/sent-pattern/sent_pattern-0.1.1-py3-none-any.whl/test/test_phrase.py
@@ -0,0 +1,21 @@
+from sent_pattern.core.elements.sub.phrase import PrepPhrase
+import spacy
+from sent_pattern import tags
+
+from sent_pattern.core.factory.elements import  ElementsFactory
+
+nlp = spacy.load("en_core_web_lg")
+
+def test_phrase():
+    doc = nlp("The Eureka client handles all aspects of service instance registration and deregistration")
+    phrase = PrepPhrase()
+    doc = phrase.register_prep_phrase(doc)
+    print(doc.spans["prep_noun"])
+
+
+def test_custom_phrase():
+    text = "The Eureka client handles all aspects of service instance registration and deregistration"
+    doc = nlp(text)
+    dep_list = tags.create_dep_list(doc)
+    custom = ElementsFactory.make_custom_elements(dep_list, doc=doc, option="prep")
+    phrase = custom.option
