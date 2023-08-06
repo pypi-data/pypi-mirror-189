6f530e4324c9984dd1dee503d9001ac7827e7246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:47 +0000

    Add package XadNacos/0.0.2/XadNacos-0.0.2.tar.gz

---
+++ packages/XadNacos/XadNacos-0.0.2.tar.gz/XadNacos-0.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import setuptools
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='XadNacos',
+    version='0.0.2',
+    description='a package for nacos',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='xadocker',
+    author_email='1793360097@qq.com',
+    license='Apache License 2.0',
+    url='',
+    packages=setuptools.find_packages(),
+    install_requires=['requests>=2.20.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
