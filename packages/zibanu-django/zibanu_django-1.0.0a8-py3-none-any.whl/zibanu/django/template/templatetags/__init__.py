b8339e6def1e664a3c227068cf9a023abf6b80b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:45 +0000

    Add package zibanu-django/1.0.0a8/zibanu_django-1.0.0a8-py3-none-any.whl

---
+++ packages/zibanu-django/zibanu_django-1.0.0a8-py3-none-any.whl/zibanu/django/template/templatetags/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#  Developed by CQ Inversiones SAS. Copyright ©. 2019 - 2023. All rights reserved.
+#  Desarrollado por CQ Inversiones SAS. Copyright ©. 2019 - 2023. Todos los derechos reservado
+
+# ****************************************************************
+# IDE:          PyCharm
+# Developed by: macercha
+# Date:         28/01/23 15:33
+# Project:      CFHL Transactional Backend
+# Module Name:  __init__.py
+# Description:
+# ****************************************************************
+from .static_uri import static_uri
+
+
+__all__ = [
+    "static_uri"
+]
+
