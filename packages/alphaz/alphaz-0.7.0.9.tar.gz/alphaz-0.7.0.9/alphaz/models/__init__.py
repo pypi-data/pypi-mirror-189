a9fb7ef6061e28b87211c8cd25ee52bb34e2e4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:58 +0000

    Add package alphaz/0.7.0.9/alphaz-0.7.0.9.tar.gz

---
+++ packages/alphaz/alphaz-0.7.0.9.tar.gz/alphaz-0.7.0.9/alphaz/models/__init__.py
@@ -0,0 +1,3 @@
+from typing import Dict, List, Union
+
+from .main._process import process
---
+++ packages/alphaz/alphaz-0.7.0.9.tar.gz/alphaz-0.7.0.9/alphaz/models/main/__init__.py
@@ -0,0 +1,11 @@
+from ._base import AlphaDataclass, AlphaClass, AlphaMappingAttribute, AlphaEnum
+from ._file import AlphaFile
+from ._singleton import singleton, Singleton
+from ._exception import AlphaException
+from ._core._core import AlphaCore
+from ._request import AlphaTransaction
+from ._enum import EnumStr
+
+from dataclasses import dataclass, field
+
+from typing import Dict, List
