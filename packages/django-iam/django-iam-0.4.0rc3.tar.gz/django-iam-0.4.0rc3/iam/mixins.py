09f85704767a297a07fd8e95842bd379f8395396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:03 +0000

    Add package django-iam/0.4.0rc3/django-iam-0.4.0rc3.tar.gz

---
+++ packages/django-iam/django-iam-0.4.0rc3.tar.gz/django-iam-0.4.0rc3/iam/mixins.py
@@ -0,0 +1,99 @@
+from typing import Optional
+
+import rules
+from django.db import models
+
+from .registry import get_registered_roles
+
+
+class RolePredicateMixin(models.Model):
+    """
+    Mixin used on profile classes. Provides class method `get_predicate` to create rules predicates to check if a user
+    has a profile or not.
+    Provides `parent` attribute. If a role has a parent, and a user has the parent profile, the user will have all
+    permissions associated with the child role too.
+
+    :param parent: A role could be a specific subset of another parent role. Set parent to that. If a user has a role
+        that is the child of another roles, they automatically get all the permissions for the parent role too.
+    """
+    parent: 'RolePredicateMixin' = None
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def get_predicate(cls, extra_check=None):
+        """
+        Return a rules predicate that checks if a user has a corresponding profile thus having that role.
+
+        :param extra_check: Optionally check extra parameters on the user's profile instance.
+        :return: Predicate
+        """
+
+        def has_role(user):
+            if user.is_superuser:
+                return True
+
+            profile_instance = user.get_or_set_role(cls._meta.model)
+            return bool(profile_instance) and (not extra_check or extra_check(profile_instance))
+
+        role_name = cls._meta.verbose_name.rstrip(' profile').replace(' ', '_')
+        predicate = rules.predicate(has_role, name=f'is_{role_name}')
+        if cls.parent:
+            return predicate | cls.parent.get_predicate(extra_check)
+        return predicate
+
+
+class IAMUserMixin:
+    """
+    Mixin used on custom user models that use IAM for their permission management. Provides interfaces to the user model
+    to interact with the IAM, and cache permissions.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._roles = {}  # { ProfileModel: instance | False }
+        super(IAMUserMixin, self).__init__(*args, **kwargs)
+
+    @property
+    def roles(self):
+        return self._roles
+
+    def _set_role(self, model_cls) -> Optional[RolePredicateMixin]:
+        """
+        Check if this user has a particular profile denoted by `model_cls`. If they do, cache the profile instance. If
+        not cache a False value, so we don't need to hit the database again for this check.
+
+        :param model_cls: The model class of the profile to be checked.
+        :return: The profile instance belonging to the user or False
+        """
+        try:
+            profile_instance = model_cls.objects.available().get(user=self)
+        except model_cls.DoesNotExist:
+            profile_instance = False
+        self._roles[model_cls] = profile_instance
+        return profile_instance
+
+    def get_or_set_role(self, model_cls):
+        """
+        Check if this user has a particular profile denoted by `model_cls`.
+
+        First hit the cache. If there is no value
+        in the cache, hit the database. If the user has the profile, cache the profile instance. If
+        not, cache a False value, so we don't need to hit the database again for this check.
+
+        :param model_cls: The model class of the profile to be checked.
+        :return: The profile instance belonging to the user or None
+        """
+        profile_instance = self.roles.get(model_cls)  # None | False | instance
+        if profile_instance is None:
+            profile_instance = self._set_role(model_cls)
+        return profile_instance if profile_instance else None
+
+    def load_roles(self):
+        """
+        Load all the roles (and profiles) this user has in their cache.
+        """
+        self._roles = {}
+        for model_cls in get_registered_roles():
+            self._set_role(model_cls)
+        return self._roles
---
+++ packages/django-iam/django-iam-0.4.0rc3.tar.gz/django-iam-0.4.0rc3/iam/models.py
@@ -0,0 +1,83 @@
+"""
+This module contains an abstract model `UserProfileModel` to be used as a mixin for any profile model class
+"""
+
+from typing import Callable, Any, Optional
+from warnings import warn
+
+from building_blocks.models import Archivable, UnnamedKaosModel
+from django.conf import settings
+from django.db import models
+
+from .mixins import RolePredicateMixin
+
+
+class UserProfileModel(
+    RolePredicateMixin,
+    Archivable,
+    UnnamedKaosModel,
+    models.Model
+):
+    """
+    Abstract model for profile models to inherit from. Provides a one-to-one user field that points to the owner of a
+    profile.
+    Override the user field, if you'd like to set its `related_name` or set it to optional (e.g. for bot profiles)
+
+    :param user: The user this profile belongs to
+    """
+
+    class Meta:
+        abstract = True
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    deactivate = Archivable.archive
+
+    def __str__(self):
+        return f"{self.user} as {self._meta.verbose_name}"
+
+    @classmethod
+    def check_user(cls, check_func) -> Callable[[Any], bool]:
+        """
+        Return a function that first, checks if a user has the role denoted by this profile class, and then runs
+        `check_func` on the user's profile for this role to determine extra permissions.
+
+        :param check_func: Function that receives a profile instance and checks if it passes a condition or not.
+        :return: Function that accepts a user instance as an argument, and checks if they have the role and some extra
+            conditions.
+
+        Example:
+            >>> # models.py
+            >>> @register_role
+            >>> class AuthorProfile(UserProfileModel):
+            >>>     is_super_author = models.BooleanField(default=False)
+            >>>
+            >>>     @staticmethod
+            >>>     def check_super_author(user):
+            >>>         return AuthorProfile.check_user(lambda p: p.is_super_author)(user)
+        """
+
+        warn('This method is deprecated.', DeprecationWarning)
+
+        def check_user_func(user) -> bool:
+            profile = user.get_or_set_role(cls)
+            if not profile:
+                return False
+            return check_func(profile)
+
+        return check_user_func
+
+    @classmethod
+    def get_for_user(cls, user) -> Optional['UserProfileModel']:
+        """
+        Check if a user has a profile denoted by this class.
+
+        :param user: the user to check
+        :return: The profile instance belonging to the user or None
+        """
+        return user.get_or_set_role(cls)
+
+
+__all__ = [
+    'UserProfileModel',
+]
