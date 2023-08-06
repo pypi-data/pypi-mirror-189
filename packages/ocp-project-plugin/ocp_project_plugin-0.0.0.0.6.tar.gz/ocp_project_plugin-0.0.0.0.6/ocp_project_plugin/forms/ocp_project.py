96467b12f62a7d5475fbcba95013fbdb47d33144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:54 +0000

    Add package ocp-project-plugin/0.0.0.0.6/ocp_project_plugin-0.0.0.0.6.tar.gz

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.6.tar.gz/ocp_project_plugin-0.0.0.0.6/ocp_project_plugin/forms/ocp_project.py
@@ -0,0 +1,203 @@
+from django.forms import (
+    CharField,
+    BooleanField,
+)
+
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from ocp_project_plugin.models import AppEnvironment, OCPProject
+
+
+class OCPProjectForm(NetBoxModelForm):
+    """Form for creating a new App Environment object."""
+    name = CharField(
+        label="OCP Project Name",
+        help_text="The ocp project name e.g. web-shop",
+    )
+    description = CharField(
+        label="Description",
+        help_text="The description of the project e.g. A web shop software",
+    )
+    display_name = CharField(
+        label="Display name",
+        help_text="Display Name of the project e.g. Web Shop Shopify"
+    )
+    owner = CharField(
+        label="Owner",
+        help_text="Choose the tenant"
+    )
+    contact = CharField(
+        label="Contact E-Mail",
+        help_text="Write the e-mail of the contact e.g. ana.meier@domain.com"
+    )
+    customer = CharField(
+        label="Customer name",
+        help_text="Choose the tenant"
+    )
+    url = CharField(
+        label="URL",
+        help_text="The url of the project documentation",
+    )
+    workload = BooleanField(
+        label="Workload",
+        help_text="The workload contents e.g. Postgres DB, nginx",
+    )
+    request = BooleanField(
+        label="Jira Request",
+        help_text="The jira request id e.g. TICKET1234",
+    )
+
+    class Meta:
+        model = OCPProject
+
+        fields = ["name", "description", "display_name", "owner", "contact", "customer", "url", "workload", "request"]
+
+
+class OCPProjectFilterForm(NetBoxModelFilterSetForm):
+    """Form for filtering App Environment instances."""
+
+    model = AppEnvironment
+
+    name = CharField(
+        required=False,
+        label="OCP Project Name",
+        help_text="The ocp project name e.g. web-shop",
+    )
+    description = CharField(
+        required=False,
+        label="Description",
+        help_text="The description of the project e.g. A web shop software",
+    )
+    display_name = CharField(
+        required=False,
+        label="Display name",
+        help_text="Display Name of the project e.g. Web Shop Shopify"
+    )
+    owner = CharField(
+        required=False,
+        label="Owner",
+        help_text="Choose the tenant"
+    )
+    contact = CharField(
+        required=False,
+        label="Contact E-Mail",
+        help_text="Write the e-mail of the contact e.g. ana.meier@domain.com"
+    )
+    customer = CharField(
+        required=False,
+        label="Customer name",
+        help_text="Choose the tenant"
+    )
+    url = CharField(
+        required=False,
+        label="URL",
+        help_text="The url of the project documentation",
+    )
+    workload = BooleanField(
+        required=False,
+        label="Workload",
+        help_text="The workload contents e.g. Postgres DB, nginx",
+    )
+    request = BooleanField(
+        required=False,
+        label="Jira Request",
+        help_text="The jira request id e.g. TICKET1234",
+    )
+
+
+class OCPProjectImportForm(NetBoxModelImportForm):
+    name = CharField(
+        label="OCP Project Name",
+        help_text="The ocp project name e.g. web-shop",
+    )
+    description = CharField(
+        label="Description",
+        help_text="The description of the project e.g. A web shop software",
+    )
+    display_name = CharField(
+        label="Display name",
+        help_text="Display Name of the project e.g. Web Shop Shopify"
+    )
+    owner = CharField(
+        label="Owner",
+        help_text="Choose the tenant"
+    )
+    contact = CharField(
+        label="Contact E-Mail",
+        help_text="Write the e-mail of the contact e.g. ana.meier@domain.com"
+    )
+    customer = CharField(
+        label="Customer name",
+        help_text="Choose the tenant"
+    )
+    url = CharField(
+        label="URL",
+        help_text="The url of the project documentation",
+    )
+    workload = BooleanField(
+        label="Workload",
+        help_text="The workload contents e.g. Postgres DB, nginx",
+    )
+    request = BooleanField(
+        label="Jira Request",
+        help_text="The jira request id e.g. TICKET1234",
+    )
+
+    class Meta:
+        model = AppEnvironment
+
+        fields = ["name", "description", "display_name", "owner", "contact", "customer", "url", "workload", "request"]
+
+
+class OCPProjectBulkEditForm(NetBoxModelBulkEditForm):
+    model = AppEnvironment
+
+    name = CharField(
+        required=False,
+        label="OCP Project Name",
+        help_text="The ocp project name e.g. web-shop",
+    )
+    description = CharField(
+        required=False,
+        label="Description",
+        help_text="The description of the project e.g. A web shop software",
+    )
+    display_name = CharField(
+        required=False,
+        label="Display name",
+        help_text="Display Name of the project e.g. Web Shop Shopify"
+    )
+    owner = CharField(
+        required=False,
+        label="Owner",
+        help_text="Choose the tenant"
+    )
+    contact = CharField(
+        required=False,
+        label="Contact E-Mail",
+        help_text="Write the e-mail of the contact e.g. ana.meier@domain.com"
+    )
+    customer = CharField(
+        required=False,
+        label="Customer name",
+        help_text="Choose the tenant"
+    )
+    url = CharField(
+        required=False,
+        label="URL",
+        help_text="The url of the project documentation",
+    )
+    workload = BooleanField(
+        required=False,
+        label="Workload",
+        help_text="The workload contents e.g. Postgres DB, nginx",
+    )
+    request = BooleanField(
+        required=False,
+        label="Jira Request",
+        help_text="The jira request id e.g. TICKET1234",
+    )
