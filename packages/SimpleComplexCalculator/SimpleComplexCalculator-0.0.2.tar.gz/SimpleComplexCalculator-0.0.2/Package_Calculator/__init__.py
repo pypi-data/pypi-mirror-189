67c7d55e2620860f3226360108adfa76dc828c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:51 +0000

    Add package SimpleComplexCalculator/0.0.2/SimpleComplexCalculator-0.0.2.tar.gz

---
+++ packages/SimpleComplexCalculator/SimpleComplexCalculator-0.0.2.tar.gz/SimpleComplexCalculator-0.0.2/Package_Calculator/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 31 17:39:39 2023
+
+@author: tom.ebermeyer
+"""
+
---
+++ packages/SimpleComplexCalculator/SimpleComplexCalculator-0.0.2.tar.gz/SimpleComplexCalculator-0.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 08:28:30 2023
+
+@author: tom.ebermeyer
+"""
+
+
+from setuptools import setup
+
+setup(
+    name="SimpleComplexCalculator",
+    version="0.0.2",
+    author="Tom Ebermeyer",
+    author_email="tom.ebermeyer@cpe.fr",
+    description="Calculateur simple entre nombres complexes",
+    packages=['Package_Calculator'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
