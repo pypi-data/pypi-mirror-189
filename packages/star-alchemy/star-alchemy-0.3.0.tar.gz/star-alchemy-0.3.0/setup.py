0c5c611966177c76f974dddc080b8fc2e8c20dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package star-alchemy/0.3.0/star-alchemy-0.3.0.tar.gz

---
+++ packages/star-alchemy/star-alchemy-0.3.0.tar.gz/star-alchemy-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['star_alchemy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sqlalchemy>=2,<3', 'toolz>=0.12.0,<0.13.0']
+
+setup_kwargs = {
+    'name': 'star-alchemy',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Daniel Bradburn',
+    'author_email': 'moagstar@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
