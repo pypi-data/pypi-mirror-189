0942047f671f18e497ed6488dfc5c66aec8ff254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:38 +0000

    Add package spec-utils/0.28.8/spec_utils-0.28.8.tar.gz

---
+++ packages/spec-utils/spec_utils-0.28.8.tar.gz/spec_utils-0.28.8/spec_utils/_schemas/__init__.py
@@ -0,0 +1,6 @@
+from .base import JWT as JWT, KwargRelation as KwargRelation
+from .specmanager import Employee as SMEmployee
+from .certronic import (
+    Clocking as CertronicClocking,
+    ClockingList as CertronicCLockingList
+)
