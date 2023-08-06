5736dcf2ef3954cd1772af064fbffdb23023404d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:29 +0000

    Add package blockzaz/1.0.2/blockzaz-1.0.2.tar.gz

---
+++ packages/blockzaz/blockzaz-1.0.2.tar.gz/blockzaz-1.0.2/blockzaz/blockzaz.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from prefect.blocks.core import Block
+from pydantic import SecretStr
+
+class CubeJs(Block):
+    urlCube: Optional[str] = None
+    apiSecret: Optional[SecretStr] = None
+    urlAtualizacao: Optional[str] = None
+
+class Pentaho(Block):
+    urlPentaho: Optional[str] = None
+    apiSecret: Optional[SecretStr] = None
+    urlAtualizacao: Optional[str] = None
