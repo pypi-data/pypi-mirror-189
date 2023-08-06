836136b07a3438af6ead46bb5af38b301e6ec179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:56 +0000

    Add package pyCGNS/6.2.2/pyCGNS-6.2.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyCGNS/pyCGNS-6.2.2-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/CGNS/pyCGNSconfig.py
@@ -0,0 +1,52 @@
+#  -------------------------------------------------------------------------
+#  pyCGNS - Python package for CFD General Notation System
+#  See license.txt file in the root directory of this Python module source  
+#  -------------------------------------------------------------------------
+# This file has been generated on [2023-02-01 22:41:45]
+# Using platform [Linux a56d9d549f0d x86_64]
+
+DATE="2023-02-01 22:41:45"
+PLATFORM="Linux a56d9d549f0d x86_64"
+PFX="#"
+
+HAS_HDF5=True
+HAS_H5PY=0
+HAS_VTK=False
+HAS_MSW=0
+USE_COMPACT_STORAGE=1
+
+INCLUDE_DIRS=['/opt/_internal/cpython-3.8.16/lib/python3.8/site-packages/numpy/core/include', '/usr/local/include', '', '/opt/_internal/cpython-3.8.16/include', '../include', '/usr/local']
+LIBRARY_DIRS=['/usr/local/lib', '', '/opt/_internal/cpython-3.8.16/lib', '../lib', '/usr/local', '/usr/local/lib64']
+
+HDF5_VERSION="1.8.20"
+HDF5_PATH_INCLUDES=['/usr/local/include']
+HDF5_PATH_LIBRARIES=['/usr/local/lib']
+HDF5_LINK_LIBRARIES=['hdf5']
+HDF5_EXTRA_ARGS=[]
+
+NUMPY_VERSION="1.24.1"
+NUMPY_VERSION_API="0x01000009"
+NUMPY_PATH_INCLUDES=['/opt/_internal/cpython-3.8.16/lib/python3.8/site-packages/numpy/core/include']
+NUMPY_PATH_LIBRARIES=[]
+NUMPY_LINK_LIBRARIES=[]
+NUMPY_EXTRA_ARGS=[]
+
+PYQT_VERSION=""
+QT_VERSION=""
+CYTHON_VERSION=""
+VTK_VERSION=""
+
+__version__=6 # @@UPDATEVERSION@@
+__release__=1 # @@UPDATERELEASE@@
+__revision__=4 # @@UPDATEREVISION@@
+__vid__="%s.%s.%s"%(__version__,__release__,__revision__)
+__doc__="pyCGNS - %s - Python package for CGNS (CFD General Notation System)"        %(__vid__)
+version=__vid__
+
+NAME="pyCGNS"
+VERSION=__vid__
+DESCRIPTION=__doc__
+AUTHOR="Marc Poinot"
+EMAIL="marc.poinot@safrangroup.com"
+LICENSE="LGPL 2"
+
