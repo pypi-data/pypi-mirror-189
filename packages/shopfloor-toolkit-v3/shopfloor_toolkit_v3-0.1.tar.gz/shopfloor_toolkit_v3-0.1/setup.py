01f50223fc25d2211020dabf33911008bcb179f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:31 +0000

    Add package shopfloor-toolkit-v3/0.1/shopfloor_toolkit_v3-0.1.tar.gz

---
+++ packages/shopfloor-toolkit-v3/shopfloor_toolkit_v3-0.1.tar.gz/shopfloor_toolkit_v3-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='shopfloor_toolkit_v3',
+      version='0.01',
+      description='Toolkit for the Shopfloor endpoints.',
+      author='Medulla',
+      packages=['toolkit'],
+      install_requires=['requests~=2.28.1'],
+      zip_safe=False)
