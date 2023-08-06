eebc73b8d0937c02214a22fa6db972a40c651146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:48 +0000

    Add package ocp-project-plugin/0.0.0.0.83/ocp_project_plugin-0.0.0.0.83-py3-none-any.whl

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.83-py3-none-any.whl/ocp_project_plugin/__init__.py
@@ -0,0 +1,27 @@
+from extras.plugins import PluginConfig
+
+__version__ = "0.0.0.0.83"
+
+
+class OCPProjectConfig(PluginConfig):
+    name = "ocp_project_plugin"
+    verbose_name = "OCP Project Plugin"
+    description = "The netbox ocp project plugin, for creating ocp projects"
+    min_version = "3.4.0"
+    version = __version__
+    author = "Tim Rhomberg"
+    author_email = "timrhomberg@hotmail.com"
+    required_settings = [
+        "gitlab_project_url",
+        "values_path",
+        "default_access_token",
+        'jira_browse_url',
+        'ocp_tst_url',
+        'ocp_dev_url',
+        'ocp_int_url',
+        'ocp_prd_url'
+    ]
+    base_url = "ocp-project-plugin"
+
+
+config = OCPProjectConfig
