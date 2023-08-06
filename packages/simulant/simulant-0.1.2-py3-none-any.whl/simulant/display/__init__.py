0e89642d99a53d2b3ca38b648f3182265961233b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:53 +0000

    Add package simulant/0.1.2/simulant-0.1.2-py3-none-any.whl

---
+++ packages/simulant/simulant-0.1.2-py3-none-any.whl/simulant/display/__init__.py
@@ -0,0 +1,44 @@
+import os
+
+os.environ.get("DISPLAY", os.environ.update({'DISPLAY': ':1'}))
+from Xlib import display as disp
+from Xlib import X
+from PIL import Image
+
+
+class Display:
+    @property
+    def dsp(self):
+        self.dsp = disp.Display()
+        return self.dsp
+
+    @property
+    def width(self):
+        try:
+            scr = self.dsp.screen()
+            width = scr.width_in_pixels
+            return width
+        finally:
+            self.dsp.close()
+
+    @property
+    def height(self):
+        try:
+            scr = self.dsp.screen()
+            height = scr.height_in_pixels
+            return height
+        finally:
+            self.dsp.close()
+
+    def screenshot(self, width=None, height=None):
+        image = None
+        try:
+            scr = self.dsp.screen()
+            width = width if width else scr.width_in_pixels
+            height = height if height else scr.height_in_pixels
+            root = scr.root
+            raw = root.get_image(0, 0, width, height, X.ZPixmap, 0xffffffff)
+            image = Image.frombytes("RGB", (width, height), raw.data, "raw", "BGRX")
+        finally:
+            self.dsp.close()
+        return image
