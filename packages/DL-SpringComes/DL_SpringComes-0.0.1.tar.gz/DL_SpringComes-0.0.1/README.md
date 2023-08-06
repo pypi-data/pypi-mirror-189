97ef03bcc8d9c357cc49e8f9ea40907784e93d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:47 +0000

    Add package DL-SpringComes/0.0.1/DL_SpringComes-0.0.1.tar.gz

---
+++ packages/DL-SpringComes/DL_SpringComes-0.0.1.tar.gz/DL_SpringComes-0.0.1/README.md
@@ -0,0 +1,20 @@
+# 深度学习库
+
+### 安装环境
+
+> + torch深度学习框架
+> + matplotlib等可视化工具
+
+### 功能
+
++ 导入数据集
+
+### 版本
+
++ v1.0 实现方便使用深度学习框架的API
+
+### github代码库
+
+[TheSpringComes](https://github.com/TheSpringComes)
+
+### 作者: Jing
---
+++ packages/DL-SpringComes/DL_SpringComes-0.0.1.tar.gz/DL_SpringComes-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="DL_SpringComes",
+    version="0.0.1",
+    author="TheSpringComes",
+    author_email="3034547573@qq.com",
+    description="",
+    long_description="text/markdown",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/TheSpringComes",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
