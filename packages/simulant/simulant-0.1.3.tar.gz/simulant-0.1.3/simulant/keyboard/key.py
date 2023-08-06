c677da88dd217a9b5bfa429b012ae40b3e7e5fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:20 +0000

    Add package simulant/0.1.3/simulant-0.1.3.tar.gz

---
+++ packages/simulant/simulant-0.1.3.tar.gz/simulant-0.1.3/simulant/keyboard/key.py
@@ -0,0 +1,48 @@
+letters = {
+    'а': 'Cyrillic_a',
+    'б': 'Cyrillic_be',
+    'в': 'Cyrillic_ve',
+    'г': 'Cyrillic_ghe',
+    'д': 'Cyrillic_de',
+    'е': 'Cyrillic_ie',
+    'ё': 'Cyrillic_io',
+    'ж': 'Cyrillic_zhe',
+    'з': 'Cyrillic_ze',
+    'и': 'Cyrillic_i',
+    'й': 'Cyrillic_shorti',
+    'к': 'Cyrillic_ka',
+    'л': 'Cyrillic_el',
+    'м': 'Cyrillic_em',
+    'н': 'Cyrillic_en',
+    'о': 'Cyrillic_o',
+    'п': 'Cyrillic_pe',
+    'р': 'Cyrillic_er',
+    'с': 'Cyrillic_es',
+    'т': 'Cyrillic_te',
+    'у': 'Cyrillic_u',
+    'ф': 'Cyrillic_ef',
+    'х': 'Cyrillic_ha',
+    'ц': 'Cyrillic_tse',
+    'ч': 'Cyrillic_che',
+    'ш': 'Cyrillic_sha',
+    'щ': 'Cyrillic_shcha',
+    'ъ': 'Cyrillic_hardsign',
+    'ы': 'Cyrillic_yeru',
+    'ь': 'Cyrillic_softsign',
+    'э': 'Cyrillic_e',
+    'ю': 'Cyrillic_yu',
+    'я': 'Cyrillic_ya',
+}
+
+
+class Keys:  # todo __add__
+    F6 = 'F6'
+    ESC = 'Escape'
+    ENTER = 'Return'
+    TAB = 'Tab'
+    CTRL = 'ctrl'
+    RIGHT = 'Right'
+    LEFT = 'Left'
+    UP = 'Up'
+    DOWN = 'Down'
+    SPACE = 'space'
