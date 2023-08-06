956a1b20f548d443c8df121f7d7e225df9c7b0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:49 +0000

    Add package compose-cms/1.0.5/compose_cms-1.0.5.tar.gz

---
+++ packages/compose-cms/compose_cms-1.0.5.tar.gz/compose_cms-1.0.5/include/compose_cms/__init__.py
@@ -0,0 +1,8 @@
+from .json import *
+from .compose import Compose, ComposePackage, ComposePage, ComposeDatabase, \
+    ComposePackageConfiguration
+from .restapi import ComposeRestAPI
+from .exceptions import APIError
+
+
+__version__ = "1.0.5"
