dc74bae895b6748fd91494dfe1de296739c9aab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:13 +0000

    Add package nsense-detection/1.0.0.1/nsense_detection-1.0.0.1-py3-none-any.whl

---
+++ packages/nsense-detection/nsense_detection-1.0.0.1-py3-none-any.whl/nsense_detection/lib/__init__.py
@@ -0,0 +1,10 @@
+###########################################################################################
+# Developed by: Rafael Padilla                                                            #
+#        SMT - Signal Multimedia and Telecommunications Lab                               #
+#        COPPE - Universidade Federal do Rio de Janeiro                                   #
+#        Last modification: May 24th 2018                                                 #
+###########################################################################################
+from .utils import *
+from .BoundingBox import *
+from .BoundingBoxes import *
+from .Evaluator import *
