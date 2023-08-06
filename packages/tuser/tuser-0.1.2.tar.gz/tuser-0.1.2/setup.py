22b431f0f3bb5dd16f768591f8f7bb1636a077e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:44 +0000

    Add package tuser/0.1.2/tuser-0.1.2.tar.gz

---
+++ packages/tuser/tuser-0.1.2.tar.gz/tuser-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
+]
+
+libFolder = os.path.dirname(os.path.realpath(__file__))
+reqsFile = libFolder + "/reqs.txt"
+
+if os.path.isfile(reqsFile):
+    with open(reqsFile) as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name="tuser",
+    version="0.1.2",
+    author="Daniil Baikalov",
+    author_email="felix.trof@gmail.com",
+    description="Tuser allows you to create databases for users of your project.",
+    long_description="file: README.md",
+    long_description_content_type="text/markdown",
+    license="GNU",
+    classifiers=classifiers,
+    url="",
+    packages=find_packages(),
+    install_requires=install_requires
+)
