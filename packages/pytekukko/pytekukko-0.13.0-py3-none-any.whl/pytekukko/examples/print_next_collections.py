7621d5bcfc29cd8db7bdb921b451b750655433de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:10 +0000

    Add package pytekukko/0.13.0/pytekukko-0.13.0-py3-none-any.whl

---
+++ packages/pytekukko/pytekukko-0.13.0-py3-none-any.whl/pytekukko/examples/print_next_collections.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Print next collection dates."""
+
+import asyncio
+import json
+
+from pytekukko.examples import example_argparser, example_client
+
+
+async def run_example() -> None:
+    """Run the example."""
+    client, cookie_jar, cookie_jar_path = example_client(
+        example_argparser(__doc__).parse_args(),
+    )
+
+    data = []
+    async with client.session:
+        services = await client.get_services()
+        for service in (x for x in services if x.next_collection):
+            data.append(
+                {
+                    "name": service.name,
+                    "collection_date": service.next_collection.isoformat(),  # type: ignore[union-attr]
+                },
+            )
+        if not cookie_jar_path:
+            await client.logout()
+
+    print(json.dumps(data))  # noqa: T201
+
+    if cookie_jar_path:
+        cookie_jar.save(cookie_jar_path)
+
+
+def main() -> None:
+    """Run example in event loop."""
+    asyncio.run(run_example())
+
+
+if __name__ == "__main__":
+    main()
