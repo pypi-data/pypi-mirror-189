9db02ddfbb66ce0fdafd1bb0b846f650a6bc93cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:34 +0000

    Add package pytest-mini/0.1.9/pytest_mini-0.1.9.tar.gz

---
+++ packages/pytest-mini/pytest_mini-0.1.9.tar.gz/pytest_mini-0.1.9/pytest_mini/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : tingbai
+# @Time     : 2023/2/4 10:20
+# @File     : __init__.py
+# @Project  : PyCharm
+__version__ = "0.1.9"
+__description__ = "pytest版 微信小程序测试"
