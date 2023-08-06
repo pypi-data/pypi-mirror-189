8f81a9d34fc946a8cbaa5c168d03856d927df72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:40 +0000

    Add package screenpy/4.1.0/screenpy-4.1.0.tar.gz

---
+++ packages/screenpy/screenpy-4.1.0.tar.gz/screenpy-4.1.0/screenpy/exceptions.py
@@ -0,0 +1,43 @@
+"""
+Common exceptions for ScreenPy.
+"""
+
+
+class ScreenPyError(Exception):
+    """The base exception for all of ScreenPy."""
+
+
+class UnableToDirect(ScreenPyError):
+    """The Director cannot direct."""
+
+
+class UnableToNarrate(ScreenPyError):
+    """The Narrator cannot narrate."""
+
+
+class UnableToPerform(ScreenPyError):
+    """The Actor lacks the Ability to perform an Action."""
+
+
+class AbilityError(ScreenPyError):
+    """These errors are raised when an Ability fails in some way."""
+
+
+class ActionError(ScreenPyError):
+    """These errors are raised when an Action fails."""
+
+
+class DeliveryError(ActionError):
+    """The Action encountered an error while being performed."""
+
+
+class UnableToAct(ActionError):
+    """The Action is missing key information."""
+
+
+class QuestionError(ScreenPyError):
+    """These errors are raised when a Question fails."""
+
+
+class UnableToAnswer(QuestionError):
+    """The Question is not answerable."""
---
+++ packages/screenpy/screenpy-4.1.0.tar.gz/screenpy-4.1.0/screenpy/settings.py
@@ -0,0 +1,13 @@
+"""
+Settings that affect ScreenPy's behavior.
+"""
+
+# Log actions taken by actors. Set to False to squelch beats and asides.
+LOG_ACTIONS = True
+
+# Indent the logs to indicate encapsulation.
+INDENT_LOGS = True
+INDENT_CHAR = " "
+INDENT_SIZE = 4
+
+TIMEOUT = 20
