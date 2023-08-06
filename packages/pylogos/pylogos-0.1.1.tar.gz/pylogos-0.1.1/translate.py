73ed7d3f403ccb3fd727755edc98de6a24cec5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:47 +0000

    Add package pylogos/0.1.1/pylogos-0.1.1.tar.gz

---
+++ packages/pylogos/pylogos-0.1.1.tar.gz/pylogos-0.1.1/translate.py
@@ -0,0 +1,11 @@
+from src.algorithm.MRP import MRP
+from src.query_graph.koutrika_query_graph import Query_graph
+
+
+def translate(query_graph: Query_graph) -> str:
+    mrp = MRP
+    translated_nl = mrp(query_graph.query_subjects[0], None, None, query_graph.simplified_graph)
+    return translated_nl
+
+if __name__ == "__main__":
+    pass
