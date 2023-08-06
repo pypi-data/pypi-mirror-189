e8a818c4ff4f5df1a93f8e9ccb621902e51f3171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:47 +0000

    Add package ITA-test3/2.1/ITA-test3-2.1.tar.gz

---
+++ packages/ITA-test3/ITA-test3-2.1.tar.gz/ITA-test3-2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='ITA-test3',
+    version='2.1',
+    description='Italian tweets analyzer is a tool created for the thesis work at the University of Bari \"Aldo Moro\" '
+                'of the course \"Methods for the information retrieval\". It can perform analysis on Italian tweets and it provides several features'
+                'This project is an upgraded version of the tool: Hate-Tweet-Map.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    author='sdvolpicella',
+    author_email='davidevolpicella@gmail.com',
+    license='',
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['']
+)
