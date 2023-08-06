a01f18f4ed5d3fc16b58fd03759a3ebb13549041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:14 +0000

    Add package abba-models/0.0.4/abba_models-0.0.4.tar.gz

---
+++ packages/abba-models/abba_models-0.0.4.tar.gz/abba_models-0.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="abba_models",
+    version="0.0.4",
+    license="MIT",
+    author="ABBA",
+    author_email="",
+    description="abba model",
+    url="",
+    packages=setuptools.find_packages(),
+    package_data={"":["**/*"]},
+    install_requires=open("requirements.txt", "r").read().splitlines(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+
+)
