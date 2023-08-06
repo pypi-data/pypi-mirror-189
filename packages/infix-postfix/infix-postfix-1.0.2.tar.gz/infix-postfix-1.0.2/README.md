99b8f8d8c8e6b4453c829e9be3ff080fed819051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:53 +0000

    Add package infix-postfix/1.0.2/infix-postfix-1.0.2.tar.gz

---
+++ packages/infix-postfix/infix-postfix-1.0.2.tar.gz/infix-postfix-1.0.2/README.md
@@ -0,0 +1,39 @@
+<div align="center">
+<image src="https://github.com/SoumadeepChoudhury/infixpostfix/tree/main/res/banner.jpg"/>
+<p>An application to convert infix to postfix and vice versa...</p>
+</div>
+
+# Key Features
+- Runs standalone
+- Converts Infix expression to postfix expression.
+- Converts postfix expression to infix expression.
+
+
+# Basic Requirements (Pre-installed)
+- Python 3.x
+- Windows OS or MacOS or Other Linux Distribution.
+
+
+# Installation
+- Open terminal/cmd in windows/macos respectively.
+- Run the command `pip install infix-postfix`.
+
+
+# Usage
+- To convert infix to postfix, run `infix-postfix --infix ${infix_expression}`
+- To convert postfix to infix, run `infix-postfix --postfix ${postfix_expression}`
+- To run this application in your own application or python script, run
+ 
+```python
+from infix_postfix.infix_postfix import infix,postfix
+$(variable) = $(infix/postfix){expression}
+print($(variable))
+```
+
+Example :- 
+
+<image src="https://github.com/SoumadeepChoudhury/infixpostfix/tree/main/res/example.jpg">
+
+# Contribution
+Anyone is free to contribute to this open source project. 🎉🎉
+
---
+++ packages/infix-postfix/infix-postfix-1.0.2.tar.gz/infix-postfix-1.0.2/infix_postfix/infix_postfix.py
@@ -0,0 +1,89 @@
+
+class Stack:
+    def __init__(self) -> None:
+        self.STACK = list()
+
+    def isEmpty(self) -> bool:
+        if self.STACK == []:
+            return True
+        return False
+
+    def push(self, item: str) -> None:
+        self.STACK.append(item)
+
+    def pop(self) -> str:
+        if self.isEmpty():
+            return "Underflow"
+        item: str = self.STACK.pop()
+        return item
+
+    def peek(self) -> str:
+        if self.isEmpty():
+            return "Underflow"
+        else:
+            return self.STACK[-1]
+
+
+def isHigherPrecedence(val1: str, val2: str) -> bool:
+    """Returns True if 1st value have higher precedence than 2nd value."""
+    PRECEDENCE: list = ["^", "*/", "+-"]
+    val1_index: int = None
+    val2_index: int = None
+    for i in range(len(PRECEDENCE)):
+        if val1 in PRECEDENCE[i]:
+            val1_index = i
+        if val2 in PRECEDENCE[i]:
+            val2_index = i
+    if val1_index != None and val2_index != None:
+        if val1_index > val2_index:
+            return False
+        else:
+            return True
+    else:
+        return False
+
+
+def postfix(infix: str) -> str:
+    STACK: object = Stack()
+    if infix[0] != "(" or infix[-1] != ")":
+        infix = "("+infix+")"
+    postfix: str = ""
+    for i in infix:
+        if i in "(+-*/^":
+            while isHigherPrecedence(STACK.peek(), i):
+                item: str = STACK.pop()
+                if item not in ("Underflow", "(", ")"):
+                    postfix += item
+            STACK.push(i)
+        elif i == ")":
+            while STACK.peek() != "(":
+                item: str = STACK.pop()
+                if item not in ("Underflow", "(", ")"):
+                    postfix += item
+            item: str = STACK.pop()
+            if item not in ("Underflow", "(", ")"):
+                postfix += item
+        else:
+            postfix += i
+    return postfix
+
+
+def infix(postfix: str) -> str:
+    STACK: object = Stack()
+    for i in postfix:
+        if i not in "+-*/^":
+            STACK.push(i)
+        else:
+            after: str = STACK.pop()
+            before: str = STACK.pop()
+            evaluated: str = "("+before+i+after+")"
+            STACK.push(evaluated)
+    return STACK.pop()[1:-1]
+
+
+def main():
+    import sys
+    if sys.argv[1] == "--infix":
+        print(postfix(sys.argv[2]))
+    elif sys.argv[1] == "--postfix":
+        print(infix(sys.argv[2]))
