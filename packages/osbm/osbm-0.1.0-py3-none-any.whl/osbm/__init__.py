002146106a93266dec188c6ac02882cbd86b960a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:39 +0000

    Add package osbm/0.1.0/osbm-0.1.0-py3-none-any.whl

---
+++ packages/osbm/osbm-0.1.0-py3-none-any.whl/osbm/__init__.py
@@ -0,0 +1,12 @@
+"""
+This is the docs for the osbm package.
+"""
+
+import importlib
+import os
+from .utils import *
+from . import imaging
+
+module_path = __file__.split("__init__.py", maxsplit=1)[0]
+__version__ = open(module_path + "VERSION", encoding="utf-8").read().strip()
+del module_path
