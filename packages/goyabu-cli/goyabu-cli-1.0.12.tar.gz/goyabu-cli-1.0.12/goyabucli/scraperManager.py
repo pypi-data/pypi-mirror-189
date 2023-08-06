42867ec882258cc3627bf52a2aa4150781e276c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:27 +0000

    Add package goyabu-cli/1.0.12/goyabu-cli-1.0.12.tar.gz

---
+++ packages/goyabu-cli/goyabu-cli-1.0.12.tar.gz/goyabu-cli-1.0.12/goyabucli/scraperManager.py
@@ -0,0 +1,38 @@
+from typing import List, Dict
+from .scraper import Scraper,Anime,bindScrapers
+from .scrapers.goyabu import Goyabu
+from .scrapers.animefire import AnimeFire
+from .scrapers.gogoanime import Gogoanime
+
+SCRAPERS:List[Scraper] = [AnimeFire(), Goyabu(), Gogoanime()]
+
+class ScraperManager():
+    def __init__(self):
+        self.scrapers = SCRAPERS
+
+        self.animes:Dict[str,Anime] = {
+            # 'id1' : Anime('title', 'anime1')
+        }
+
+    @bindScrapers
+    def search(self, query:str, preferedScrapers=[]) -> List[Anime]:
+        scrapers = self.scrapers
+        if preferedScrapers:
+            scrapers = [scraper for scraper in self.scrapers if scraper.name in preferedScrapers]
+
+        new_animes = []
+        for scraper in scrapers:
+            for anime in scraper.search(query):
+                new_animes.append(anime)
+                self._addAnime(anime)
+
+        return new_animes
+
+    def searchLocal(self, query:str) -> List[Anime]:
+        return [Anime('title', 'anime1')]
+
+    def _addAnime(self, new_anime:Anime):
+        if new_anime.id in self.animes:
+            self.animes[new_anime.id].merge(new_anime)
+        else:
+            self.animes[new_anime.id] = new_anime
