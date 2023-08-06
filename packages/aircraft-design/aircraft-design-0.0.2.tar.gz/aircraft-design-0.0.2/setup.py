c733abdc2a3705d674590fbd7f58fd27038e387d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:06 +0000

    Add package aircraft-design/0.0.2/aircraft-design-0.0.2.tar.gz

---
+++ packages/aircraft-design/aircraft-design-0.0.2.tar.gz/aircraft-design-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+with open('README.md', 'r') as arq:
+    readme = arq.read()
+
+setup(name='aircraft-design',
+        version='0.0.2',
+        license='MIT',
+        author='NisusAerodesign Irisson Lima',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        author_email='ufsc.nisus@gmail.com',
+        keywords='aircraft design VLM vortex lattice',
+        description=u'Biblioteca para fazer análises de aeronaves',
+        packages=find_packages(),
+        install_requires=['numpy', 'matplotlib', 'scipy', 'avlwrapper'])
