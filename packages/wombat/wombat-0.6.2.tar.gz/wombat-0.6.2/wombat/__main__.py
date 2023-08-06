2d2bd6903e174489c81963c22bc668c6c848104e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:17 +0000

    Add package wombat/0.6.2/wombat-0.6.2.tar.gz

---
+++ packages/wombat/wombat-0.6.2.tar.gz/wombat-0.6.2/wombat/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""Package entry point. No CLI implemented at this point"""
+
+
+def main():
+    """An undefined CLI"""
+    pass
+
+
+if __name__ == "__main__":
+    main()
