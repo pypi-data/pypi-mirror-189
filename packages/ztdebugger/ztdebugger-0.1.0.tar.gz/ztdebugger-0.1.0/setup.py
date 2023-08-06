099ca613144664ee56a6706aa058926f7dba61f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:22 +0000

    Add package ztdebugger/0.1.0/ztdebugger-0.1.0.tar.gz

---
+++ packages/ztdebugger/ztdebugger-0.1.0.tar.gz/ztdebugger-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+setup(
+    name='ztdebugger',  # package name
+    version=VERSION,  # package version
+    description='my debugger',  # package description
+    packages=find_packages(),
+    zip_safe=False,
+    long_description="Detail description can be found at [GitHub](https://github.com/zhou6140919/debugger)",
+    install_requires=[
+        'typer',
+        'pysnooper',
+        'icecream',
+        'q',
+    ],
+)
