527d4d77c406452aab15a74676af9fe0910fff44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:27 +0000

    Add package marktplaats/0.0.2/marktplaats-0.0.2-py3-none-any.whl

---
+++ packages/marktplaats/marktplaats-0.0.2-py3-none-any.whl/marktplaats/models/listing_image.py
@@ -0,0 +1,20 @@
+class ListingImage:
+    def __init__(self, extra_small, medium, large, extra_large):
+        self.extra_small = extra_small
+        self.medium = medium
+        self.large = large
+        self.extra_large = extra_large
+
+    @classmethod
+    def parse_images(cls, data):
+        if data is None:
+            return []
+        images = []
+        for image_data in data:
+            images.append(cls(
+                image_data["extraSmallUrl"],
+                image_data["mediumUrl"],
+                image_data["largeUrl"],
+                image_data["extraExtraLargeUrl"],
+            ))
+        return images
