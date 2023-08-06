a7b9502d7b811d43668bb9b0c0eca6d4b2a6939a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:22 +0000

    Add package calou/0.1/calou-0.1.tar.gz

---
+++ packages/calou/calou-0.1.tar.gz/calou-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='calou',
+    version='0.1',
+    author='Rakwan',
+    description='SimpleCal',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent"
+    ]
+)
