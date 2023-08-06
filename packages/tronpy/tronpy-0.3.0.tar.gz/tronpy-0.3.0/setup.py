7bed048cb39568d03c8c3ce9fbd45de73593bbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:32 +0000

    Add package tronpy/0.3.0/tronpy-0.3.0.tar.gz

---
+++ packages/tronpy/tronpy-0.3.0.tar.gz/tronpy-0.3.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+packages = ["tronpy", "tronpy.keys", "tronpy.providers"]
+
+package_data = {"": ["*"]}
+
+install_requires = [
+    "base58",
+    "coincurve",
+    "eth_abi>=4.0.0a,<5.0.0",
+    "httpx",
+    "pycryptodome<4",
+    "requests",
+]
+
+setup_kwargs = {
+    "name": "tronpy",
+    "version": "0.3.0",
+    "description": "TRON Python client library",
+    "long_description": open("README.md").read(),
+    "long_description_content_type": "text/markdown",
+    "author": "andelf",
+    "author_email": "andelf@gmail.com",
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": "https://github.com/andelf/tronpy",
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.7",
+}
+
+
+setup(**setup_kwargs)
