549d5ae001b9accd2fd1eea94668ff7bdf184aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:40 +0000

    Add package plotly-resampler/0.8.1/plotly_resampler-0.8.1-cp310-cp310-manylinux_2_35_x86_64.whl

---
+++ packages/plotly-resampler/plotly_resampler-0.8.1-cp310-cp310-manylinux_2_35_x86_64.whl/plotly_resampler/aggregation/__init__.py
@@ -0,0 +1,18 @@
+"""
+Compatible implementation for various downsample methods and open interface to 
+other downsample methods.
+
+"""
+
+__author__ = "Jonas Van Der Donckt"
+
+
+from .aggregation_interface import AbstractSeriesAggregator
+from .aggregators import (
+    LTTB,
+    EfficientLTTB,
+    EveryNthPoint,
+    FuncAggregator,
+    MinMaxAggregator,
+    MinMaxOverlapAggregator,
+)
