e52a4cf186cc22f8a7c7fc132abc4222b95818ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:20 +0000

    Add package pyneorain/1.0.1/pyneorain-1.0.1.tar.gz

---
+++ packages/pyneorain/pyneorain-1.0.1.tar.gz/pyneorain-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='pyneorain',
+    version='1.0.1',
+    py_modules=['pyneorain'],
+    install_requires=[
+        'numpy',
+        'blessed',
+        'six',
+        'wcwidth'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyneorain = pyneorain:main'
+        ]
+    },
+    author='Ankit Gupta',
+    author_email='ankitgupta.work@gmail.com',
+    license='MIT',
+    description='Simple Matrix Rain',
+)
