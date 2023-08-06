2f6d9759ae57bb1d5a3ee72903ea7ae7a0b0dbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:08 +0000

    Add package ipsocgen/0.0.1/ipsocgen-0.0.1.tar.gz

---
+++ packages/ipsocgen/ipsocgen-0.0.1.tar.gz/ipsocgen-0.0.1/README.md
@@ -0,0 +1,7 @@
+## Instructions
+
+```bash
+python3 -m venv venv
+source venv/bin/activate 
+pip install -Ur requirements.txt
+```
---
+++ packages/ipsocgen/ipsocgen-0.0.1.tar.gz/ipsocgen-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Generic SoC builder in HDL'
+LONG_DESCRIPTION = 'Build SoCs (System-on-Chip) and MPSoCs (Multi-Processor) through yaml configuration files.'
+
+# Setting up
+setup(
+    name="ipsocgen",
+    version=VERSION,
+    author="aignacio (Anderson Ignacio)",
+    author_email="<anderson@aignacio.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pytest'],
+    keywords=['soc', 'hdl', 'builder'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
