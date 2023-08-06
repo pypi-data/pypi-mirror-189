365c637fd5ee4a150c19f1442c4d20af52c5bf50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:07 +0000

    Add package medutils/0.1.20/medutils-0.1.20.tar.gz

---
+++ packages/medutils/medutils-0.1.20.tar.gz/medutils-0.1.20/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="medutils", # Replace with your own username
+    version="0.1.20",
+    author="Jingnan",
+    author_email="jiajingnan2222@gmail.com",
+    description="A package of common utilities for medical images segmentation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jingnan-Jia/medutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pingouin'],
+
+)
