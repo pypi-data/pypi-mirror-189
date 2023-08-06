6f9c91439388a590d7b404dd7ec36d0dfd178807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:44 +0000

    Add package timetk/1.0.12/timetk-1.0.12.tar.gz

---
+++ packages/timetk/timetk-1.0.12.tar.gz/timetk-1.0.12/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['timetk']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'timetk',
+    'version': '1.0.12',
+    'description': 'time toolkit',
+    'long_description': '',
+    'author': 'wayfaring-stranger',
+    'author_email': 'zw6p226m@duck.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
