f9e117ff23d6e60adedc96f59645ee2a9a657650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:17 +0000

    Add package shpdf/2.0/shpdf-2.0.tar.gz

---
+++ packages/shpdf/shpdf-2.0.tar.gz/shpdf-2.0/shpdf/pad2text.py
@@ -0,0 +1,14 @@
+""" This Module provides functions to convert a PDF to text. """
+
+
+def convert():
+    """ 
+    Convert the given PDF to text. 
+
+    Parameters:
+    Path(str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
