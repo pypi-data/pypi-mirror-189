a03e2875fd5e3c5ddd8f6327cd1dab963888bc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:52 +0000

    Add package backup-github/0.2.0/backup_github-0.2.0-py3-none-any.whl

---
+++ packages/backup-github/backup_github-0.2.0-py3-none-any.whl/backup_github/main.py
@@ -0,0 +1,23 @@
+import argparse
+import logging
+import sys
+
+from backup import Backup
+from parse_args import parse_args
+
+logging.basicConfig(level=logging.NOTSET)
+
+if __name__ == "__main__":
+    parsed_args = None
+    backup = None
+    try:
+        parsed_args = parse_args(sys.argv[1:])
+        backup = Backup(parsed_args.token, parsed_args.organization, parsed_args.output_dir, parsed_args.repository)
+        backup.backup_members()
+        backup.backup_repositories()
+        backup.backup_issues()
+        backup.backup_pulls()
+    except argparse.ArgumentError as e:
+        logging.error(e.message)
+    except AttributeError as e:
+        logging.error(e)
