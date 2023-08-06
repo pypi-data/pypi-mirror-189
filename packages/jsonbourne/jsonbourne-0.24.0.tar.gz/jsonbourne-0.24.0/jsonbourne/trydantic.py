48a1e654c24f283c2898efc3864cefeae06937fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:11 +0000

    Add package jsonbourne/0.24.0/jsonbourne-0.24.0.tar.gz

---
+++ packages/jsonbourne/jsonbourne-0.24.0.tar.gz/jsonbourne-0.24.0/jsonbourne/trydantic.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""trydantic = try + pydantic"""
+from __future__ import annotations
+
+from dataclasses import (
+    MISSING,
+    Field,
+    FrozenInstanceError,
+    InitVar,
+    asdict,
+    astuple,
+    field,
+    fields,
+    is_dataclass,
+    make_dataclass,
+    replace,
+)
+
+PYDANTIC_AVAILABLE: bool = False
+
+try:
+    # use `pydantic.dataclasses.dataclass` if available
+    from pydantic.dataclasses import dataclass as dataclass
+
+    PYDANTIC_AVAILABLE = True
+except ImportError:
+    from dataclasses import dataclass as _dataclass
+    from functools import wraps
+
+    dataclass = wraps(_dataclass)(
+        lambda *args, **kwargs: _dataclass(
+            *args, **{k: v for k, v in kwargs.items() if k != "config"}
+        )
+    )
+
+__all__ = (
+    "Field",
+    "FrozenInstanceError",
+    "InitVar",
+    "MISSING",
+    "PYDANTIC_AVAILABLE",
+    "asdict",
+    "astuple",
+    "dataclass",
+    "field",
+    "fields",
+    "is_dataclass",
+    "make_dataclass",
+    "replace",
+)
