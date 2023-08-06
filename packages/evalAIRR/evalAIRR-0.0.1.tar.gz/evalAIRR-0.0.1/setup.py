f5aacf42d8ece5fe59425a83fc441f0fedea5d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:56 +0000

    Add package evalAIRR/0.0.1/evalAIRR-0.0.1.tar.gz

---
+++ packages/evalAIRR/evalAIRR-0.0.1.tar.gz/evalAIRR-0.0.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Comparison of real and simulated AIRR datasets'
+
+setup(
+    name="evalAIRR",
+    version=VERSION,
+    author="Lukas Sparnauskas",
+    author_email="<lukas.11sp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    include_package_data=True,
+    license='MIT',
+    packages=find_packages(),
+    keywords=['python', 'airr', 'simulated data'],
+    entry_points={'console_scripts': ['evalairr=evalairr.main:run']},
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.10",
+        "Environment :: Console",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
