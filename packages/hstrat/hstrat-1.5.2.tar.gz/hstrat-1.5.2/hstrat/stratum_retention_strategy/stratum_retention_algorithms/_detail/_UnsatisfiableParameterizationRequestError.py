dfe6ebd4efcc5726507c3ef73a531e1ae6853d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:29 +0000

    Add package hstrat/1.5.2/hstrat-1.5.2.tar.gz

---
+++ packages/hstrat/hstrat-1.5.2.tar.gz/hstrat-1.5.2/hstrat/stratum_retention_strategy/stratum_retention_algorithms/_detail/_UnsatisfiableParameterizationRequestError.py
@@ -0,0 +1,8 @@
+class UnsatisfiableParameterizationRequestError(ValueError):
+    """Raised when parameter with required properties cannot be found.
+
+    May be due to inexistance of satisfactory parameter or due to violation of
+    assumed monotonic relationship between parameter and focal property.
+    """
+
+    pass
