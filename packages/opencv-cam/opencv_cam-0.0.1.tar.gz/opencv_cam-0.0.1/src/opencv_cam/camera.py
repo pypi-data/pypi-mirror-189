03d9b442da8ac4e7f670035b3f188cf229116205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:12 +0000

    Add package opencv-cam/0.0.1/opencv_cam-0.0.1.tar.gz

---
+++ packages/opencv-cam/opencv_cam-0.0.1.tar.gz/opencv_cam-0.0.1/src/opencv_cam/camera.py
@@ -0,0 +1,17 @@
+import cv2
+from imutils.video import VideoStream
+import imutils
+
+def cam(rtsp):
+    vid = VideoStream(rtsp).start()
+
+    while(True):
+        frame = vid.read()
+        frame = imutils.resize(frame, width=800)
+        cv2.line(frame, (0, 180), (800,180), (0, 255, 255), 1)
+        cv2.line(frame, (0, 320), (800,320), (0, 0, 255), 1)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    vid.release()
+    cv2.destroyAllWindows()
