572fe0b8539c1226a97c41f197225bbe3996e6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:44 +0000

    Add package discordreq99/0.0.0/discordreq99-0.0.0.tar.gz

---
+++ packages/discordreq99/discordreq99-0.0.0.tar.gz/discordreq99-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    name="discordreq99",
+    author="decan",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
