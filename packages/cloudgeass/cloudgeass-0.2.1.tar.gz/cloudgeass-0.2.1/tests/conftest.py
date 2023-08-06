a956cc8ec1426afc93c6e80bdc3d281400d170fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:33 +0000

    Add package cloudgeass/0.2.1/cloudgeass-0.2.1.tar.gz

---
+++ packages/cloudgeass/cloudgeass-0.2.1.tar.gz/cloudgeass-0.2.1/tests/conftest.py
@@ -0,0 +1,13 @@
+"""
+SCRIPT: conftest.py
+
+CONTEXTO:
+---------
+Arquivo de conferência/configuração do pytest para
+alocação de fixtures e outros elementos e insumos
+utilizados durante a execução dos testes.
+---------------------------------------------------
+"""
+
+# Importando bibliotecas
+import cloudgeass
---
+++ packages/cloudgeass/cloudgeass-0.2.1.tar.gz/cloudgeass-0.2.1/tests/test_s3.py
@@ -0,0 +1,25 @@
+"""
+SCRIPT: test_s3.py
+
+OBJETIVO:
+---------
+Consolidar uma suíte de testes capaz de testar e validar
+funcionalidades presentes no módulo s3 do cloudgeass.
+------------------------------------------------------
+
+------------------------------------------------------
+---------- 1. PREPARAÇÃO INICIAL DO SCRIPT -----------
+          1.1 Importação das bibliotecas
+---------------------------------------------------"""
+
+# Importando módulos para uso
+
+
+"""---------------------------------------------------
+------------ 2. DEFININDO SUÍTE DE TESTES ------------
+           2.1 Construindo testes unitários
+---------------------------------------------------"""
+
+
+def test_dummy():
+    assert 1 == 1
