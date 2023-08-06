955523be5b3198e4003f77c88c0467e8c13ed0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:49 +0000

    Add package yes-no-very-good/1.9/yes-no-very-good-1.9.tar.gz

---
+++ packages/yes-no-very-good/yes-no-very-good-1.9.tar.gz/yes-no-very-good-1.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='yes-no-very-good',
+version='1.9',
+description='testing',
+author='cracker911181',
+url='https://github.com/cracker911181/cracker_say',
+packages=['cracker_say'],
+
+zip_safe=False)
