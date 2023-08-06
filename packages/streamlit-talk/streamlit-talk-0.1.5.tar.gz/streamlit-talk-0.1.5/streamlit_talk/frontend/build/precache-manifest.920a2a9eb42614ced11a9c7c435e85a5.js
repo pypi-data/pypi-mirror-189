9d682db8f8c2b3a2227000dc7dc08ee412c05c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:39 +0000

    Add package streamlit-talk/0.1.5/streamlit-talk-0.1.5.tar.gz

---
+++ packages/streamlit-talk/streamlit-talk-0.1.5.tar.gz/streamlit-talk-0.1.5/streamlit_talk/frontend/build/precache-manifest.920a2a9eb42614ced11a9c7c435e85a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e39269bfdea938b3d9e212f3856651",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a0a9048f349e5857a5f1",
+    "url": "./static/js/2.5fcf9f35.chunk.js"
+  },
+  {
+    "revision": "b015924f6369f745e996360942575eaa",
+    "url": "./static/js/2.5fcf9f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8e2a5d87db0cb7e561",
+    "url": "./static/js/main.40f1344c.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
