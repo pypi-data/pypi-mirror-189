0396370d2f428a0e55788817a597b90c5d2fd300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:57 +0000

    Add package linelib/2.2.1/linelib-2.2.1.tar.gz

---
+++ packages/linelib/linelib-2.2.1.tar.gz/linelib-2.2.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='linelib',
+    version='2.2.1',
+    packages=['linelib', 'linelib.notify', 'linelib.ext', 'linelib.connect', 'linelib.model'],
+    license='MIT',
+    description="The solution to simplicity.",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    author='AWeirdScratcher',
+    author_email = "aweirdscrather@gmail.com",
+    install_requires=[
+        'httpx', 'urllib3', 'flask', 'flask-cors', 'termcolor', 'tqdm'
+    ],
+    keywords = ['line', 'bot', 'line bot', 'sdk', 'line notify', 'commands'],
+    url="https://github.com/AWeirdScratcher/linelib",
+    classifiers=[
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10'
+  ]
+)
