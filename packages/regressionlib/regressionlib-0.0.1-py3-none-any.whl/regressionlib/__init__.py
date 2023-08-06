3aa89b781c3b9a8247e563ae40e3cb8eb3d56b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:44 +0000

    Add package regressionlib/0.0.1/regressionlib-0.0.1-py3-none-any.whl

---
+++ packages/regressionlib/regressionlib-0.0.1-py3-none-any.whl/regressionlib/__init__.py
@@ -0,0 +1,3 @@
+from regressionlib.experiments import RA_univariate_linear, RA_multivariate_linear, RA_univariate_polynomial, \
+    RA_Forsythe_non_uniform, RA_Forsythe_uniform, RA_Forsythe_polynomial, RA_Forsythe_detx, RA_Forsythe_virtual_to_real, \
+    RA_Forsythe_detx_real_to_virtual, RA_to_float
