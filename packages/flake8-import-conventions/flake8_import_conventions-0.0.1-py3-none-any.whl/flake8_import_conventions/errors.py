6b79f2891dd06d565c748b74becce6058b0f7da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:07 +0000

    Add package flake8-import-conventions/0.0.1/flake8_import_conventions-0.0.1-py3-none-any.whl

---
+++ packages/flake8-import-conventions/flake8_import_conventions-0.0.1-py3-none-any.whl/flake8_import_conventions/errors.py
@@ -0,0 +1,22 @@
+from typing import Dict
+
+
+def generate_message(number: int, package_name: str, alias: str) -> str:
+    pad_number = str(number).zfill(3)
+    import_convention = f"import {package_name} as {alias}"
+
+    return f"IC{pad_number} {package_name} should be imported as `{import_convention}`"
+
+
+# Alphabetical order
+name2asname: Dict[str, str] = {
+    "altair": "alt",
+    "numpy": "np",
+    "pandas": "pd",
+    "seaborn": "sns",
+}
+
+name2message: Dict[str, str] = {
+    key: generate_message(idx + 1, key, value)
+    for idx, (key, value) in enumerate(name2asname.items())
+}
