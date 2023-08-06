4cafacef22a10ad4fdb56dc24d653878d23dac23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:30 +0000

    Add package topicmodeltuner/0.3.4/topicmodeltuner-0.3.4-py3-none-any.whl

---
+++ packages/topicmodeltuner/topicmodeltuner-0.3.4-py3-none-any.whl/topictuner/__init__.py
@@ -0,0 +1,11 @@
+from loguru import logger
+from topictuner.basetuner import BaseHDBSCANTuner
+from topictuner.topictuner import TopicModelTuner
+try:
+    from topictuner.cuml_topictuner import cumlTopicModelTuner
+except ImportError:
+    logger.info('cuML not present - cumlTopicModelTuner not avaialable')
+
+__version__ = '0.3.4'
+
+__all__ = ['TopicModelTuner', 'BaseHDBSCANTuner', 'cumlTopicModelTuner']
