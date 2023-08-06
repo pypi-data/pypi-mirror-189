6d1908aea9c3fa40f3bc9cf45cb69675f0ab4b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:51 +0000

    Add package cracker-saying/3.1/cracker-saying-3.1.tar.gz

---
+++ packages/cracker-saying/cracker-saying-3.1.tar.gz/cracker-saying-3.1/README.md
@@ -0,0 +1,34 @@
+## cracker_say PIP by cracker911181
+```
+
+This PIP only Made For Design Termux-Tool and Termux-Welcome Banner.
+
+[+] You Can Install By Bellow Command:
+
+$ pip install cracker-saying
+
+
+
+[+] Pypi Project Link:
+
+https://pypi.org/project/cracker-saying/
+
+
+[+] Importing pip
+
+import cracker_say
+
+
+
+
+Owner:
+------
+
+Owner: cracker911181
+Facebook: https:/facebook.com/cracker911181
+Telegram: https://t.me/cracker911181
+Github: https://github.com/cracker911181
+Email: admin@cracker911181.cf
+Website: https://www.cracker911181.cf
+
+```
---
+++ packages/cracker-saying/cracker-saying-3.1.tar.gz/cracker-saying-3.1/cracker_say/cracker_say.py
@@ -0,0 +1,178 @@
+
+# Coded by Cracker
+# CRACKER911181
+import os
+
+def  cow(spch):
+	
+	set1=[]
+	set2=[]
+	
+	
+	
+	ln=len(spch)
+	for f in range(ln+2):
+		set1.append("=")
+	for f in range(ln+2):
+	
+		set2.append("=")
+		
+	
+	xc=str(set1)
+	xc1=str(set2)
+	
+	xx=xc.replace("'","")
+	xx1=xx.replace(",","")
+	xx2=xx1.replace("[","")
+	xx3=xx2.replace("]","")
+	xx4=xx3.replace(" ","")
+	
+	
+	xx2=xc1.replace("'","")
+	xx21=xx2.replace(",","")
+	xx22=xx21.replace("[","")
+	xx23=xx22.replace("]","")
+	xx24=xx23.replace(" ","")
+	
+	spech=str(" "+xx4+"\n | "+spch+" |\n  "+xx24)
+	
+	
+	cow=("""\n
+ """+spech+"""
+         \   
+          \\
+             ^__^ 
+             (oo)\_______
+             (__)\       )\/\\ 
+                 ||----w |
+                 ||     ||
+    """)
+	
+	print(cow)
+
+
+def rabbit(spch):
+	import os
+	
+	set1=[]
+	set2=[]
+	
+	
+	
+	ln=len(spch)
+	for f in range(ln+2):
+		set1.append("=")
+	for f in range(ln+2):
+	
+		set2.append("=")
+		
+	
+	xc=str(set1)
+	xc1=str(set2)
+	
+	xx=xc.replace("'","")
+	xx1=xx.replace(",","")
+	xx2=xx1.replace("[","")
+	xx3=xx2.replace("]","")
+	xx4=xx3.replace(" ","")
+	
+	
+	xx2=xc1.replace("'","")
+	xx21=xx2.replace(",","")
+	xx22=xx21.replace("[","")
+	xx23=xx22.replace("]","")
+	xx24=xx23.replace(" ","")
+	
+	spech=str(" "+xx4+"\n | "+spch+" |\n  "+xx24)
+	
+	
+	rabbit=("""\n                        `.         ,-,
+                        ` `.    ,;' / 
+                         `.  ,'/ .'
+                          `. X /.'
+                .-;--''--.._` ` (
+              .'            /   `
+             ,           ` '   Q '
+             ,         ,   `._    \    """+xx4+"""
+          ,.|         '     `-.;_'  →| """+spch+""" |
+          :  . `  ;    `  ` --,.._;    """+xx24+"""
+           ' `    ,   )   .'
+              `._ ,  '   /_
+                 ; ,''-,;' ``-
+                  ``-..__``--`""")
+	print(rabbit)
+
+
+import os
+
+
+colouroff="\x1b[00m" #colour off
+
+red="\x1b[04m" #red
+green="\x1b[92m" #green
+yellow="\x1b[93m" #yellow
+blue="\x1b[94m" #blue
+rosy="\x1b[95m" #rosy
+pest="\x1b[96m" #pest
+
+red1="\x1b[41m" #red1
+
+
+
+def  bennar(spchx):
+	
+	set1=[]
+	set2=[]
+	
+	spch=str("Hello "+str(spchx))
+	ln=len(spch)
+	for f in range(ln+2):
+		set1.append("=")
+	for f in range(ln+2):
+	
+		set2.append("=")
+		
+	
+	xc=str(set1)
+	xc1=str(set2)
+	
+	xx=xc.replace("'","")
+	xx1=xx.replace(",","")
+	xx2=xx1.replace("[","")
+	xx3=xx2.replace("]","")
+	xx4=xx3.replace(" ","")
+	
+	
+	xx2=xc1.replace("'","")
+	xx21=xx2.replace(",","")
+	xx22=xx21.replace("[","")
+	xx23=xx22.replace("]","")
+	xx24=xx23.replace(" ","")
+	
+	spch1="Welcome To Termux"
+	
+	spech=str(" "+xx4+"\n | "+spch+" |\n  "+xx24)
+	spech1=str("	   	 +++++++++++++++++++\n		| "+spch1+" |\n	    	 +++++++++++++++++++")
+	
+
+
+	bennar=(pest+"""\n
+ """+spech+"""
+         /
+        /
+  ,   ----    , 
+ /             \ 
+((__---,,,---__))   """+colouroff+red1+"""developed by cracker911181"""+colouroff+pest+"""
+   (_) O O (_)_________
+      \ _ /            |\ """+yellow+"""
+       o_o \  CRACKER  | \ 
+            \   _____  |  *
+             |||   WW||| 
+             |||     |||\n"""+pest+"""
+ """+spech1+"")
+ 
+ 
+	print(bennar)
+	
+
+
