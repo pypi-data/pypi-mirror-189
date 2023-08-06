39868ca120411cf14aba29b1891bd064b2e9f648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:25 +0000

    Add package CustomIntents/0.8.1/CustomIntents-0.8.1.tar.gz

---
+++ packages/CustomIntents/CustomIntents-0.8.1.tar.gz/CustomIntents-0.8.1/CustomIntents/__init__.py
@@ -0,0 +1,6 @@
+from .Chatbot import ChatBot
+from .JsonIntents import JsonIntents
+from .PLinearRegression import PLinearRegression
+from .BinaryImageClassificator import BinaryImageClassificator
+from .ImageClassificator import ImageClassificator
+from .StyleTransformer import StyleTransformer
