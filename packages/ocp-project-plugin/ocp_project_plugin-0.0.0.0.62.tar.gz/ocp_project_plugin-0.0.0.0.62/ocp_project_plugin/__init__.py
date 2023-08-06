adab090f6d182d50b2214862bb7eae3bacfe32d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:28 +0000

    Add package ocp-project-plugin/0.0.0.0.62/ocp_project_plugin-0.0.0.0.62.tar.gz

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.62.tar.gz/ocp_project_plugin-0.0.0.0.62/ocp_project_plugin/__init__.py
@@ -0,0 +1,18 @@
+from extras.plugins import PluginConfig
+
+__version__ = "0.0.0.0.62"
+
+
+class OCPProjectConfig(PluginConfig):
+    name = "ocp_project_plugin"
+    verbose_name = "OCP Project Plugin"
+    description = "The netbox ocp project plugin, for creating ocp projects"
+    min_version = "3.4.0"
+    version = __version__
+    author = "Tim Rhomberg"
+    author_email = "timrhomberg@hotmail.com"
+    required_settings = ["gitlab_project_url", "values_path", "default_access_token"]
+    base_url = "ocp-project-plugin"
+
+
+config = OCPProjectConfig
