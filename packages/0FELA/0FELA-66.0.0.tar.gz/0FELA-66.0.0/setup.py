b3e403b0f65550a107c0c95579d8416d05340400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:48 +0000

    Add package 0FELA/66.0.0/0FELA-66.0.0.tar.gz

---
+++ packages/0FELA/0FELA-66.0.0.tar.gz/0FELA-66.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='0FELA',
+   version='66.0.0',
+   author='Yandex',
+   author_email='security@yandex-team.ru',
+   url='https://ya.ru',
+   description='A package to prevent Dependency Confusion attacks against Yandex',
+   )
