4968f49c3c5f3adc8b7f1dab380c506005bc8870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:24 +0000

    Add package mqt.ddsim/1.14.0/mqt.ddsim-1.14.0.tar.gz

---
+++ packages/mqt.ddsim/mqt.ddsim-1.14.0.tar.gz/mqt.ddsim-1.14.0/test/python/unitarysimulator/test_standalone_unitary_simulator.py
@@ -0,0 +1,48 @@
+import unittest
+
+import numpy as np
+from qiskit import *
+
+from mqt import ddsim
+
+
+class MQTStandaloneUnitarySimulatorTests(unittest.TestCase):
+    def setUp(self) -> None:
+        circ = QuantumCircuit(3)
+        circ.h(0)
+        circ.cx(0, 1)
+        circ.cx(0, 2)
+        self.circuit = circ
+        self.unitary = np.zeros((2 ** circ.num_qubits, 2 ** circ.num_qubits), dtype=complex)
+
+    def test_standalone_sequential_mode(self):
+        sim = ddsim.UnitarySimulator(self.circuit, mode=ddsim.ConstructionMode.sequential)
+        sim.construct()
+
+        ddsim.get_matrix(sim, self.unitary)
+        print(self.unitary)
+        self.assertEqual(np.count_nonzero(self.unitary), 16)
+
+    def test_standalone_sequential_mode_with_seed(self):
+        sim = ddsim.UnitarySimulator(self.circuit, seed=1337, mode=ddsim.ConstructionMode.sequential)
+        sim.construct()
+
+        ddsim.get_matrix(sim, self.unitary)
+        print(self.unitary)
+        self.assertEqual(np.count_nonzero(self.unitary), 16)
+
+    def test_standalone_recursive_mode(self):
+        sim = ddsim.UnitarySimulator(self.circuit, mode=ddsim.ConstructionMode.recursive)
+        sim.construct()
+
+        ddsim.get_matrix(sim, self.unitary)
+        print(self.unitary)
+        self.assertEqual(np.count_nonzero(self.unitary), 16)
+
+    def test_standalone_recursive_mode_with_seed(self):
+        sim = ddsim.UnitarySimulator(self.circuit, seed=1337, mode=ddsim.ConstructionMode.recursive)
+        sim.construct()
+
+        ddsim.get_matrix(sim, self.unitary)
+        print(self.unitary)
+        self.assertEqual(np.count_nonzero(self.unitary), 16)
