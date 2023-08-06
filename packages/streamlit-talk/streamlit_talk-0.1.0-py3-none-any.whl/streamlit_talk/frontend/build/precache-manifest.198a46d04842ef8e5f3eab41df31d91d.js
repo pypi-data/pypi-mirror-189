437cf22dc71e7a412eff51802bf5c07103733b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:14 +0000

    Add package streamlit-talk/0.1.0/streamlit_talk-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-talk/streamlit_talk-0.1.0-py3-none-any.whl/streamlit_talk/frontend/build/precache-manifest.198a46d04842ef8e5f3eab41df31d91d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0b11212f281033e0315f99395f2216",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76b36d0ef79e6d08b4f7",
+    "url": "./static/js/2.c7d238ca.chunk.js"
+  },
+  {
+    "revision": "b015924f6369f745e996360942575eaa",
+    "url": "./static/js/2.c7d238ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f202bff0ef9ed480f652",
+    "url": "./static/js/main.034a66d0.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
