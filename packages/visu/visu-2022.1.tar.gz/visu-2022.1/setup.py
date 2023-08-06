b71d6ec03c3186fce84a7587d745353b8b5b52c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:11 +0000

    Add package visu/2022.1/visu-2022.1.tar.gz

---
+++ packages/visu/visu-2022.1.tar.gz/visu-2022.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 14:06:58 2019
+open('README.md').read()
+@author: juliengautier
+"""
+
+from setuptools import setup, find_packages
+#import visu
+
+setup(name='visu',version=2022.10,
+      packages=find_packages(),
+      author='Julien Gautier',
+      author_email='julien.gautier@ensta.fr',
+      description='Data visualization',
+      include_package_data=True,
+      package_data={'visu': ['incons/*.*']},
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization"
+      ],
+      url='https://github.com/julienGautier77/visu',
+      install_requires=['pyqtgraph','matplotlib','PyQt6','qdarkstyle','scipy',])
