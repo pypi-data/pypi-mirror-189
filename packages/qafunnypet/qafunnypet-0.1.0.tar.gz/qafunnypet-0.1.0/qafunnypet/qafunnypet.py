bd21e99cc134d9f1e22b16d280d12cf3c8610b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:17:20 +0000

    Add package qafunnypet/0.1.0/qafunnypet-0.1.0.tar.gz

---
+++ packages/qafunnypet/qafunnypet-0.1.0.tar.gz/qafunnypet-0.1.0/qafunnypet/qafunnypet.py
@@ -0,0 +1,10 @@
+class Qafunnypet:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def present(self):
+        print(f'Hi I am {self.name} and I am {self.age} years old')
+
+    def speak(self):
+        print("I dont know what to say")
