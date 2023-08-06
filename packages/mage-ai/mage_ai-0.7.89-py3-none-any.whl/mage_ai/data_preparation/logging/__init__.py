8246fcf765f8c5b3f849d7e30200bd32d154c619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:17 +0000

    Add package mage-ai/0.7.89/mage_ai-0.7.89-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.7.89-py3-none-any.whl/mage_ai/data_preparation/logging/__init__.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from mage_ai.shared.config import BaseConfig
+from mage_ai.shared.logger import LoggingLevel
+from typing import Dict
+
+
+class LoggerType(str, Enum):
+    DEFAULT = 'file'
+    S3 = 's3'
+
+
+@dataclass
+class LoggingConfig(BaseConfig):
+    type: LoggerType = LoggerType.DEFAULT
+    level: LoggingLevel = LoggingLevel.INFO
+    destination_config: Dict = field(default_factory=dict)
