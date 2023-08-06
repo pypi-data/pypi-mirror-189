816acb4647135c8cbd11430ef0938824501edb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:25 +0000

    Add package vars/0.2/vars-0.2.tar.gz

---
+++ packages/vars/vars-0.2.tar.gz/vars-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vars',
+    version='0.2',
+    description='This script is a import and wrapper for all my variables and workflows',
+    author='var',
+    packages=find_packages(),
+)
