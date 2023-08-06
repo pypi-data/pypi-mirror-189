7f5fa5ea9454f1eb744000d07530865febde979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:47 +0000

    Add package wpscanupdate/2.8/wpscanupdate-2.8.tar.gz

---
+++ packages/wpscanupdate/wpscanupdate-2.8.tar.gz/wpscanupdate-2.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+setup(
+    name="wpscanupdate",
+    version='2.8',
+    license='Eclipse Public License 2.0',
+    authors=["pycoders6"],
+    author_email="<python356@gmail.com>",
+    long_description='Documentation: https://xvdd.com/',
+    packages=['wpscanupdate']
+)
+
+
