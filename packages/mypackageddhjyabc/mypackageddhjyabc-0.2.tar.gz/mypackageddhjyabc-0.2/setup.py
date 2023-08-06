cb4846aabe45b7f9c888214190c979df0ee322fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:23 +0000

    Add package mypackageddhjyabc/0.2/mypackageddhjyabc-0.2.tar.gz

---
+++ packages/mypackageddhjyabc/mypackageddhjyabc-0.2.tar.gz/mypackageddhjyabc-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackageddhjyabc',
+    version='0.2',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of the package',
+    long_description="A longer description of the package",
+    long_description_content_type="text/markdown",
+    url='https://github.com/ddhjy/mypackageddhjyabc',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+)
