6c1e82b000e490d1e7671ee3b04371557fb51fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:32 +0000

    Add package tid-my-classification-model/1.0.4/tid-my-classification-model-1.0.4.tar.gz

---
+++ packages/tid-my-classification-model/tid-my-classification-model-1.0.4.tar.gz/tid-my-classification-model-1.0.4/model/config/core.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+from typing import List
+
+from pydantic import BaseModel
+from strictyaml import YAML, load
+
+import model
+
+# from typing import Dict, Sequence
+
+
+# Project Directorie
+PACKAGE_ROOT = Path(model.__file__).resolve().parent
+ROOT = PACKAGE_ROOT.parent
+CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
+DATASET_DIR = PACKAGE_ROOT / "datasets"
+TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+
+
+class AppConfig(BaseModel):
+    """
+    Application-level config.
+    """
+
+    package_name: str
+    training_data_file: str
+    test_data_file: str
+    pipeline_save_file: str
+
+
+class ModelConfig(BaseModel):
+    """
+    All configuration relevant to model
+    training and feature engineering.
+    """
+
+    target: str
+    features: List[str]
+    test_size: float
+    random_state: int
+    n_jobs: int
+    criterion: str
+    max_depth: int
+    max_features: str
+    max_leaf_nodes: int
+    max_samples: int
+    n_estimators: int
+    numerical_vars: List[str]
+    bootstrap: bool
+
+
+class Config(BaseModel):
+    """Master config object."""
+
+    app_config: AppConfig
+    model_config: ModelConfig
+
+
+def find_config_file() -> Path:
+    """Get the path of the configuration file."""
+
+    if CONFIG_FILE_PATH.is_file():
+        return CONFIG_FILE_PATH
+
+    raise Exception(f"config file not found at {CONFIG_FILE_PATH!r}")
+
+
+def fetch_config_from_yaml(cfg_path: Path = None) -> YAML:
+    """Parse YAML containing the package configuration."""
+
+    if not cfg_path:
+        cfg_path = find_config_file()
+
+    if cfg_path:
+        with open(cfg_path, "r") as conf_file:
+            parsed_config = load(conf_file.read())
+            return parsed_config
+
+    raise OSError(f"Did not find config file at path: {cfg_path}")
+
+
+def create_and_validate_config(parsed_config: YAML = None) -> Config:
+    """Run validation on config values."""
+
+    if parsed_config is None:
+        parsed_config = fetch_config_from_yaml()
+
+    # specify the data attribute from the strictyaml YAML type.
+    _config = Config(
+        app_config=AppConfig(**parsed_config.data),
+        model_config=ModelConfig(**parsed_config.data),
+    )
+
+    return _config
+
+
+config = create_and_validate_config()
