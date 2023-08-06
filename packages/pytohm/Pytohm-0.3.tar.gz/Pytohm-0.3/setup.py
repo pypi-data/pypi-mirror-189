3eddbe85dd96bee4de76e8c78b630f577d53c297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:53 +0000

    Add package pytohm/0.3/Pytohm-0.3.tar.gz

---
+++ packages/pytohm/Pytohm-0.3.tar.gz/Pytohm-0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'Pytohm',
+  packages = ['Pytohm'], 
+  version = '0.3',
+  license= 'MIT',
+  description = "Pytohm is a package with Ohm's Law functions, making it easier to work with Ohm's Law in Python.",
+  author = 'Yvnee',
+  author_email = 'yeseirav@gmail.com',
+  url = 'https://github.com/user/Yvnee',
+  download_url = 'https://github.com/Yvnee/Pytohm/archive/refs/tags/0.2.tar.gz', 
+  keywords = ['OhmsLaw', 'Ohm', 'Resistance'],
+
+  install_requires=[],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6'
+  ],
+)
