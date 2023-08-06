cabe5e0461959e33e2f2d40fcbbb60a673b6b5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:55 +0000

    Add package packagesorry/0.1.2/packagesorry-0.1.2.tar.gz

---
+++ packages/packagesorry/packagesorry-0.1.2.tar.gz/packagesorry-0.1.2/ganit/__init__.py
@@ -0,0 +1,1 @@
+from ._basic.py import* 
---
+++ packages/packagesorry/packagesorry-0.1.2.tar.gz/packagesorry-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='packagesorry',
+version='0.1.2',
+description="This code with harry",
+long_description='This is very very long description',
+author='Harry',
+packages = ['math','ganit'],
+
+)
