1e4fa06f28aa72f1c2aafa61a031f4b1eaa36f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:30 +0000

    Add package riyazi/0.3/riyazi-0.3-py3-none-any.whl

---
+++ packages/riyazi/riyazi-0.3-py3-none-any.whl/riyazi/__init__.py
@@ -0,0 +1,17 @@
+# Version Import
+from .version import*
+from . import version
+from .version import __version__ , __author__ 
+
+# Math module Import 
+from . math import*
+from . import math 
+
+# constants module Import 
+
+from .constants import*
+from .import constants
+
+
+
+
