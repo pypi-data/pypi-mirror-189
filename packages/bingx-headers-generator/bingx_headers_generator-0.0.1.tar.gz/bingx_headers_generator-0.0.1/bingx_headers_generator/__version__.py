2ea4c1977a158d4b5df80340d87d83daef4237f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:55 +0000

    Add package bingx-headers-generator/0.0.1/bingx_headers_generator-0.0.1.tar.gz

---
+++ packages/bingx-headers-generator/bingx_headers_generator-0.0.1.tar.gz/bingx_headers_generator-0.0.1/bingx_headers_generator/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "bingx_headers_generator"
+__description__ = (
+    "BingX Headers Generator that allows to access most of the BingX API endpoints"
+)
+__version__ = "0.0.1"
+__author__ = "niewiemczego"
+__license__ = "MIT"
