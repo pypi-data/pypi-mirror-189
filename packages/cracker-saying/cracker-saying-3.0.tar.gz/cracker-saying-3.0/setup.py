8641482c17c000ab1fc2c5b9f66b02c6c745fdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:23 +0000

    Add package cracker-saying/3.0/cracker-saying-3.0.tar.gz

---
+++ packages/cracker-saying/cracker-saying-3.0.tar.gz/cracker-saying-3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+try:
+	import pypandoc
+	long_description = pypandoc.convert_file('README.md', 'rst')
+	long_description = long_description.replace("\r","") 
+except(IOError, ImportError):
+	long_description = open('README.md').read()
+
+setup(name='cracker-saying',
+version='3.0',
+license="GNU General Public License v3.0",
+description='A simple pip for designing & designing termux banner.',
+log_description = long_description,
+long_description_content_type='text/markdown',
+author='cracker911181',
+author_email='admin@cracker911181.cf',
+url='https://pypi.org/project/cracker-saying',
+download_url='https://pypi.org/project/cracker-saying',
+packages=['cracker_say'],
+
+zip_safe=False)
