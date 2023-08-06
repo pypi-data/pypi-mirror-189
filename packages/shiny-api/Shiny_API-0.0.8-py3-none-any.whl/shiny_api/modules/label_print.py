b5f560fd7f954fa9611b5a23ddc68dd1b22a4f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:37 +0000

    Add package shiny-api/0.0.8/Shiny_API-0.0.8-py3-none-any.whl

---
+++ packages/shiny-api/Shiny_API-0.0.8-py3-none-any.whl/shiny_api/modules/label_print.py
@@ -0,0 +1,27 @@
+"""Zebra printing module"""
+import socket
+import os
+from shiny_api.modules import load_config as config
+
+print(f"Importing {os.path.basename(__file__)}...")
+
+
+def print_text(text: str, barcode=None, quantity=None, password=None):
+    """Open socket to printer and send text"""
+    label_string = b"^XA^A0N,50,50^FO0,20^FB450,4,,C,^FD" + bytes(text, "utf-8")
+    if barcode:
+        label_string += b"^FS^FO40,130^FB450,4,,C,^B2N,40,Y,N,N^FD" + bytes(barcode, "utf-8")
+    if password:
+        label_string += b"^FS^A0N,30,30^FO0,210^FB450,4,,C,^FD" + bytes(password, "utf-8")
+    label_string += b"^FS^XZ"
+    quantity = int(quantity)
+    if quantity < 1:
+        quantity = 1
+
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    if socket.gethostname().lower() is not False:  # "chris-mbp":
+        mysocket.connect((config.PRINTER_HOST, config.PRINTER_PORT))  # connecting to host
+        for _ in range(quantity):
+            mysocket.send(label_string)  # using bytes
+        mysocket.close()  # closing connection
