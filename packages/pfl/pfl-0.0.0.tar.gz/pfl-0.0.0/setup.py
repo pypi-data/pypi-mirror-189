807397ae8b1c3b75335c53829fe0e60d37c3fe66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:59 +0000

    Add package pfl/0.0.0/pfl-0.0.0.tar.gz

---
+++ packages/pfl/pfl-0.0.0.tar.gz/pfl-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pfl',
+    version='0.0.0',
+    description='A dummy package to reserve the PyPI name for 2023',
+    packages=['pfl'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+)
+
