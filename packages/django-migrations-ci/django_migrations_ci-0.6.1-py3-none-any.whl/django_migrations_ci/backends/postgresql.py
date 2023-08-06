89b9f66dd6fba7d260cb389c99258220806dc368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:38 +0000

    Add package django-migrations-ci/0.6.1/django_migrations_ci-0.6.1-py3-none-any.whl

---
+++ packages/django-migrations-ci/django_migrations_ci-0.6.1-py3-none-any.whl/django_migrations_ci/backends/postgresql.py
@@ -0,0 +1,23 @@
+from django_migrations_ci import shell
+
+
+def dump(connection, output_file):
+    ctx, env = _ctx(connection.settings_dict)
+    pg_dump = "pg_dump --no-owner --inserts -h {host} -p {port} -U {user} -d {database} -f {output_file}"  # noqa: E501
+    shell.exec(pg_dump.format(output_file=output_file, **ctx), env)
+
+
+def _ctx(db_conf):
+    env = {"PGPASSWORD": db_conf["PASSWORD"]}
+    try:
+        database = db_conf["TEST"]["NAME"] or db_conf["NAME"]
+    except KeyError:
+        database = db_conf["NAME"]
+
+    data = {
+        "host": db_conf["HOST"],
+        "port": db_conf["PORT"],
+        "user": db_conf["USER"],
+        "database": database,
+    }
+    return data, env
