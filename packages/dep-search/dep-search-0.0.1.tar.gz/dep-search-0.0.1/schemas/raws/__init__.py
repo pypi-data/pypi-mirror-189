740541ad1af45062a4b96b81c042d628882bc2af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:00 +0000

    Add package dep-search/0.0.1/dep-search-0.0.1.tar.gz

---
+++ packages/dep-search/dep-search-0.0.1.tar.gz/dep-search-0.0.1/schemas/raws/__init__.py
@@ -0,0 +1,16 @@
+"""Common types."""
+
+from .types import (
+    Branch,
+    Commit,
+    CommitStatus,
+    DataRequest,
+    DataResponse,
+    DocState,
+    DocMerge,
+    MergeRequest,
+    QuerySchema,
+    SecureQuery,
+    ImportQuerySecureSchema,
+    StrictSecureQuerySchema,
+)
