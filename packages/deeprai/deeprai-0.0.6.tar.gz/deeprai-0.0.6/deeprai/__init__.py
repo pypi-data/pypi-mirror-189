02199fbc2308ad1ac5a1ff02496693c3b670ee1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:04 +0000

    Add package deeprai/0.0.6/deeprai-0.0.6.tar.gz

---
+++ packages/deeprai/deeprai-0.0.6.tar.gz/deeprai-0.0.6/deeprai/__init__.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Back, Style
+import os
+for file in os.listdir('.'):
+    if file.endswith('.so') or file.endswith('.pyd'):
+        src = os.path.join(os.getcwd(), file)
+        dst = os.path.join(os.getcwd(), 'deeprai/engine/cython', file)
+        os.rename(src, dst)
+print(Fore.CYAN + Style.BRIGHT  + "DeeprAI 0.0.5 BETA")
+Style.RESET_ALL
