72e218a4d36cd896e5a8087573623a052374c39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:28 +0000

    Add package basic-2d-game-engine/0.0.1/basic_2d_game_engine-0.0.1.tar.gz

---
+++ packages/basic-2d-game-engine/basic_2d_game_engine-0.0.1.tar.gz/basic_2d_game_engine-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="basic_2d_game_engine",
+    version="0.0.1",
+    author="caioslppuo",
+    description="A basic 2d game engine",
+    packages=["basic_2d_game_engine"]
+)
