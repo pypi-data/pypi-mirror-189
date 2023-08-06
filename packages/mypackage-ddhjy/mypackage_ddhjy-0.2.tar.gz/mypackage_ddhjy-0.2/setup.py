3cf229b4109ee91c5df5716cc3761441d18133d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:55 +0000

    Add package mypackage-ddhjy/0.2/mypackage_ddhjy-0.2.tar.gz

---
+++ packages/mypackage-ddhjy/mypackage_ddhjy-0.2.tar.gz/mypackage_ddhjy-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage_ddhjy',
+    version='0.2',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A short description of the package',
+    long_description="A longer description of the package",
+    long_description_content_type="text/markdown",
+    url='https://github.com/ddhjy/mypackage_ddhjy',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+    ],
+)
