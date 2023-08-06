fba693f752ad04d3e56fd1b03d448227dffb22e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:10 +0000

    Add package pytekukko/0.13.0/pytekukko-0.13.0.tar.gz

---
+++ packages/pytekukko/pytekukko-0.13.0.tar.gz/pytekukko-0.13.0/src/pytekukko/examples/print_invoice_headers.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""Print basic info on invoices."""
+
+import asyncio
+import json
+
+from pytekukko.examples import example_argparser, example_client
+
+
+async def run_example() -> None:
+    """Run the example."""
+    client, cookie_jar, cookie_jar_path = example_client(
+        example_argparser(__doc__).parse_args(),
+    )
+
+    data = []
+    async with client.session:
+        for invoice_header in await client.get_invoice_headers():
+            data.append(
+                {
+                    "name": invoice_header.name,
+                    "due_date": invoice_header.due_date.isoformat(),
+                    "total": invoice_header.total,
+                },
+            )
+        if not cookie_jar_path:
+            await client.logout()
+
+    print(json.dumps(data))  # noqa: T201
+
+    if cookie_jar_path:
+        cookie_jar.save(cookie_jar_path)
+
+
+def main() -> None:
+    """Run example in event loop."""
+    asyncio.run(run_example())
+
+
+if __name__ == "__main__":
+    main()
