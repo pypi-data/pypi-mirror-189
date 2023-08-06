a298a5e5e0f42fadf72f143a1e8456a79f5f6f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:34 +0000

    Add package rustylib/1.5.3/rustylib-1.5.3.tar.gz

---
+++ packages/rustylib/rustylib-1.5.3.tar.gz/rustylib-1.5.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt", "r") as req_file:
+    requirements = req_file.read().splitlines()
+
+setup(
+    name="rustylib",
+    version="1.5.3",
+    author="ZeyaTsu",
+    description="A comprehensive utility library for Python, featuring modules for color manipulation, mathematical operations, random number generation, HTTP requests, mathematics, and a versatile utility module. Streamline your workflow with this all-in-one toolbox.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZeyaTsu/rusty",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
