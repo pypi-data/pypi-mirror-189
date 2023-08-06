ce3b3496b41b7de55a3231c15ace30abae0b938a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:31 +0000

    Add package pretty-markdown-tables/0.1.1/pretty_markdown_tables-0.1.1.tar.gz

---
+++ packages/pretty-markdown-tables/pretty_markdown_tables-0.1.1.tar.gz/pretty_markdown_tables-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pretty_markdown_tables']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.6.0,<0.7.0',
+ 'markdown2>=2.4.7,<3.0.0',
+ 'pandas>=1.5.3,<2.0.0',
+ 'pretty-html-table>=0.9.16,<0.10.0',
+ 'pytest>=7.2.1,<8.0.0']
+
+setup_kwargs = {
+    'name': 'pretty-markdown-tables',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'vidigalp',
+    'author_email': 'vidigal.pedro@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
