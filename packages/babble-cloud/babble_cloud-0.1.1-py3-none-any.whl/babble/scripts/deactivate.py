ede1698529863ccb5e87c8023c85fbde9d2fd918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:20 +0000

    Add package babble-cloud/0.1.1/babble_cloud-0.1.1-py3-none-any.whl

---
+++ packages/babble-cloud/babble_cloud-0.1.1-py3-none-any.whl/babble/scripts/deactivate.py
@@ -0,0 +1,24 @@
+import os
+import util
+import subprocess
+
+from yaspin import yaspin
+
+projects = sorted(os.listdir("/projects"))
+
+if len(projects) == 0:
+    print("\n> no projects found\n")
+    exit()
+
+project = util.prompt(projects, "select a project", "ctrl-c to cancel")
+
+if os.path.exists(f"/projects/{project}/aws/url"):
+    print()
+    os.system("aws configure")
+    print()
+    spinner = yaspin(text="applying changes...").simpleDots
+    spinner.start()
+    subprocess.run(f"terraform -chdir=/projects/{project}/aws destroy -auto-approve", shell=True, capture_output=True)
+    spinner.stop()
+else:
+    print("\n> project not active\n")
