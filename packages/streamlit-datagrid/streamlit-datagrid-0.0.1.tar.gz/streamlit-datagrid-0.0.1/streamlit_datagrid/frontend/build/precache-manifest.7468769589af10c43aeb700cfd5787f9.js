bc5403b243828a884d55c41d6002b0edbdf9e43b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:30 +0000

    Add package streamlit-datagrid/0.0.1/streamlit-datagrid-0.0.1.tar.gz

---
+++ packages/streamlit-datagrid/streamlit-datagrid-0.0.1.tar.gz/streamlit-datagrid-0.0.1/streamlit_datagrid/frontend/build/precache-manifest.7468769589af10c43aeb700cfd5787f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e31ebb0b15e71f68de4fd02645e594",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa22445a27e2e8002c21",
+    "url": "./static/js/2.34d7e940.chunk.js"
+  },
+  {
+    "revision": "e7c852edbc87d2ac3401bc81e3bfe213",
+    "url": "./static/js/2.34d7e940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6f6bfd05b51fedaa7e",
+    "url": "./static/js/main.4cc03732.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
