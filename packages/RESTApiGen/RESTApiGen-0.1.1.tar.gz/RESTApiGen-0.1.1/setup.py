3fc2206d097152c2d8248f5b0d842aff80b2003c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:17 +0000

    Add package RESTApiGen/0.1.1/RESTApiGen-0.1.1.tar.gz

---
+++ packages/RESTApiGen/RESTApiGen-0.1.1.tar.gz/RESTApiGen-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="RESTApiGen",
+    version="0.1.1",
+    install_requires=[
+        "pymysql >= 1.0.2",
+        "python_version >= 3.10",
+        "inflector >=3.0.1",
+        "flask",
+        "flask-sqlalchemy",
+        "flask-restful",
+        "flask-blueprint",
+        "flask-marshmallow",
+        "marshmallow-sqlalchemy"
+    ],
+    entry_points = {
+        "console_scripts" : [
+            "RESTApiGen = RESTApiGen:main"
+        ]
+    }
+)
