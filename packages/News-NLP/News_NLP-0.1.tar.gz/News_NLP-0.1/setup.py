548689c283b0de1ab48babd91f966ad51ca06c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:42 +0000

    Add package News-NLP/0.1/News_NLP-0.1.tar.gz

---
+++ packages/News-NLP/News_NLP-0.1.tar.gz/News_NLP-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='News_NLP',
+version='0.1',
+description='Fetch google news, preprocess, classify news type and sentiment strength, geolocate',
+# url='#',
+author='JasonLu',
+author_email='luzhenxian@hotmail.com',
+license='MIT',
+packages=['News_NLP'],
+zip_safe=False)
