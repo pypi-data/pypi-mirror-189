741d99d08602619a8c7f7c855a5e53dc47454cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:35 +0000

    Add package changelogged/0.3.3/changelogged-0.3.3.tar.gz

---
+++ packages/changelogged/changelogged-0.3.3.tar.gz/changelogged-0.3.3/changelogger/models/domain_models.py
@@ -0,0 +1,59 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class SemVerType(Enum):
+    MAJOR = "major"
+    MINOR = "minor"
+    PATCH = "patch"
+
+
+class ReleaseNotes(BaseModel):
+    added: list[str] = []
+    changed: list[str] = []
+    deprecated: list[str] = []
+    removed: list[str] = []
+    fixed: list[str] = []
+    security: list[str] = []
+
+    def __getitem__(self, attr: str) -> list[str]:
+        return getattr(self, attr)
+
+    def __setitem__(self, attr: str, value: list[str]) -> None:
+        return setattr(self, attr, value)
+
+    def __bool__(self) -> bool:
+        return bool(
+            self.added
+            or self.changed
+            or self.deprecated
+            or self.removed
+            or self.fixed
+            or self.security
+        )
+
+    @classmethod
+    def sections(cls) -> list[str]:
+        return list(cls.__fields__.keys())
+
+    def markdown(self) -> str:
+        sections = self.dict()
+        md = ""
+        for name, notes in sections.items():
+            if not notes:
+                continue
+
+            formatted_notes = "- " + "\n- ".join(notes)
+            md += f"""#### {name.title()}
+
+            {formatted_notes}
+
+            """
+        return "\n".join(s.lstrip() for s in md.split("\n"))
+
+
+class ChangelogUpdate(BaseModel):
+    new_version: str
+    old_version: str
+    release_notes: ReleaseNotes
