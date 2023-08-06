43174ca36c66c6622907dfe6c3312de8ddf1d8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package streamlit-lchart-card/0.0.1/streamlit_lchart_card-0.0.1-py3-none-any.whl

---
+++ packages/streamlit-lchart-card/streamlit_lchart_card-0.0.1-py3-none-any.whl/streamlit_lchart_card/frontend/build/precache-manifest.6b414648eba2c8808d06c6dd50d906c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1160809e91ae7a59e7bf6bd009690597",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4f7258bebb15bd50639",
+    "url": "./static/js/2.e2d5a64e.chunk.js"
+  },
+  {
+    "revision": "89addff45cc2d342ad36a0e581d24725",
+    "url": "./static/js/2.e2d5a64e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e7f5df09d9c5407773",
+    "url": "./static/js/main.cc2a4800.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
