f1e1addf72e57545574c97ec4e66f551998f9581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:33 +0000

    Add package NuOCR/0.1.8/NuOCR-0.1.8-py3-none-any.whl

---
+++ packages/NuOCR/NuOCR-0.1.8-py3-none-any.whl/NuOCR/__init__.py
@@ -0,0 +1,5 @@
+from .extractor import Extractor
+from .io_adaptor import FTP, S3
+from .user import User
+from .util import *
+from .admin import Admin
---
+++ packages/NuOCR/NuOCR-0.1.8-py3-none-any.whl/NuOCR/gRPC_proto/__init__.py
@@ -0,0 +1,3 @@
+from NuOCR.gRPC_proto.extractor import *
+from NuOCR.gRPC_proto.io_adaptors import *
+from NuOCR.gRPC_proto.user import *
