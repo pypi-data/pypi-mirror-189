4eefd4c12337766d0a4984ad6a4fd3762f10a8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:16 +0000

    Add package simulant/0.0.6/simulant-0.0.6.tar.gz

---
+++ packages/simulant/simulant-0.0.6.tar.gz/simulant-0.0.6/simulant/keyboard/__init__.py
@@ -0,0 +1,44 @@
+import random
+import subprocess
+from time import sleep
+from .key import letters
+
+
+class Keyboard:
+    SHIFT_PRESS_TIMEOUT = .1
+    REPEAT_TIMEOUT = .1
+
+    def type(self, text):
+        codes = []
+        for symbol in text:
+            delay = random.uniform(.055, .22)
+            if symbol.lower() in list(letters.keys()):
+                letter = letters.get(symbol.lower())
+                before_press = after_press = delay / 2
+                if symbol.isupper():
+                    sleep(self.SHIFT_PRESS_TIMEOUT)
+                    subprocess.call(["simulant", "keydown", 'shift'])
+                    sleep(before_press)
+                    subprocess.call(["simulant", "keydown", letter])
+                    sleep(after_press)
+                    subprocess.call(["simulant", "keyup", letter])
+                    subprocess.call(["simulant", "keyup", 'shift'])
+                else:
+                    sleep(before_press)
+                    subprocess.call(["simulant", "keydown", letter])
+                    sleep(after_press)
+                    subprocess.call(["simulant", "keyup", letter])
+            else:
+                subprocess.call(["simulant", "type", symbol])
+                sleep(delay)
+
+        assert all(v == 0 for v in codes)
+
+    def key(self, keyboard_key, repeat=1):
+        if repeat > 1:
+            counter = repeat
+            while counter:
+                subprocess.call(["simulant", "key", keyboard_key])
+                sleep(self.REPEAT_TIMEOUT)
+        else:
+            subprocess.call(["simulant", "key", keyboard_key])
