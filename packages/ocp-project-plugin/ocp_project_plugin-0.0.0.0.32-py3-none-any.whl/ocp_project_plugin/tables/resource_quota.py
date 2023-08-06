c55a055cba5cb33b51922362a4e71b062b2e8278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:54 +0000

    Add package ocp-project-plugin/0.0.0.0.32/ocp_project_plugin-0.0.0.0.32-py3-none-any.whl

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.32-py3-none-any.whl/ocp_project_plugin/tables/resource_quota.py
@@ -0,0 +1,33 @@
+import django_tables2 as tables
+
+from netbox.tables import (
+    NetBoxTable,
+)
+from ocp_project_plugin.models import ResourceQuota
+
+
+class ResourceQuotaTable(NetBoxTable):
+    """Table for displaying Resource Quota objects."""
+
+    requests_cpu = tables.Column(
+        linkify=True
+    )
+    requests_memory = tables.Column(
+        linkify=True
+    )
+    limits_cpu = tables.Column(
+        linkify=True
+    )
+    limits_memory = tables.Column(
+        linkify=True
+    )
+    app_environment = tables.Column(
+        linkify=True
+    )
+
+    class Meta(NetBoxTable.Meta):
+        model = ResourceQuota
+
+        fields = ["requests_cpu", "requests_memory", "limits_cpu", "limits_memory", "app_environment"]
+
+        default_columns = ["requests_cpu", "requests_memory", "limits_cpu", "limits_memory", "app_environment"]
