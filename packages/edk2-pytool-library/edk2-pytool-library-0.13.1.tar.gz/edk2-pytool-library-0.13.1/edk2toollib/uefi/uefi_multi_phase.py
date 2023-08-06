1d5f2c56926a08687e9f897cd57b0d129ee55c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:25 +0000

    Add package edk2-pytool-library/0.13.1/edk2-pytool-library-0.13.1.tar.gz

---
+++ packages/edk2-pytool-library/edk2-pytool-library-0.13.1.tar.gz/edk2-pytool-library-0.13.1/edk2toollib/uefi/uefi_multi_phase.py
@@ -0,0 +1,47 @@
+# @file
+# Module contains defintions and structures from the UefiMultiPhase header file.
+#
+# Copyright (c) Microsoft Corporation
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+"""Module containing definitions and structures from the UefiMultiPhase header file."""
+
+import struct
+
+EFI_VARIABLE_NON_VOLATILE = 0x00000001
+EFI_VARIABLE_BOOTSERVICE_ACCESS = 0x00000002
+EFI_VARIABLE_RUNTIME_ACCESS = 0x00000004
+EFI_VARIABLE_HARDWARE_ERROR_RECORD = 0x00000008
+EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS = 0x00000010
+EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS = 0x00000020
+EFI_VARIABLE_APPEND_WRITE = 0x00000040
+
+
+class EfiVariableAttributes(object):
+    """Object representing the different efi variable attributes."""
+    # UINT32
+    _StructFormat = "<I"
+    _StructSize = struct.calcsize(_StructFormat)
+
+    STRING_MAP = {
+        EFI_VARIABLE_APPEND_WRITE: "EFI_VARIABLE_APPEND_WRITE",
+        EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS: "EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS",
+        EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS: "EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS",
+        EFI_VARIABLE_HARDWARE_ERROR_RECORD: "EFI_VARIABLE_HARDWARE_ERROR_RECORD",
+        EFI_VARIABLE_RUNTIME_ACCESS: "EFI_VARIABLE_RUNTIME_ACCESS",
+        EFI_VARIABLE_BOOTSERVICE_ACCESS: "EFI_VARIABLE_BOOTSERVICE_ACCESS",
+        EFI_VARIABLE_NON_VOLATILE: "EFI_VARIABLE_NON_VOLATILE",
+    }
+
+    def __init__(self, attributes=0x0000_0000):
+        """Inits with specified attributes."""
+        self.Attributes = attributes
+
+    def __str__(self):
+        """String representation of the attributes."""
+        result = []
+        for key in EfiVariableAttributes.STRING_MAP:
+            if self.Attributes & key:
+                result.append(EfiVariableAttributes.STRING_MAP[key])
+        return ",".join(result)
