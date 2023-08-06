51054e3f5e2f594a6ba4c79161bbee27e97b12ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:15 +0000

    Add package cosmo/0.7.0/cosmo-0.7.0-py3-none-any.whl

---
+++ packages/cosmo/cosmo-0.7.0-py3-none-any.whl/cosmo/__init__.py
@@ -0,0 +1,6 @@
+from .app import *
+from .html_page import html_page
+from .request import Request
+from .response import Response
+from .router import Router
+from .ssl import SSL
---
+++ packages/cosmo/cosmo-0.7.0-py3-none-any.whl/cosmo/request.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+
+class Request:
+    """A class representing an HTTP request."""
+
+    def __init__(
+        self,
+        method: str,
+        headers: dict,
+        address: tuple,
+        url_flags: Optional[dict] = None,
+        body: Optional[str] = None,
+    ):
+        self.method = method
+        self.headers = headers
+        self.address = address
+        self.url_flags = url_flags
+        self.body = body
