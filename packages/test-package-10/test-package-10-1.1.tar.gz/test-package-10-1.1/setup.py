f9720bb98616df89fceb0292d7c58f1281b051dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:10 +0000

    Add package test-package-10/1.1/test-package-10-1.1.tar.gz

---
+++ packages/test-package-10/test-package-10-1.1.tar.gz/test-package-10-1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="test-package-10",
+    version="1.1",
+    author="xiaoFei",
+    author_email="1228372188@qq.com",
+    description="",
+    long_description="made by george-fly,just for testing",
+    long_description_content_type="text/markdown",
+    url="https://homepage",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development",
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.6',
+)
