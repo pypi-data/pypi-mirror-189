442b7f472e407266d72d2e9f5567b27267edeb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:42 +0000

    Add package aspeak/3.2.0/aspeak-3.2.0.tar.gz

---
+++ packages/aspeak/aspeak-3.2.0.tar.gz/aspeak-3.2.0/src/aspeak/api/__init__.py
@@ -0,0 +1,3 @@
+from .format import FileFormat, AudioFormat
+from .provider import SpeechServiceProvider
+from .api import SpeechToFileService, SpeechToSpeakerService, SpeechServiceBase, SpeechToOneFileService
