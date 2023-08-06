3232ed0650eae440d99f21072f23a500bea2040b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:57 +0000

    Add package clashofclans-api/0.1.0/clashofclans-api-0.1.0.tar.gz

---
+++ packages/clashofclans-api/clashofclans-api-0.1.0.tar.gz/clashofclans-api-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="clashofclans-api",
+    install_requires=[
+        "requests~=2.28.2"
+        ]
+    )
