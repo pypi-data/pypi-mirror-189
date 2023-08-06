9187056e0b7c58fc46fcae18dbc05c2b42ab6c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:11 +0000

    Add package ccr-py/0.0.2/ccr-py-0.0.2.tar.gz

---
+++ packages/ccr-py/ccr-py-0.0.2.tar.gz/ccr-py-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ccr-py',
+    version='0.0.2',
+    description='Contextualized Construct Representations (CCR).',
+    long_description='',
+    url='https://github.com/Ali-Omrani',
+    author='Ali Omrani',
+    author_email='aomrani@usc.edu',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
