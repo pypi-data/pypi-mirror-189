54caf870b3d23eb921674e94724bf06fab90ab01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:24 +0000

    Add package pyTensileTest/0.0.6/pyTensileTest-0.0.6.tar.gz

---
+++ packages/pyTensileTest/pyTensileTest-0.0.6.tar.gz/pyTensileTest-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='pyTensileTest',
+    version='0.0.6',
+    license='MIT License',
+    author='Gabriel Valverde Zanata da Silva',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='gabriel.valverde@hotmail.com',
+    keywords='shimadzu tensile test',
+    description=u'A package for manipulating tensile test data, creating and correcting stress-strain diagrams.',
+    packages=['pyTensileTest'],
+    install_requires=['matplotlib','numpy','pandas'],
+    url='https://github.com/GabrielVZanata/pyTensileTest',)
