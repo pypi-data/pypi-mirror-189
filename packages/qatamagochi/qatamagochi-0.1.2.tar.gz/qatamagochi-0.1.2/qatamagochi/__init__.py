02dc59235de9e9c6f642ce388be217d18372d275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:02 +0000

    Add package qatamagochi/0.1.2/qatamagochi-0.1.2.tar.gz

---
+++ packages/qatamagochi/qatamagochi-0.1.2.tar.gz/qatamagochi-0.1.2/qatamagochi/__init__.py
@@ -0,0 +1,23 @@
+# from qatamagochi import Qatamagochi
+
+
+class Tamagochiowner:
+    def __init__(self, name=None):
+        self.name = name
+
+class Qatamagochi(Tamagochiowner):
+    def __init__(self, name=None, tamagochi_name=None):
+        super().__init__(name)
+        # super().show()
+        self.owner = name
+        self.name = tamagochi_name
+
+    def speak(self):
+        if self.owner is None and self.name is None:
+            print(f"Hi i dont know my name and my owner name's too")
+        elif self.owner != None and self.name is None:
+            print(f"Hi i dont know my name but my owner is {self.owner}")
+        elif self.owner is None and self.name != None:
+            print(f"Hi my name is {self.name} and i dont know my owner :(")
+        else:
+            print(f"Hi my name is {self.name} and my owner is {self.owner}")
