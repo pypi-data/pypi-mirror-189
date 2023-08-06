ee88ff0e9046c083d0b90f3fdf3292f0e533cc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:09 +0000

    Add package clophfit/0.4.3/clophfit-0.4.3.tar.gz

---
+++ packages/clophfit/clophfit-0.4.3.tar.gz/clophfit-0.4.3/tests/test_binding.py
@@ -0,0 +1,16 @@
+"""Test cases for the binding functions module."""
+
+import numpy as np
+
+from clophfit import binding
+
+
+def test_kd() -> None:
+    """It returns int float np.ndarray[float]."""
+    assert binding.kd(10, 7, 6.0) == 11
+    assert binding.kd(10, 7, 7) == 20
+    assert binding.kd(10, 7, 8) == 110.0
+    np.testing.assert_allclose(
+        binding.kd(10.0, 7.0, np.array([6.0, 8.0])), [11.0, 110.0]
+    )
+    np.testing.assert_allclose(binding.kd(10, 7, np.array([7, 6, 8.0])), [20, 11, 110])
