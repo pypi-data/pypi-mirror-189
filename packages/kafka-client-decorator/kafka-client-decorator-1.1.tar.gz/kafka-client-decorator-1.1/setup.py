006ec5f6777e9ffd80c4d76eba79219ea6384912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:53 +0000

    Add package kafka-client-decorator/1.1/kafka-client-decorator-1.1.tar.gz

---
+++ packages/kafka-client-decorator/kafka-client-decorator-1.1.tar.gz/kafka-client-decorator-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kafka-client-decorator',
+    version='1.1',
+    license='MIT',
+    description='A wrapper for confluent-kafka producer and consumer',
+    author="Quantrium",
+    author_email='firoz.mohammad@quantrium.ai',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://www.quantrium.ai/',
+    keywords=['confluent-kafka', 'Kafka-producer', 'Kafka-consumer'],
+    python_requires = ">=3.6",
+    install_requires=[
+          'confluent-kafka==2.0.2',
+      ],
+
+)
