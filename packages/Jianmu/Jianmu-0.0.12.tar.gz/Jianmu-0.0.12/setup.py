ae88847b676c6fa1be8221a3bf68cf229802521b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:35 +0000

    Add package Jianmu/0.0.12/Jianmu-0.0.12.tar.gz

---
+++ packages/Jianmu/Jianmu-0.0.12.tar.gz/Jianmu-0.0.12/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import setuptools
+
+from jianmu.info import version
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='Jianmu',
+    version=version,
+    description='A simple desktop app development framework combining Python, Vue.js, Element Plus and Electron.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Zhaoji Wang',
+    author_email='hwoam@outlook.com',
+    url='https://github.com/frederick-wang/jianmu',
+    project_urls={
+        "Bug Tracker": "https://github.com/frederick-wang/jianmu/issues",
+    },
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    install_requires=['dataclasses', 'Flask', 'flask-socketio', 'gevent', 'gevent-websocket', 'requests', 'rich', 'pyreactivity', 'typing_extensions'],
+    packages=setuptools.find_packages(include=['jianmu', 'jianmu.*']),
+    entry_points={
+        'console_scripts': ['jianmu=jianmu.cli:parse'],
+    },
+    python_requires=">=3.7",
+)
