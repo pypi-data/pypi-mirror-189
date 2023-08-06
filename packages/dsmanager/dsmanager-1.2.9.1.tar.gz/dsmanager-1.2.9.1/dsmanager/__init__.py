b6e5b7b7fb851c55bbeaf78288556b21e0bc77a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:46 +0000

    Add package dsmanager/1.2.9.1/dsmanager-1.2.9.1.tar.gz

---
+++ packages/dsmanager/dsmanager-1.2.9.1.tar.gz/dsmanager-1.2.9.1/dsmanager/__init__.py
@@ -0,0 +1,21 @@
+"""@Author: Rayane AMROUCHE
+
+The source code is composed of three main components:
+
+- A DataManager component
+
+- A Controller component
+
+- A Model component
+
+The View component can also be used if needed.
+"""
+
+from dsmanager.controller import Config
+
+from dsmanager.datamanager import DataManager
+from dsmanager.datamanager.utils import DataPipeline
+from dsmanager.datamanager.datastorage import DataStorage
+
+from dsmanager.model import ModelManager
+from dsmanager.model import Model
