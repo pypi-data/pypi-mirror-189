066929e4e25898b5915dfe14caee2b1572a76a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:11 +0000

    Add package gamut/1.0.0b0/gamut-1.0.0b0.tar.gz

---
+++ packages/gamut/gamut-1.0.0b0.tar.gz/gamut-1.0.0b0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+MODULE = 'gamut'
+VERSION = '1.0.0-beta'
+DESCRIPTION = 'Granular Audio Musaicing Toolkit for Python'
+
+setup(
+    name=MODULE,
+    version=VERSION,
+    author='Felipe Tovar-Henao',
+    author_email='<felipe.tovar.henao@gmail.com>',
+    description=DESCRIPTION,
+    url='https://pypi.org/project/gamut/',
+    packages=find_packages(),
+    license='OSI Approved :: ISC License (ISCL)',
+    install_requires=['progress', 'librosa', 'sounddevice'],
+    keywords=['DSP', 'audio musaicing', 'granulation', 'machine learning',
+              'ML', "MIR", 'music', 'sound design', 'concatenative synthesis'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Other Audience',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
