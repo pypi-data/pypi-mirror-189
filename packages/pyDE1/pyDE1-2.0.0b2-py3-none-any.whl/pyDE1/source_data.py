de16a8affbae1bfe67989337872fb546311f2563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:58 +0000

    Add package pyDE1/2.0.0b2/pyDE1-2.0.0b2-py3-none-any.whl

---
+++ packages/pyDE1/pyDE1-2.0.0b2-py3-none-any.whl/pyDE1/source_data.py
@@ -0,0 +1,11 @@
+# DO NOT EDIT -- This file automatically generated -- DO NOT EDIT
+git_data = {
+    "hash": "590e49ce",
+    "subject": "Update setup.cfg for v2.0b2",
+    "ref_names": "HEAD -> devel-2023-02",
+    "author_date": "Sat Feb 4 21:40:49 2023 -0800",
+    "commit_date": "Sat Feb 4 21:40:49 2023 -0800",
+}
+source_data = {
+    "git": git_data,
+}
