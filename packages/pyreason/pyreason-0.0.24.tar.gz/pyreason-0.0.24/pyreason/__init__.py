3e6c771eff13b0931d5acf2bb6190a5cacb799c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:52 +0000

    Add package pyreason/0.0.24/pyreason-0.0.24.tar.gz

---
+++ packages/pyreason/pyreason-0.0.24.tar.gz/pyreason-0.0.24/pyreason/__init__.py
@@ -0,0 +1,26 @@
+# Set numba environment variable
+import os
+package_path = os.path.abspath(os.path.dirname(__file__))
+cache_path = os.path.join(package_path, 'cache')
+os.environ['NUMBA_CACHE_DIR'] =  cache_path
+
+from pyreason.pyreason import *
+
+print(cache_path)
+print(package_path)
+if not os.path.exists(cache_path):
+    os.makedirs(cache_path)
+    print('Imported PyReason for the first time. Initializing ... this will take a minute')
+    graph_path = os.path.join(package_path, 'examples', 'hello-world', 'friends.graphml')
+    labels_path = os.path.join(package_path, 'examples', 'hello-world', 'labels.yaml')
+    facts_path = os.path.join(package_path, 'examples', 'hello-world', 'facts.yaml')
+    rules_path = os.path.join(package_path, 'examples', 'hello-world', 'rules.yaml')
+    ipl_path = os.path.join(package_path, 'examples', 'hello-world', 'ipl.yaml')
+
+    settings.verbose = False
+    load_graph(graph_path)
+    load_labels(labels_path)
+    load_facts(facts_path)
+    load_rules(rules_path)
+    load_inconsistent_predicate_list(ipl_path)
+    reason(timesteps=2)
