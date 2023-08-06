4891672c0c35ae829bba74a9398cc91414dd2f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:42 +0000

    Add package pyreason/0.0.9/pyreason-0.0.9.tar.gz

---
+++ packages/pyreason/pyreason-0.0.9.tar.gz/pyreason-0.0.9/pyreason/__init__.py
@@ -0,0 +1,6 @@
+from pyreason.pyreason import *
+
+print('Imported PyReason for the first time. Initializing ... this will take a minute')
+print(os.system('ls -a'))
+print(os.path.exists('./cache'))
+print(os.path.exists('./docs'))
