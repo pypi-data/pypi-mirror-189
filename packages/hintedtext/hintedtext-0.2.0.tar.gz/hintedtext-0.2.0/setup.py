a1dbade8ad3c0da0a476a042aadeeee41901edf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:35 +0000

    Add package hintedtext/0.2.0/hintedtext-0.2.0.tar.gz

---
+++ packages/hintedtext/hintedtext-0.2.0.tar.gz/hintedtext-0.2.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['hintedtext']
+setup_kwargs = {
+    'name': 'hintedtext',
+    'version': '0.2.0',
+    'description': 'Tkinter Text widget with an extra hint attribute.',
+    'long_description': None,
+    'author': 'Billy',
+    'author_email': 'billydevbusiness@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
