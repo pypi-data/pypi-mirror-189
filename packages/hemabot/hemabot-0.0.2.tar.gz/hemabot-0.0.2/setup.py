730fec2f98b8cf44266094bc6f7266856b03275c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:36 +0000

    Add package hemabot/0.0.2/hemabot-0.0.2.tar.gz

---
+++ packages/hemabot/hemabot-0.0.2.tar.gz/hemabot-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hemabot",
+    version="0.0.2",  # Latest version .
+    author="slipper",
+    author_email="r2fscg@gmail.com",
+    description="A package for faster coding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/private_repo/hemabot",
+    packages=setuptools.find_packages(),
+    install_requires=['codefast', 'python-telegram-bot==13.12'],
+    entry_points={'console_scripts': [
+        'hemabot=hemabot.api:start_bot',
+    ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
