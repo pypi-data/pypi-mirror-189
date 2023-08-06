0bc0b689ce1d124835a72bcd16a8c4c0b32d0852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:16 +0000

    Add package berkoidf/0.3/berkoidf-0.3.tar.gz

---
+++ packages/berkoidf/berkoidf-0.3.tar.gz/berkoidf-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md','r') as f:
+  long_desc = f.read() 
+
+setup(
+    name='berkoidf',
+    version='0.3',
+    license='MIT',
+    author="Berke Dilekoglu",
+    long_description= long_desc,
+    long_description_content_type='text/markdown',
+    url = 'https://github.com/berkedilekoglu/tfidfvectorizerinheritencecase',   
+    download_url = 'https://github.com/berkedilekoglu/tfidfvectorizerinheritencecase/archive/refs/tags/vectorizer.tar.gz',    
+    
+    packages=find_packages(),
+    keywords='machine-learning tf-idf',
+    test_suite="tests", 
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+      ],
+
+)
