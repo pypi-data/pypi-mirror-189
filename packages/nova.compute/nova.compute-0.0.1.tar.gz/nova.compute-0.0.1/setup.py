0be1b7e170088eb85c9fa378f9aabb26a90dad41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:42 +0000

    Add package nova.compute/0.0.1/nova.compute-0.0.1.tar.gz

---
+++ packages/nova.compute/nova.compute-0.0.1.tar.gz/nova.compute-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name="nova.compute",
+        version="0.0.1",
+        description="Security Research for Dangling Packages",
+        long_description="Security Research for Dangling Packages",
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages()
+)
