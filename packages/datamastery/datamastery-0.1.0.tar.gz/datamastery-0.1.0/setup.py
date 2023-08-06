a55d9e6cbe8d65279abda4efdf1e9509e9ebc850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:50 +0000

    Add package datamastery/0.1.0/datamastery-0.1.0.tar.gz

---
+++ packages/datamastery/datamastery-0.1.0.tar.gz/datamastery-0.1.0/setup.py
@@ -0,0 +1,27 @@
+"""Install packages as defined in this file into the Python environment."""
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="datamastery",
+    author="Baran Kutluay",
+    author_email="barankutluay19@gmail.com",
+    # url="xxx",
+    description="Datamastery is a Python package for data preprocessing and visualisation.",
+    long_description="Datamastery is a Python package for data preprocessing and visualisation. It provides a set of functions to make it easier to work with data.",
+    version="0.1.0",
+    package_dir={"": "src"},
+    packages=find_namespace_packages(where="src", exclude=["tests"]),
+    install_requires=[
+        "setuptools>=65.5.0",
+        "matplotlib>=3.6.3",
+        "numpy>=1.24.1",
+        "seaborn>=0.12.2",
+        "pandas>=1.5.3",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+    ],
+)
