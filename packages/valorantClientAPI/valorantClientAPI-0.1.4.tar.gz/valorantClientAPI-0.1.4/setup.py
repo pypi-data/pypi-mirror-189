b79aacf13d975170b613417b9b24dec92281857f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:32 +0000

    Add package valorantClientAPI/0.1.4/valorantClientAPI-0.1.4.tar.gz

---
+++ packages/valorantClientAPI/valorantClientAPI-0.1.4.tar.gz/valorantClientAPI-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as readme:
+    long_desc = readme.read()
+
+setup(
+    name="valorantClientAPI",
+    version="0.1.4",
+    author="Jet612",
+    description="Asynchronous Python wrapper for VALORANT's Client API ",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jet612/VALORANT-Async-Client-API",
+    project_urls={
+        "Documentation": "https://github.com/Jet612/VALORANT-Async-Client-API/tree/main/docs",
+        "Bug Tracker": "https://github.com/users/Jet612/projects/8",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8",
+)
