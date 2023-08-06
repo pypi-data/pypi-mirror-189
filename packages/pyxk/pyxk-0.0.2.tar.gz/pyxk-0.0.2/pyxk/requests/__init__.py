5a082e6b90f3ff20dffd276b4a4e420125ca64fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:22 +0000

    Add package pyxk/0.0.2/pyxk-0.0.2.tar.gz

---
+++ packages/pyxk/pyxk-0.0.2.tar.gz/pyxk-0.0.2/pyxk/requests/__init__.py
@@ -0,0 +1,20 @@
+import os
+import importlib
+
+
+__all__ = [
+    item.removesuffix(".py")
+    for item in os.listdir(os.path.dirname(__file__))
+    if item not in ("__pycache__", "__init__.py")
+]
+
+
+def __getattr__(name):
+    # print(f"{__name__!r}.__getattr__: {name!r}")
+    if name not in __all__:
+        raise AttributeError
+    return importlib.import_module(f".{name}", package=__name__)
+
+
+def __dir__():
+    return __all__
