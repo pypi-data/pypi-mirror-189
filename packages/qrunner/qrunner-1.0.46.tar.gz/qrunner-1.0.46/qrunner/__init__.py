f2a2f32fa19877d8d80701998bcedac1c13e9607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:54 +0000

    Add package qrunner/1.0.46/qrunner-1.0.46.tar.gz

---
+++ packages/qrunner/qrunner-1.0.46.tar.gz/qrunner-1.0.46/qrunner/__init__.py
@@ -0,0 +1,7 @@
+from qrunner.case import TestCase
+from qrunner.page import Page
+from qrunner.page import Elem
+from qrunner.running.runner import main
+from qrunner.utils.config import config
+from qrunner.utils.decorate import *
+from qrunner.utils.log import logger
