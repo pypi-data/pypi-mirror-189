ed284ab68ab1175a389583ee8ffb676979009188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:02 +0000

    Add package sqlalchemize/0.7.1/sqlalchemize-0.7.1.tar.gz

---
+++ packages/sqlalchemize/sqlalchemize-0.7.1.tar.gz/sqlalchemize-0.7.1/src/sqlalchemize/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.7.1'
+
+import sqlalchemize.create as create
+import sqlalchemize.delete as delete
+import sqlalchemize.drop as drop
+import sqlalchemize.features as features
+import sqlalchemize.insert as insert
+import sqlalchemize.select as select
+import sqlalchemize.update as update
+import sqlalchemize.type_convert as type_convert
+import sqlalchemize.records as records
+from sqlalchemize.create import create_engine
+from sqlalchemize.features import get_table, get_table_names, get_engine_table
