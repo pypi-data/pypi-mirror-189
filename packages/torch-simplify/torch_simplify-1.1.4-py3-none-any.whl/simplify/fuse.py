2af23cbe27ff0be1e8e4bad5a159612553903ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:58 +0000

    Add package torch-simplify/1.1.4/torch_simplify-1.1.4-py3-none-any.whl

---
+++ packages/torch-simplify/torch_simplify-1.1.4-py3-none-any.whl/simplify/fuse.py
@@ -0,0 +1,188 @@
+#  Copyright (c) 2022 EIDOSLab. All rights reserved.
+#  See the LICENSE file for licensing terms (BSD-style).
+from typing import List, Tuple
+
+import torch
+import torch.nn as nn
+
+
+@torch.no_grad()
+def get_module(model: torch.nn.Module, name: List[str]) -> torch.nn.Module:
+    """
+    Get a module inside `model` based on its name.
+
+    Args:
+        model (torch.nn.Module): Model in which to perform the research.
+        name (str): List of string representing the old module name. i.e if the module name is layer1.0.conv1 `name` should be ["layer1", "0", "conv1"]
+
+    Returns:
+        torch.nn.Module: Found module.
+
+    """
+    module = model
+    for idx, sub in enumerate(name):
+        if idx < len(name):
+            module = getattr(module, sub)
+
+    return module
+
+
+@torch.no_grad()
+def substitute_module(model: torch.nn.Module, new_module: torch.nn.Module, sub_module_names: List[str]) -> None:
+    """
+    Substitute a `torch.nn.Module` with another based on the module's name.
+    
+    Args:
+        model: PyTorch model on which the substitution occurs.
+        new_module: New module to insert in the model.
+        sub_module_names: List of string representing the old module name. i.e if the module name is layer1.0.conv1 `sub_module_names` should be ["layer1", "0", "conv1"]
+
+    """
+    if new_module is not None:
+        attr = model
+        for idx, sub in enumerate(sub_module_names):
+            if idx < len(sub_module_names) - 1:
+                attr = getattr(attr, sub)
+            else:
+                setattr(attr, sub, new_module)
+
+
+@torch.no_grad()
+def fuse(model: torch.nn.Module, bn_folding: List[Tuple[str, str]]) -> torch.nn.Module:
+    """
+    Fuse a model's `torch.nn.Conv2d`-`torch.nn.BatchNorm2d` or `torch.nn.Linear`-`torch.nn.BatchNorm2d` couples.
+
+    Args:
+        model (torch.nn.Module): Model to fuse.
+        bn_folding (List[Tuple[str, str]]): List of tuples containing the names of the modules to fuse.
+
+    Returns:
+        torch.nn.Module: Fused model.
+
+    """
+    for module_pair in bn_folding:
+        fused_module = None
+
+        preceding_name = module_pair[0].split(".")
+        bn_name = module_pair[1].split(".")
+        preceding = get_module(model, preceding_name)
+        bn = get_module(model, bn_name)
+
+        if isinstance(bn, nn.BatchNorm2d):
+            if isinstance(preceding, nn.Linear):
+                fused_module = fuse_fc_and_bn(preceding, bn)
+            if isinstance(preceding, nn.Conv2d):
+                fused_module = fuse_conv_and_bn(preceding, bn)
+
+        if fused_module is not None:
+            substitute_module(model, fused_module, preceding_name)
+            substitute_module(model, nn.Identity(), bn_name)
+
+    return model
+
+
+@torch.no_grad()
+def fuse_conv_and_bn(conv: torch.nn.Conv2d, bn: torch.nn.BatchNorm2d) -> torch.nn.Conv2d:
+    """
+    Perform modules fusion.
+
+    Args:
+        conv (torch.nn.Conv2d): nn.Conv2d module.
+        bn (torch.nn.BatchNorm2d): nn.BatchNorm2d module.
+
+    Returns:
+        torch.nn.Conv2d: nn.Conv2d originated from $conv$ and $bn$ fusion.
+    """
+    # https://tehnokv.com/posts/fusing-batchnorm-and-conv/
+    # init
+    device = conv.weight.device
+
+    fusedconv = nn.Conv2d(in_channels=conv.in_channels,
+                          out_channels=conv.out_channels,
+                          kernel_size=conv.kernel_size,
+                          stride=conv.stride,
+                          padding=conv.padding,
+                          dilation=conv.dilation,
+                          groups=conv.groups,
+                          bias=True,
+                          padding_mode=conv.padding_mode).to(device)
+
+    bn_weight = bn.weight.to(torch.double)
+    bn_bias = bn.bias.to(torch.double)
+    bn_mean = bn.running_mean.to(torch.double)
+    bn_var = bn.running_var.to(torch.double)
+    bn_eps = bn.eps
+    conv_weight = conv.weight.view(conv.out_channels, -1).to(torch.double)
+    conv_bias = conv.bias.to(torch.double) if conv.bias is not None \
+        else torch.zeros(conv.weight.size(0), dtype=torch.double, device=device)
+
+    # prepare filters
+    bn_diag = torch.diag(
+        bn_weight.div(
+            torch.sqrt(
+                bn_eps +
+                bn_var.to(
+                    torch.double))))
+    fusedconv_weight = torch.mm(
+        bn_diag,
+        conv_weight).view(
+        fusedconv.weight.size()).to(
+        torch.float)
+    fusedconv.weight.copy_(fusedconv_weight)
+
+    # prepare spatial bias
+    b_bn = bn_bias - bn_weight.mul(bn_mean).div(torch.sqrt(bn_var + bn_eps))
+    fusedconv.bias.copy_(
+        (torch.mm(bn_diag, conv_bias.reshape(-1, 1)).reshape(-1) + b_bn).to(torch.float))
+
+    return fusedconv
+
+
+@torch.no_grad()
+def fuse_fc_and_bn(fc: torch.nn.Linear, bn: torch.nn.BatchNorm2d) -> torch.nn.Linear:
+    """
+    Perform modules fusion.
+
+    Args:
+        fc (torch.nn.Linear): nn.Linear module.
+        bn (torch.nn.BatchNorm2d): nn.BatchNorm2d module.
+
+    Returns:
+        torch.nn.Linear: nn.Linear originated from `fc` and `bn` fusion.
+    """
+    device = fc.weight.device
+
+    fusedlinear = nn.Linear(
+        in_features=fc.in_features,
+        out_features=fc.out_features,
+        bias=True).to(device)
+
+    bn_weight = bn.weight.to(torch.double)
+    bn_bias = bn.bias.to(torch.double)
+    bn_mean = bn.running_mean.to(torch.double)
+    bn_var = bn.running_var.to(torch.double)
+    bn_eps = bn.eps
+    fc_weight = fc.weight.to(torch.double)
+    fc_bias = fc.bias.to(torch.double) if fc.bias is not None \
+        else torch.zeros(fc.weight.size(0), dtype=torch.double, device=device)
+
+    # prepare filters
+    bn_diag = torch.diag(
+        bn_weight.div(
+            torch.sqrt(
+                bn_eps +
+                bn_var.to(
+                    torch.double))))
+    fusedlinear_weight = torch.mm(
+        bn_diag,
+        fc_weight).view(
+        fusedlinear.weight.size()).to(
+        torch.float)
+    fusedlinear.weight.copy_(fusedlinear_weight)
+
+    # prepare spatial bias
+    b_bn = bn_bias - bn_weight.mul(bn_mean).div(torch.sqrt(bn_var + bn_eps))
+    fusedlinear.bias.copy_(
+        (torch.mm(bn_diag, fc_bias.reshape(-1, 1)).reshape(-1) + b_bn).to(torch.float))
+
+    return fusedlinear
---
+++ packages/torch-simplify/torch_simplify-1.1.4-py3-none-any.whl/simplify/utils.py
@@ -0,0 +1,154 @@
+#  Copyright (c) 2022 EIDOSLab. All rights reserved.
+#  See the LICENSE file for licensing terms (BSD-style).
+
+import copy
+import re
+from typing import Any, Dict, Type, Iterable, Tuple, List
+
+import torch
+import torch.nn as nn
+from torch import fx
+from torchvision.models import MobileNetV3, ShuffleNetV2
+from torchvision.models.densenet import _DenseLayer
+
+
+def matches_module_pattern(pattern: Iterable[Type], node: fx.Node, modules: Dict[str, Any]) -> bool:
+    """
+    Reimplementation of PyTorch experimental function `matches_module_pattern`.
+    See https://github.com/pytorch/pytorch/blob/master/torch/fx/experimental/optimization.py#L26
+    """
+    if len(node.args) == 0:
+        return False
+    nodes = (node.args[0], node)
+    for expected_type, current_node in zip(pattern, nodes):
+        if not isinstance(current_node, fx.Node):
+            return False
+        if current_node.op != 'call_module':
+            return False
+        if not isinstance(current_node.target, str):
+            return False
+        if current_node.target not in modules:
+            return False
+        if type(modules[current_node.target]) is not expected_type:
+            return False
+    return True
+
+
+def get_bn_folding(model: nn.Module) -> List[Tuple[str, str]]:
+    """
+    Search for tuples of adjacent `nn.Conv2d` and `nn.BatchNorm2d` modules.
+
+    Args:
+        model (nn.Module): Model on which to perform the conv-bn search.
+
+    Returns:
+        List[Tuple[str, str]]: List of the found modules.
+    """
+    bn_folding = []
+
+    try:
+        patterns = [(torch.nn.Conv2d, torch.nn.BatchNorm2d)]
+        fx_model = fx.symbolic_trace(model)
+        modules = dict(fx_model.named_modules())
+
+        for pattern in patterns:
+            for node in fx_model.graph.nodes:
+                if matches_module_pattern(pattern, node, modules):
+                    if len(node.args[0].users) > 1:
+                        continue
+                    bn_folding.append((node.args[0].target, node.target))
+
+    except Exception as e:
+        last_module = None
+
+        for name, module in model.named_modules():
+            if isinstance(module, _DenseLayer):
+                last_module = None
+            if isinstance(module, (nn.Linear, nn.Conv2d)):
+                last_module = (name, module)
+            if isinstance(module, nn.BatchNorm2d):
+                if last_module is not None and last_module[1].weight.shape[0] == module.weight.shape[0]:
+                    bn_folding.append((last_module[0], name))
+
+    return bn_folding
+
+
+def get_previous_layer(connections: Dict, module: fx.Node) -> fx.Node:
+    """
+    Recursively find the node the precedes `module` in the dictionary of nodes `connections`.
+
+    Args:
+        connections (Dict): Dictionary of nodes.
+        module (fx.Node): target node.
+
+    Returns:
+        fx.Node: Found node.
+    """
+    for k in connections:
+        if any([c == module for c in connections[k]["next"]]):
+            if not isinstance(connections[k]["class"], (nn.Conv2d, nn.BatchNorm2d)):
+                return get_previous_layer(connections, k)
+            else:
+                return k
+
+
+def get_pinned(model: torch.nn.Module) -> List[str]:
+    """
+    Try to find all the modules for which the output shape needs to stay fixed,
+    (e.g. modules involved in residual connections with a sum).
+
+    Args:
+        model (torch.nn.Module): The model on which to perform the research.
+
+    Returns:
+        Dict[str]: Dictionary of all the found modules.
+    """
+    fx_model = fx.symbolic_trace(copy.deepcopy(model))
+    modules = dict(fx_model.named_modules())
+
+    names = {re.sub('[_.]', '', n): n for n in modules}
+
+    connections = {}
+
+    # Build dictionary node -> list of connected nodes
+    for i, node in enumerate(fx_model.graph.nodes):
+        if node.target in modules:
+            module = modules[node.target]
+        else:
+            module = None
+
+        connections[node.name] = {"next": [str(user) for user in node.users], "class": module}
+
+    # Remove duplicates and build list of "to-pin" nodes (may contain nodes not CONV nor BN)
+    same_next = []
+    for k in connections:
+        for k2 in connections:
+            if k != k2:
+                if "add" in str(set(connections[k]["next"]) & set(connections[k2]["next"])):
+                    same_next.append([k, k2])
+
+    same_next = set([item for sublist in same_next for item in sublist])
+
+    # Add input node of CONV with grouping, layer.6 for MNASNet and fc2 for MobileNetV3
+    for i, node in enumerate(fx_model.graph.nodes):
+        if (isinstance(model, MobileNetV3) and "fc2" in node.name) or \
+                (isinstance(model, ShuffleNetV2) and (node.name == "conv1_1" or
+                                                      "branch1_3" in node.name or
+                                                      "branch2_1" in node.name or
+                                                      "branch2_6" in node.name)):
+            same_next.add(str(node.name))
+        name = node.name.replace("_", ".")
+        if re.sub('[_.]', '', name) in names:
+            module = modules[names[re.sub('[_.]', '', name)]]
+            if isinstance(module, nn.Conv2d) and module.groups > 1:
+                same_next.add(str(node.prev))
+
+    # For each node not CONV nor BN recover the closest previous CONV or BN
+    to_pin = []
+    for m in same_next:
+        if not isinstance(connections[m]["class"], (nn.Conv2d, nn.BatchNorm2d)):
+            to_pin.append(get_previous_layer(connections, m))
+        else:
+            to_pin.append(m)
+
+    return [names[re.sub('[_.]', '', n)] for n in list(set(to_pin))]
