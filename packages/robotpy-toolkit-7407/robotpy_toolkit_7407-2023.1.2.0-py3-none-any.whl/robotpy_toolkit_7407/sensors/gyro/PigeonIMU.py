35213b14849c9a0c57b63f0c033e3411d2ffffd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:27 +0000

    Add package robotpy-toolkit-7407/2023.1.2.0/robotpy_toolkit_7407-2023.1.2.0-py3-none-any.whl

---
+++ packages/robotpy-toolkit-7407/robotpy_toolkit_7407-2023.1.2.0-py3-none-any.whl/robotpy_toolkit_7407/sensors/gyro/PigeonIMU.py
@@ -0,0 +1,39 @@
+import ctre
+import math
+
+from robotpy_toolkit_7407.utils.units import radians
+from robotpy_toolkit_7407.sensors.gyro.base_gyro import Gyro
+
+
+class PigeonIMUGyro_Wrapper(Gyro):
+    """
+    Wrapper class for the Pigeon2 IMU gyro.
+    """
+
+    def __init__(self, port):
+        """
+        Args:
+            port (int): CAN ID of the Pigeon gyro
+        """
+        self._gyro = ctre.Pigeon2(port)
+        self._gyro.configMountPose(0, 0, 0)
+
+    def init(self, gyro_start_angle=0):
+        """
+        Initialize gyro
+        """
+        self.reset_angle(gyro_start_angle)
+
+    def get_robot_heading(self) -> radians:
+        """
+        Returns the angle of the robot's heading in radians (yaw)
+        :return: Robot heading (radians)
+        """
+        return math.radians(self._gyro.getYaw())
+
+    # reset the gyro
+    def reset_angle(self, angle=0):
+        """
+        Resets the gyro's yaw.
+        """
+        self._gyro.setYaw(angle)
