ffdd7de3dfa02202a3fb24a3edd8cf1f46d2b710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:15 +0000

    Add package etlgpx/0.1.6/etlgpx-0.1.6.tar.gz

---
+++ packages/etlgpx/etlgpx-0.1.6.tar.gz/etlgpx-0.1.6/README.md
@@ -0,0 +1,6 @@
+# GPX-TRACKS-ETL
+
+1. pip3 install -e . -r requirements.txt
+
+python3 etlgpx/main.py
+etlgpx pipeline
---
+++ packages/etlgpx/etlgpx-0.1.6.tar.gz/etlgpx-0.1.6/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(version="0.1.6", packages=find_packages())
