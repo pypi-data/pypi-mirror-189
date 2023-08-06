1e484861ae57de0fdb2274ab0a358bb8c6884d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:16 +0000

    Add package sanic-api/0.2.3/sanic-api-0.2.3.tar.gz

---
+++ packages/sanic-api/sanic-api-0.2.3.tar.gz/sanic-api-0.2.3/sanic_api/api/api.py
@@ -0,0 +1,154 @@
+from abc import ABCMeta
+from dataclasses import dataclass
+from functools import partial
+from typing import Any, Callable, Dict, Optional
+
+# noinspection PyUnresolvedReferences
+import ujson
+from pydantic import BaseModel
+from pydantic import ValidationError as PyDanticValidationError
+from sanic import HTTPResponse, Sanic
+from sanic.response import json
+
+from sanic_api.api.exception import ValidationError
+from sanic_api.enum import ParamEnum, RespCodeEnum
+from sanic_api.model import ListModel
+from sanic_api.utils import json_dumps
+
+
+@dataclass
+class Response:
+    # http响应码 默认是200
+    http_code: int = 200
+    # 自定义的响应头
+    headers: Optional[Dict[str, str]] = None
+    # 自定义的服务码
+    server_code: RespCodeEnum = RespCodeEnum.SUCCESS
+    # 返回的消息
+    message: str = ""
+    # 返回的数据
+    data: Any = None
+
+    def json_resp(
+        self, dumps: Optional[Callable[[Any], Any]] = None, **kwargs
+    ) -> HTTPResponse:
+        """
+        返回json格式的响应
+        Args:
+            dumps: 序列化方法，默认使用自定义的序列化方法
+            **kwargs: 序列化方法的参数
+
+        Returns:
+            返回一个sanic的HTTPResponse
+        """
+
+        if isinstance(self.data, ListModel):
+            self.data = self.data.to_list()
+        elif isinstance(self.data, BaseModel):
+            self.data = self.data.dict()
+        else:
+            self.data = self.data
+
+        dumps = dumps or partial(ujson.dumps, ensure_ascii=False, default=json_dumps)
+        data = {
+            self._get_tmp("code_tmp", "code"): self.server_code.value,
+            self._get_tmp("msg_tmp", "msg"): self.message or self.server_code.desc,
+            self._get_tmp("data_tmp", "data"): self.data,
+        }
+        return json(
+            body=data,
+            status=self.http_code,
+            headers=self.headers,
+            dumps=dumps,
+            **kwargs,
+        )
+
+    @staticmethod
+    def _get_tmp(key: str, default):
+        """
+        获取字段模板
+        """
+        app = Sanic.get_app()
+        sanic_api: dict = app.config.get("sanic_api", {})
+        return sanic_api.get(key, default)
+
+
+class API(metaclass=ABCMeta):
+    json_req: Optional[BaseModel] = None
+    form_req: Optional[BaseModel] = None
+    query_req: Optional[BaseModel] = None
+    resp: Optional[Any] = None
+    tags: list = []
+    description: str = ""
+
+    def __init__(self):
+        self.response_type = self.__class__.__annotations__.get("resp")
+        self.json_req_type = self.__class__.__annotations__.get("json_req")
+        self.form_req_type = self.__class__.__annotations__.get("form_req")
+        self.query_req_type = self.__class__.__annotations__.get("query_req")
+        try:
+            self.resp = (
+                self.response_type()
+                if self.response_type and issubclass(self.response_type, BaseModel)
+                else ""
+            )
+        except PyDanticValidationError:
+            pass
+
+    def req_to_json(self):
+        data = {}
+        if self.json_req:
+            data["json"] = self.json_req.dict()
+        elif self.form_req:
+            data["form"] = self.form_req.dict()
+        if self.query_req:
+            data["query"] = self.query_req.dict()
+
+        return ujson.dumps(data, ensure_ascii=False, default=json_dumps, indent=4)
+
+    def validate_params(self, req_data: dict, param_enum: ParamEnum):
+        """
+        验证参数，参数有问题抛出异常
+        Args:
+            req_data: 数据
+            param_enum: 参数类型
+
+        Returns:
+
+        """
+        try:
+            if param_enum == ParamEnum.JSON:
+                self.json_req = self.json_req_type(**req_data)
+            elif param_enum == ParamEnum.QUERY:
+                self.query_req = self.query_req_type(**req_data)
+            elif param_enum == ParamEnum.FORM:
+                self.form_req = self.form_req_type(**req_data)
+        except PyDanticValidationError as e:
+            raise ValidationError(e.errors()) from e
+
+    def json_resp(
+        self,
+        http_code: int = 200,
+        headers: Optional[Dict[str, str]] = None,
+        server_code: RespCodeEnum = RespCodeEnum.SUCCESS,
+        message: str = "",
+    ):
+        """
+        根据响应数据返回json格式的响应
+        Args:
+            http_code: http协议响应码
+            headers: 响应头
+            server_code: 应用协议响应码
+            message: 响应消息
+
+        Returns:
+            返回响应
+        """
+
+        return Response(
+            data=self.resp,
+            http_code=http_code,
+            headers=headers,
+            server_code=server_code,
+            message=message,
+        ).json_resp()
---
+++ packages/sanic-api/sanic-api-0.2.3.tar.gz/sanic-api-0.2.3/sanic_api/openapi/openapi.py
@@ -0,0 +1,151 @@
+import inspect
+
+from pydantic import BaseModel
+from sanic import Sanic
+from sanic_ext.extensions.openapi.builders import (
+    OperationBuilder,
+    OperationStore,
+    SpecificationBuilder,
+)
+from sanic_ext.extensions.openapi.definitions import Schema
+from sanic_ext.extensions.openapi.types import Array, Object
+from sanic_ext.utils.route import clean_route_name, get_all_routes
+
+from sanic_api.api import API
+from sanic_api.api.validators import get_handler_param
+from sanic_api.model import ListModel
+
+
+# noinspection PyUnusedLocal
+def auto_doc(app: Sanic, loop):
+    config = app.config
+    specification = SpecificationBuilder()
+
+    for (
+        uri,
+        route_name,
+        route_parameters,
+        method_handlers,
+        host,
+    ) in get_all_routes(app, config.OAS_URL_PREFIX):
+        uri = uri if uri == "/" else uri.rstrip("/")
+
+        for method, _handler in method_handlers:
+            if (
+                (method == "OPTIONS" and app.config.OAS_IGNORE_OPTIONS)
+                or (method == "HEAD" and app.config.OAS_IGNORE_HEAD)
+                or method == "TRACE"
+            ):
+                continue
+
+            api_cls = get_handler_param(_handler)
+            if not api_cls:
+                continue
+
+            api: API = api_cls()
+
+            operation: OperationBuilder = OperationStore()[_handler]
+            operation_exclude = operation._exclude
+            operation_default = operation._default
+            operation_autodoc = operation._autodoc
+            operation_allow_autodoc = operation._allow_autodoc
+
+            if operation_exclude or "openapi" in operation.tags:
+                continue
+
+            # 读取蓝图上面的 blueprint.ctx.desc 属性来代替name设置中文tag名
+            if len(route_name.split(".")) > 1:
+                blueprint = app.blueprints[route_name.split(".")[0]]
+                blueprint.ctx.desc = blueprint.ctx.desc or blueprint.name
+                api.tags.insert(0, blueprint.ctx.desc)
+
+            # 设置接口的标签和描述
+            operation.tag(*api.tags)
+            tags_str = " ".join([f"[{tag}](/docs#tag/{tag})" for tag in api.tags])
+            operation.describe(description=f"### 标签: {tags_str}\n{api.description}")
+
+            docstring = inspect.getdoc(_handler)
+
+            if docstring and app.config.OAS_AUTODOC and operation_allow_autodoc:
+                operation.autodoc(docstring)
+
+            if api.json_req_type:
+                body_type = api.json_req_type
+                mine_type = "application/json"
+            elif api.form_req_type:
+                body_type = api.form_req_type
+                mine_type = "application/x-www-form-urlencoded"
+            else:
+                body_type, mine_type, body_dict = ["", "", {}]
+            if body_type:
+                body_schema: dict = body_type.schema(
+                    ref_template="#/components/schemas/{model}"
+                )
+                body_dict = {
+                    mine_type: Object(body_schema["properties"]),
+                }
+                for model_name, schema_model in body_schema.get(
+                    "definitions", {}
+                ).items():
+                    specification.add_component("schemas", model_name, schema_model)
+                # noinspection PyProtectedMember
+                body_dict[mine_type]._fields["required"] = body_schema.get(
+                    "required", []
+                )
+                operation.body(body_dict)
+
+            if api.query_req_type:
+                for k, v in api.query_req_type.schema()[
+                    "properties"
+                ].items():  # type: (str, dict)
+                    operation.parameter(k, Schema(**v))
+
+            if api.response_type:
+                if issubclass(api.response_type, BaseModel):
+                    schema: Schema = Object(
+                        api.response_type.schema(
+                            ref_template="#/components/schemas/{model}"
+                        )["properties"]
+                    )
+                    if issubclass(api.response_type, ListModel):
+                        schema = Array(schema)
+                    operation.response(
+                        status=200,
+                        content={"application/json": schema},
+                        description="成功",
+                    )
+                    specification.add_component(
+                        "schemas", api.response_type.__name__, schema
+                    )
+
+            operation_default["operationId"] = f"{method.lower()}~{route_name}"
+            operation_default["summary"] = clean_route_name(route_name)
+
+            if host:
+                if "servers" not in operation_default:
+                    operation_default["servers"] = []
+                operation_default["servers"].append({"url": f"//{host}"})
+
+            for _parameter in route_parameters:
+                if any(
+                    param.fields["name"] == _parameter.name
+                    for param in operation.parameters
+                ):
+                    continue
+
+                kwargs = {}
+                if operation_autodoc and (
+                    parameters := operation_autodoc.get("parameters")
+                ):
+                    description = None
+                    for param in parameters:
+                        if param["name"] == _parameter.name:
+                            description = param.get("description")
+                            break
+                    if description:
+                        kwargs["description"] = description
+
+                operation.parameter(_parameter.name, _parameter.cast, "path", **kwargs)
+
+            operation._app = app
+            specification.operation(uri, method, operation)
