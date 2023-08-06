2587cc4609c16a6f612c760f0bffdeb9cdd4cb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:22 +0000

    Add package bighudders/0.0.1/bighudders-0.0.1.tar.gz

---
+++ packages/bighudders/bighudders-0.0.1.tar.gz/bighudders-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.1'
+DESCRIPTION = 'Myles is amazing'
+
+# Setting up
+setup(
+    name="bighudders",
+    version=VERSION,
+    author="Edward Hardy",
+    author_email="<hardyedward18@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pillow'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
