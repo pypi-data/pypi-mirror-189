b20ae93e9a98fc8a8ab9a6752b43b39bf0cd23d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:26 +0000

    Add package ddtank/0.0.7.3/ddtank-0.0.7.3-py3-none-any.whl

---
+++ packages/ddtank/ddtank-0.0.7.3-py3-none-any.whl/ddtank/__init__.py
@@ -0,0 +1,21 @@
+__version__ = '0.0.7.3'
+
+
+import win32api
+import win32con
+import win32gui
+import win32ui
+import time
+import string
+import cv2
+import numpy as np
+import re
+import os
+import onnxruntime
+
+from ctypes import windll
+from threading import Thread
+
+from .dl_script import load_onnx, build_preprocess, build_postprocess
+
+VkKeyScanA = windll.user32.VkKeyScanA
