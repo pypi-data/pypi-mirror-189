2d078d2edf1259027a1a45cde0b5a4b04dffe2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:56 +0000

    Add package packagesorry/0.0.4/packagesorry-0.0.4.tar.gz

---
+++ packages/packagesorry/packagesorry-0.0.4.tar.gz/packagesorry-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='packagesorry',
+version='0.0.4',
+
+packages = find_packages(),
+package_dir= {
+    'math': 'math'
+},
+description="This code with harry",
+long_description='This is very very long description',
+author='Harry',
+
+
+)
