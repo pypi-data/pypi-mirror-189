367ec94a6442e6569e6005e16a63fb2d76dccb92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:51 +0000

    Add package netbox-storage/0.0.0.0.1/netbox_storage-0.0.0.0.1.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.1.tar.gz/netbox_storage-0.0.0.0.1/ocp_project_plugin/forms/resource_quota.py
@@ -0,0 +1,119 @@
+from django.core.validators import MinValueValidator
+from django.forms import (
+    CharField,
+    FloatField, BooleanField,
+)
+
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from utilities.forms import (
+    CSVModelChoiceField,
+    DynamicModelChoiceField,
+)
+
+from ocp_project_plugin.models import AppEnvironment, ResourceQuota
+
+
+class ResourceQuotaForm(NetBoxModelForm):
+    """Form for creating a new Resource Quota object."""
+    limits_cpu = CharField(
+        label="Limits CPU",
+        help_text="E.g. 2",
+    )
+    limits_memory = CharField(
+        label="Limits Memory",
+        help_text="E.g. 200Mi",
+    )
+    requests_cpu = CharField(
+        label="Request CPU",
+        help_text="E.g. 1",
+    )
+    requests_memory = CharField(
+        label="Request Memory",
+        help_text="E.g. 100Mi",
+    )
+
+    class Meta:
+        model = ResourceQuota
+
+        fields = ["requests_cpu", "requests_memory", "limits_cpu", "limits_memory", "app_environment"]
+
+
+class ResourceQuotaFilterForm(NetBoxModelFilterSetForm):
+    """Form for filtering App Environment instances."""
+
+    model = ResourceQuota
+
+    limits_cpu = CharField(
+        required=False,
+        label="Limits CPU",
+        help_text="E.g. 2",
+    )
+    limits_memory = CharField(
+        required=False,
+        label="Limits Memory",
+        help_text="E.g. 200Mi",
+    )
+    requests_cpu = CharField(
+        required=False,
+        label="Request CPU",
+        help_text="E.g. 1",
+    )
+    requests_memory = CharField(
+        required=False,
+        label="Request Memory",
+        help_text="E.g. 100Mi",
+    )
+
+
+class ResourceQuotaImportForm(NetBoxModelImportForm):
+    limits_cpu = CharField(
+        label="Limits CPU",
+        help_text="E.g. 2",
+    )
+    limits_memory = CharField(
+        label="Limits Memory",
+        help_text="E.g. 200Mi",
+    )
+    requests_cpu = CharField(
+        label="Request CPU",
+        help_text="E.g. 1",
+    )
+    requests_memory = CharField(
+        label="Request Memory",
+        help_text="E.g. 100Mi",
+    )
+
+    class Meta:
+        model = ResourceQuota
+
+        fields = ["requests_cpu", "requests_memory", "limits_cpu", "limits_memory", "app_environment"]
+
+
+class ResourceQuotaBulkEditForm(NetBoxModelBulkEditForm):
+    model = ResourceQuota
+
+    limits_cpu = CharField(
+        required=False,
+        label="Limits CPU",
+        help_text="E.g. 2",
+    )
+    limits_memory = CharField(
+        required=False,
+        label="Limits Memory",
+        help_text="E.g. 200Mi",
+    )
+    requests_cpu = CharField(
+        required=False,
+        label="Request CPU",
+        help_text="E.g. 1",
+    )
+    requests_memory = CharField(
+        required=False,
+        label="Request Memory",
+        help_text="E.g. 100Mi",
+    )
