a0d923540bb53648f3691c5627f84457a2ca15d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:55 +0000

    Add package packagesorry/0.0.3/packagesorry-0.0.3-py3-none-any.whl

---
+++ packages/packagesorry/packagesorry-0.0.3-py3-none-any.whl/ganit/setup.py
@@ -0,0 +1,11 @@
+"""
+from setuptools import setup
+
+setup(name='ganit',
+version='0.1.2',
+description="This code with harry",
+long_description='This is very very long description',
+author='Harry',
+
+instaall_require=[])
+"""
