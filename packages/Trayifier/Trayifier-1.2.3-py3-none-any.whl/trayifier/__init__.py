251226672d55aa92ab941857e35a6927aa5f1ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:13 +0000

    Add package Trayifier/1.2.3/Trayifier-1.2.3-py3-none-any.whl

---
+++ packages/Trayifier/Trayifier-1.2.3-py3-none-any.whl/trayifier/__init__.py
@@ -0,0 +1,14 @@
+import sys
+from trayifier.__main__ import trayify_from_script as trayify
+from trayifier.__main__ import main as main
+from importlib import resources
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+__version__ = "1.2.3"
+
+assert sys.platform == "win32", "Trayify only works on the win32 platform."
+
+_cfg = tomllib.loads(resources.read_text("trayifier", "config.toml"))
