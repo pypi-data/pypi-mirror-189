d551bf53b8de82e900ad650f9068551b678392b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:11 +0000

    Add package xia-models/0.0.4/xia_models-0.0.4-cp310-none-manylinux1_x86_64.whl

---
+++ packages/xia-models/xia_models-0.0.4-cp310-none-manylinux1_x86_64.whl/xia_models/__init__.py
@@ -0,0 +1,9 @@
+from xia_models.model import DataModel, DataField
+from xia_models.address import DataAddress
+
+__all__ = [
+    "DataModel", "DataField",
+    "DataAddress"
+]
+
+__version__ = "0.0.4"
