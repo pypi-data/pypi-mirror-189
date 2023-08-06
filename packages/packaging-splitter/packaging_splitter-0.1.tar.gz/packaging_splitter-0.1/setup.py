a20227aaf1a13c08925853a8dc0b1660180530c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:56 +0000

    Add package packaging-splitter/0.1/packaging_splitter-0.1.tar.gz

---
+++ packages/packaging-splitter/packaging_splitter-0.1.tar.gz/packaging_splitter-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='packaging_splitter',
+    version='0.1',
+    packages=find_packages(),
+    author='Mubaraka Banadda',
+    author_email='mubarakabanadda68@gmail.com',
+    description='A package for splitting image datasets into train, test, and validation sets'
+)
