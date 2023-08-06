bc6c73495327b5cae53ba4c43f606345011a34f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:05:10 +0000

    Add package polars/0.16.2/polars-0.16.2.tar.gz

---
+++ packages/polars/polars-0.16.2.tar.gz/polars-0.16.2/local_dependencies/polars-ops/src/series/ops/mod.rs
@@ -0,0 +1,34 @@
+#[cfg(feature = "round_series")]
+mod floor_divide;
+#[cfg(feature = "is_first")]
+mod is_first;
+#[cfg(feature = "log")]
+mod log;
+#[cfg(feature = "rolling_window")]
+mod rolling;
+#[cfg(feature = "search_sorted")]
+mod search_sorted;
+mod various;
+
+#[cfg(feature = "round_series")]
+pub use floor_divide::*;
+#[cfg(feature = "is_first")]
+pub use is_first::*;
+#[cfg(feature = "log")]
+pub use log::*;
+use polars_core::prelude::*;
+#[cfg(feature = "rolling_window")]
+pub use rolling::*;
+#[cfg(feature = "search_sorted")]
+pub use search_sorted::*;
+pub use various::*;
+
+pub trait SeriesSealed {
+    fn as_series(&self) -> &Series;
+}
+
+impl SeriesSealed for Series {
+    fn as_series(&self) -> &Series {
+        self
+    }
+}
