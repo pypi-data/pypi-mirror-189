7df591c8b1bfa18948d55ea537f976e66b619c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:30 +0000

    Add package c64tools/0.12.0/c64tools-0.12.0.tar.gz

---
+++ packages/c64tools/c64tools-0.12.0.tar.gz/c64tools-0.12.0/setup.py
@@ -0,0 +1,45 @@
+# ===================================================================
+# c64tools - c64 Python helper / setup
+#
+# (c) Daniel Krajzewicz 2016-2023
+# daniel@krajzewicz.de
+# http://www.krajzewicz.de/blog/c64-python-helper.php
+# https://github.com/dkrajzew/c64tools
+# Available under the BSD license.
+# ===================================================================
+
+# --- imports -------------------------------------------------------
+import setuptools
+
+
+# --- definitions ---------------------------------------------------
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name="c64tools",
+  version="0.12.0",
+  author="dkrajzew",
+  author_email="d.krajzewicz@gmail.com",
+  description="c64 Python helper",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/dkrajzew/c64tools",
+  packages=setuptools.find_packages(),
+  # see https://pypi.org/classifiers/
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Other Audience",
+    "Programming Language :: Python",
+    "Topic :: Artistic Software",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Other/Nonlisted Topic",
+    "Topic :: Multimedia :: Graphics"
+  ],
+  python_requires='>=2.7, <4',
+)
+
