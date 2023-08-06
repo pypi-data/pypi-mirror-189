bc7465cc70b023c70dadc74592a7d68e8847c49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:03 +0000

    Add package whendone/1.0.1/whendone-1.0.1.tar.gz

---
+++ packages/whendone/whendone-1.0.1.tar.gz/whendone-1.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['slack_sdk']
+
+test_requirements = ['slack_sdk']
+
+setup(
+    author="Sabri Barac",
+    author_email='sabribarac@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python package to notify you when your function is done through Telegram and/or Slack.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='whendone',
+    name='whendone',
+    packages=find_packages(include=['whendone', 'whendone.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/sabribarac/whendone',
+    version='1.0.1',
+    zip_safe=False,
+)
