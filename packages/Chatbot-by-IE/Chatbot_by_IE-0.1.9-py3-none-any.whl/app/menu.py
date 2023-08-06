3b398c4598eea287dfbe3dd2cc9888507e4ad959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:35 +0000

    Add package Chatbot-by-IE/0.1.9/Chatbot_by_IE-0.1.9-py3-none-any.whl

---
+++ packages/Chatbot-by-IE/Chatbot_by_IE-0.1.9-py3-none-any.whl/app/menu.py
@@ -0,0 +1,36 @@
+"""C H A T B O T"""
+
+from .addressbook import main as ab_main
+from .notes import main as nb_main
+from .sort import main as sr_main
+from prettytable import PrettyTable
+
+
+def main():
+    """Main function"""
+
+    while True:
+        x = PrettyTable()
+        x.field_names = ['C H A T B O T']
+        x.align = 'l'
+        x.add_row(['0: Exit'])
+        x.add_row(['1: AddressBook'])
+        x.add_row(['2: NoteBook'])
+        x.add_row(['3: Sort files'])
+        print(x)
+        user_input = input(f"\nEnter command {chr(10151)*3} ")
+        if user_input == "1":
+            ab_main()
+        elif user_input == "2":
+            nb_main()
+        elif user_input == "3":
+            sr_main()
+        elif user_input == "0":
+            print(f"\n{chr(128075)} Good bay!")
+            break
+        else:
+            print(f"\n{chr(129400)}I don't understand you!")
+
+
+if __name__ == "__main__":
+    main()
