8672ce39e2f77afe269dc6e1c2e1052d0c0ddcfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:32 +0000

    Add package cracker-saying/2.0/cracker-saying-2.0.tar.gz

---
+++ packages/cracker-saying/cracker-saying-2.0.tar.gz/cracker-saying-2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+long = open("README.md","r").read()
+
+setup(name='cracker-saying',
+version='2.0',
+description='A simple pip for designing & designing termux banner.',
+log_description = long,
+
+long_description_content_type='text/markdown',
+author='cracker911181',
+author_email='admin@cracker911181.cf',
+url='https://pypi.org/project/cracker-saying',
+download_url='https://pypi.org/project/cracker-saying',
+packages=['cracker_say'],
+
+zip_safe=False)
