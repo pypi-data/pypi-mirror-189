d00537815bc82c1dcdec45ca25009500cb21f191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:22 +0000

    Add package ubermag/0.66.1/ubermag-0.66.1.tar.gz

---
+++ packages/ubermag/ubermag-0.66.1.tar.gz/ubermag-0.66.1/ubermag/__init__.py
@@ -0,0 +1,40 @@
+"""Meta package for the Ubermag project."""
+import pkg_resources
+import pytest
+
+from .info import debug_info
+
+__version__ = pkg_resources.get_distribution(__name__).version
+
+
+def test():
+    """Run all package tests.
+
+    Examples
+    --------
+    1. Run all tests.
+
+    >>> import ubermag
+    ...
+    >>> # ubermag.test()
+
+    """
+    return pytest.main(
+        [
+            "-m",
+            "not travis and not docker",
+            "-v",
+            "--pyargs",
+            "ubermagutil",
+            "discretisedfield",
+            "ubermagtable",
+            "micromagneticmodel",
+            "oommfc",
+            "micromagnetictests",
+            "micromagneticdata",
+            "mag2exp",
+            # "mumax3c",  # mumax3 is not necessarily available -> no tests
+            "ubermag",
+            "-l",
+        ]
+    )  # pragma: no cover
