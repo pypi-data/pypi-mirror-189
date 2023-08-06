8fa075337e9d32d75d358d915292f511c1f8d9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:55 +0000

    Add package zh-lunar-date/0.0.2/zh_lunar_date-0.0.2-py3-none-any.whl

---
+++ packages/zh-lunar-date/zh_lunar_date-0.0.2-py3-none-any.whl/zhdate/model.py
@@ -0,0 +1,10 @@
+class ZhModel:
+    def __init__(self, *, zh_year, zh_month, zh_day, year_tiandi, shengxiao):
+        self.zh_year = zh_year
+        self.zh_month = zh_month
+        self.zh_day = zh_day
+        self.year_tiandi = year_tiandi
+        self.shengxiao = shengxiao
+
+    def __str__(self):
+        return f"{self.zh_year}{self.zh_month}{self.zh_day} {self.year_tiandi} ({self.shengxiao}年)"
