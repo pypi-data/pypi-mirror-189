e9bc2f362085564ab5a0f5c33f1ce98ba0667ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:54 +0000

    Add package kiki-utils-image/1.0.7/kiki_utils_image-1.0.7-py3-none-any.whl

---
+++ packages/kiki-utils-image/kiki_utils_image-1.0.7-py3-none-any.whl/kikiutils_image/aio.py
@@ -0,0 +1,44 @@
+from asyncio import get_event_loop
+
+from .sync import convert_image, get_image
+from .typehint import ImageData
+
+
+__all__ = [
+    'aconvert_image',
+    'aget_image'
+]
+
+
+async def aconvert_image(
+    image_data: ImageData,
+    format: str = 'webp',
+    quality: int = 100,
+    get_bytes: bool = False
+):
+    loop = get_event_loop()
+    return await loop.run_in_executor(
+        None,
+        convert_image,
+        image_data,
+        format,
+        quality,
+        get_bytes
+    )
+
+
+async def aget_image(
+    url: str,
+    check: bool = True,
+    get_bytes: bool = False,
+    request_method: str = 'GET'
+):
+    loop = get_event_loop()
+    return await loop.run_in_executor(
+        None,
+        get_image,
+        url,
+        check,
+        get_bytes,
+        request_method
+    )
