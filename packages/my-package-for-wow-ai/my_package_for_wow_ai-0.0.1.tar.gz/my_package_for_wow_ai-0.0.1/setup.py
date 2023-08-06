4da23349ddf969563f5b99768d18ad852cdd59c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:55 +0000

    Add package my-package-for-wow-ai/0.0.1/my_package_for_wow_ai-0.0.1.tar.gz

---
+++ packages/my-package-for-wow-ai/my_package_for_wow_ai-0.0.1.tar.gz/my_package_for_wow_ai-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_package_for_wow_ai',
+    version='0.0.1',
+    description='A simple package to add two numbers',
+    author='cuong',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+)
