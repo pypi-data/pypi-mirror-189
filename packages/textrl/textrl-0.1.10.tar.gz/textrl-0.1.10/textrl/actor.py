4d2ab2c6c9a50b3af296cc9f91957693c1ff6c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:23 +0000

    Add package textrl/0.1.10/textrl-0.1.10.tar.gz

---
+++ packages/textrl/textrl-0.1.10.tar.gz/textrl-0.1.10/textrl/actor.py
@@ -0,0 +1,173 @@
+import itertools
+
+import pfrl
+import torch
+import torch.nn.functional as F
+from pfrl.agents.ppo import _elementwise_clip
+from torch import autocast
+from transformers import top_k_top_p_filtering
+
+
+class TextRLActor:
+    @autocast('cuda')
+    def __init__(self, env, model, tokenizer, device=0, act_deterministically=True):
+        self.agent = None
+        self.n_actions = max(model.config.vocab_size, tokenizer.vocab_size)
+        self.env = env
+        self.device = device
+        self.model = model
+        self.obs_size = model.config.hidden_size
+        self.converter = self.model.lm_head
+        self.act_deterministically = act_deterministically
+
+    @autocast('cuda')
+    def agent_ppo(self, update_interval=10, minibatch_size=3000, epochs=20):
+        policy = torch.nn.Sequential(
+            self.converter,
+            SoftmaxCategoricalHead()
+        )
+        vf = torch.nn.Sequential(
+            torch.nn.Linear(self.obs_size, 1),
+        )
+        model = pfrl.nn.Branched(policy, vf)
+        opt = torch.optim.SGD(model.parameters(), lr=5e-5)
+        model = model.cuda()
+        agent = TextPPO(
+            model,
+            opt,
+            gpu=self.device,
+            update_interval=update_interval,
+            minibatch_size=minibatch_size,
+            epochs=epochs,
+            clip_eps_vf=None,
+            entropy_coef=0,
+            gamma=1,
+            lambd=1,
+            standardize_advantages=True,
+            act_deterministically=self.act_deterministically
+        )
+        self.agent = agent
+        return agent
+
+    @autocast('cuda')
+    def predict(self, input_item, max_episode_len=100):
+        t = 0
+        with torch.inference_mode():
+            with self.agent.eval_mode():
+                obs = self.env.reset(input_item)
+                while True:
+                    action = self.agent.act(obs)
+                    obs, reward, done, pred = self.env.step(action)
+                    t += 1
+                    reset = t >= max_episode_len
+                    self.agent.observe(obs, reward, done, reset)
+                    if done or reset:
+                        return pred.get('predicted_str')
+
+
+class TextPPO(pfrl.agents.PPO):
+    @autocast('cuda')
+    def _update_if_dataset_is_ready(self):
+        dataset_size = (
+                sum(len(episode) for episode in self.memory)
+                + len(self.last_episode)
+                + (
+                    0
+                    if self.batch_last_episode is None
+                    else sum(len(episode) for episode in self.batch_last_episode)
+                )
+        )
+        if dataset_size >= self.update_interval:
+            self._flush_last_episode()
+            if self.recurrent:
+                dataset = pfrl.agents.ppo._make_dataset_recurrent(
+                    episodes=self.memory,
+                    model=self.model,
+                    phi=self.phi,
+                    batch_states=self.batch_states,
+                    obs_normalizer=self.obs_normalizer,
+                    gamma=self.gamma,
+                    lambd=self.lambd,
+                    max_recurrent_sequence_len=self.max_recurrent_sequence_len,
+                    device=self.device,
+                )
+                self._update_recurrent(dataset)
+            else:
+                dataset = pfrl.agents.ppo._make_dataset(
+                    episodes=self.memory,
+                    model=self.model,
+                    phi=self.phi,
+                    batch_states=self.batch_states,
+                    obs_normalizer=self.obs_normalizer,
+                    gamma=self.gamma,
+                    lambd=self.lambd,
+                    device=self.device,
+                )
+                assert len(dataset) == dataset_size
+                self._update(dataset)
+            self.explained_variance = pfrl.agents.ppo._compute_explained_variance(
+                list(itertools.chain.from_iterable(self.memory))
+            )
+            self.memory = []
+
+    @autocast('cuda')
+    def batch_act(self, batch_obs):
+        if self.training:
+            return self._batch_act_train(batch_obs)
+        else:
+            return self._batch_act_eval(batch_obs)
+
+    @autocast('cuda')
+    def _lossfun(
+            self, entropy, vs_pred, log_probs, vs_pred_old, log_probs_old, advs, vs_teacher
+    ):
+
+        prob_ratio = torch.exp(log_probs - log_probs_old)
+        loss_policy = -torch.mean(
+            torch.min(
+                (prob_ratio * advs),
+                (torch.clamp(prob_ratio, 1 - self.clip_eps, 1 + self.clip_eps) * advs),
+            ),
+        )
+
+        if self.clip_eps_vf is None:
+            loss_value_func = F.mse_loss(vs_pred, vs_teacher)
+        else:
+            clipped_vs_pred = _elementwise_clip(
+                vs_pred,
+                vs_pred_old - self.clip_eps_vf,
+                vs_pred_old + self.clip_eps_vf,
+            )
+            loss_value_func = torch.mean(
+                torch.max(
+                    F.mse_loss(vs_pred, vs_teacher, reduction="none"),
+                    F.mse_loss(clipped_vs_pred, vs_teacher, reduction="none"),
+                )
+            )
+        loss_entropy = -torch.mean(entropy)
+
+        self.value_loss_record.append(float(loss_value_func))
+        self.policy_loss_record.append(float(loss_policy))
+
+        loss = (
+                loss_policy
+                + self.value_func_coef * loss_value_func
+                + self.entropy_coef * loss_entropy
+        )
+        return loss
+
+
+class SoftmaxCategoricalHead(torch.nn.Module):
+    def __init__(self, temperature=0.3, total_num_of_trial=3, top_k=0, top_p=1.0):
+        super().__init__()
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.temperature = temperature
+        self.total_num_of_trial = total_num_of_trial
+        self.top_k = top_k
+        self.top_p = top_p
+
+    @autocast('cuda')
+    def forward(self, logits):
+        logits = logits / self.temperature
+        logits = top_k_top_p_filtering(logits, top_k=self.top_k, top_p=self.top_p)
+        return torch.distributions.Multinomial(total_count=self.total_num_of_trial, probs=self.softmax(logits))
---
+++ packages/textrl/textrl-0.1.10.tar.gz/textrl-0.1.10/textrl/environment.py
@@ -0,0 +1,95 @@
+import logging
+import random
+import sys
+
+import gym
+import numpy
+import torch
+
+
+class TextRLEnv(gym.Env):
+    def __init__(self, model, tokenizer, observation_input=[], max_length=100):
+        vocabs = list(dict(sorted(tokenizer.vocab.items(), key=lambda item: item[1])).keys())
+        self.action_space = gym.spaces.Discrete(len(vocabs))
+        self.actions = vocabs
+        self.model = model
+        self.tokenizer = tokenizer
+        self.observation_space = observation_input
+        self.target_table = {}
+        self.input_item = [""]
+        self.predicted = []
+        self.env_max_length = min(max(self.model.config.max_length, self.tokenizer.model_max_length), max_length)
+        self.reset()
+
+        self.gen_stop_toks = []
+        logging.disable(sys.maxsize)
+        if self.tokenizer.sep_token:
+            self.gen_stop_toks.append(self.tokenizer.sep_token)
+        if self.tokenizer.eos_token:
+            self.gen_stop_toks.append(self.tokenizer.eos_token)
+        logging.disable(logging.NOTSET)
+
+    def step(self, action):
+        if isinstance(action, numpy.ndarray):
+            action = numpy.argmax(action)
+        predicted, finish, predicted_str = self._predict(vocab_id=action)
+        reward = self.get_reward(self.input_item, predicted, finish)
+        self.predicted = predicted
+        return self._get_obs(predicted), reward, finish, {"predicted_str": predicted_str}
+
+    def get_reward(self, input_item, predicted_list, finish):
+        reward = 1
+        return reward
+
+    def gat_obs_input(self, input_item):
+        return input_item[0]
+
+    def reset(self, input_item=None):
+        self.predicted = []
+        if input_item is None:
+            self.input_item = random.choice(self.observation_space)
+        else:
+            self.input_item = input_item
+        return self._get_obs()
+
+    def _get_obs(self, predicted=[]):
+        with torch.inference_mode():
+            p_text = self.tokenizer.convert_tokens_to_string(predicted)
+            if len([k for k, v in self.model.named_parameters() if 'decoder' in k]) > 0:
+                feature_dict = self.tokenizer([self.gat_obs_input(self.input_item)],
+                                              return_tensors='pt',
+                                              add_special_tokens=False).to(self.model.device)
+                predicted = [self.tokenizer.eos_token] + predicted
+                dec_input = torch.tensor([self.tokenizer.convert_tokens_to_ids(predicted)]).to(self.model.device)
+                feature_dict['decoder_input_ids'] = dec_input
+                prediction = self.model(**feature_dict, output_hidden_states=True)
+                outputs = prediction.decoder_hidden_states[-1].squeeze(0)
+            else:
+                if self.model.__class__.__name__ == 'DistributedBloomForCausalLM':
+                    with self.model.inference_session(max_length=self.env_max_length) as sess:
+                        feature_dict = self.tokenizer([[self.gat_obs_input(self.input_item), p_text]],
+                                                      return_tensors='pt',
+                                                      add_special_tokens=False).to(self.model.device)
+                        embs = self.model.transformer.word_embeddings(feature_dict.input_ids)
+                        embs = self.model.transformer.word_embeddings_layernorm(embs)
+                        h = sess.step(embs)
+                        outputs = self.model.transformer.ln_f(h[:, -1])
+                else:
+                    feature_dict = self.tokenizer([[self.gat_obs_input(self.input_item), p_text]],
+                                                  return_tensors='pt',
+                                                  add_special_tokens=False).to(self.model.device)
+                    prediction = self.model(**feature_dict, output_hidden_states=True)
+                    outputs = prediction.hidden_states[-1].squeeze(0)
+            return outputs.data[-1]
+
+    def _predict(self, vocab_id):
+        predicted = self.predicted
+        with torch.inference_mode():
+            pred_word = self.actions[vocab_id]
+            if pred_word in self.gen_stop_toks \
+                    or len(pred_word) < 1 \
+                    or len(self.predicted) > self.env_max_length:
+                return predicted, True, self.tokenizer.convert_tokens_to_string(predicted)
+            else:
+                predicted += [pred_word]
+                return predicted, False, self.tokenizer.convert_tokens_to_string(predicted)
