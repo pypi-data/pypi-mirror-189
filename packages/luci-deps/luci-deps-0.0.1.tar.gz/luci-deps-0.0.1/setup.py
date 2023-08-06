b3734549fbbdaf42c45e65da619ebc819ba6d67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:53 +0000

    Add package luci-deps/0.0.1/luci-deps-0.0.1.tar.gz

---
+++ packages/luci-deps/luci-deps-0.0.1.tar.gz/luci-deps-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "luci-deps",
+    version = "0.0.1",
+    author = "Charlie James",
+    author_email="charliejamescode@gmail.com",
+    description = "short package description",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = find_packages(where="src"),
+    python_requires = ">=3.7"
+)
