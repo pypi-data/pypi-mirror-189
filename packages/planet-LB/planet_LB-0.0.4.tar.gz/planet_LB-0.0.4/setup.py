737e72bd1c8c6f1452b230736bd9b0479d00c9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:27 +0000

    Add package planet-LB/0.0.4/planet_LB-0.0.4.tar.gz

---
+++ packages/planet-LB/planet_LB-0.0.4.tar.gz/planet_LB-0.0.4/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name="planet_LB",
+        version="0.0.4",
+        author="Craig O'Neill",
+        description="Lattice Boltzmann Solver for python",
+        packages=["planet_LB"]
+)
