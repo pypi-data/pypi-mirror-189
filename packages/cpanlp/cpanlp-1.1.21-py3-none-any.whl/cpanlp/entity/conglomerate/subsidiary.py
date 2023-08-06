121c4917eaa776b5aff8f1a4c0e9a6d571223635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package cpanlp/1.1.21/cpanlp-1.1.21-py3-none-any.whl

---
+++ packages/cpanlp/cpanlp-1.1.21-py3-none-any.whl/cpanlp/entity/conglomerate/subsidiary.py
@@ -0,0 +1,7 @@
+from cpanlp.entity.LLC import *
+
+
+class Subsidiary(LLC):
+    def __init__(self, name,type,capital, parent_company):
+        super().__init__(name,type,capital)
+        self.parent_company = parent_company
