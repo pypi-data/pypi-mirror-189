bf8593c2966b72232363bc4b3a688f6c7d008ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:32 +0000

    Add package py-synologydsm-api/2.1.3/py_synologydsm_api-2.1.3-py3-none-any.whl

---
+++ packages/py-synologydsm-api/py_synologydsm_api-2.1.3-py3-none-any.whl/synology_dsm/api/core/security.py
@@ -0,0 +1,58 @@
+"""DSM Security data."""
+from synology_dsm.api import SynoBaseApi
+
+
+class SynoCoreSecurity(SynoBaseApi):
+    """Class containing Security data."""
+
+    API_KEY = "SYNO.Core.SecurityScan.Status"
+
+    async def update(self):
+        """Updates security data."""
+        raw_data = await self._dsm.get(self.API_KEY, "system_get")
+        if raw_data:
+            self._data = raw_data["data"]
+
+    @property
+    def checks(self):
+        """Gets the checklist by check category."""
+        return self._data.get("items", {})
+
+    @property
+    def last_scan_time(self):
+        """Gets the last scan time."""
+        return self._data.get("lastScanTime")
+
+    @property
+    def start_time(self):
+        """Gets the start time (if in progress)."""
+        return self._data.get("startTime")
+
+    @property
+    def success(self):
+        """Gets the last scan success."""
+        return self._data.get("success")
+
+    @property
+    def progress(self):
+        """Gets the scan progress.
+
+        Returns: 100 if finished
+        """
+        return self._data.get("sysProgress")
+
+    @property
+    def status(self):
+        """Gets the last scan status.
+
+        Possible values: safe, danger, info, outOfDate, risk, warning.
+        """
+        return self._data.get("sysStatus")
+
+    @property
+    def status_by_check(self):
+        """Gets the last scan status per check."""
+        status = {}
+        for category in self.checks:
+            status[category] = self.checks[category]["failSeverity"]
+        return status
