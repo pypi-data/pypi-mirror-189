fff8a99b8b5f768007f6b74acdacb76220dd722b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:46 +0000

    Add package policyengine-canada/0.31.0/policyengine-canada-0.31.0.tar.gz

---
+++ packages/policyengine-canada/policyengine-canada-0.31.0.tar.gz/policyengine-canada-0.31.0/policyengine_canada/variables/gov/benefits.py
@@ -0,0 +1,18 @@
+from policyengine_canada.model_api import *
+
+
+class benefits(Variable):
+    value_type = float
+    entity = Household
+    label = "benefits"
+    unit = CAD
+    definition_period = YEAR
+    adds = [
+        "canada_child_benefit",
+        "child_disability_benefit",
+        "canada_workers_benefit",
+        "dental_benefit",
+        "old_age_security_pension",
+        # Ontario programs.
+        "on_child_benefit",
+    ]
