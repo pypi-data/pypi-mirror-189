f15e983160ff3a302e9a72a988ea2f9d6b8e65b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:57 +0000

    Add package gcbinspy/0.0.1/gcbinspy-0.0.1.tar.gz

---
+++ packages/gcbinspy/gcbinspy-0.0.1.tar.gz/gcbinspy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gcbinspy',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'bs4',
+        'dateutil'
+        'datetime',
+    ],
+)
