f9dd86d79d2862f0b26d062f1b35bb742f687307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:49 +0000

    Add package tellolib/1.0.3/tellolib-1.0.3.tar.gz

---
+++ packages/tellolib/tellolib-1.0.3.tar.gz/tellolib-1.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+import setuptools
+
+setuptools.setup(
+    name='tellolib',
+    version='1.0.3',
+    description='A library meant to fly the tello drone',
+    author='Aarav Prakash',
+    packages = find_packages()
+)
