00cf6ac24e4f0a8103e5ae7509725ce7971a22ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:48 +0000

    Add package poetry-plugin-use-pip-global-index-url/0.2.0/poetry_plugin_use_pip_global_index_url-0.2.0.tar.gz

---
+++ packages/poetry-plugin-use-pip-global-index-url/poetry_plugin_use_pip_global_index_url-0.2.0.tar.gz/poetry_plugin_use_pip_global_index_url-0.2.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['poetry_plugin_use_pip_global_index_url']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['poetry>=1.3.0,<1.4.0']
+
+entry_points = \
+{'poetry.plugin': ['demo = '
+                   'poetry_plugin_use_pip_global_index_url.plugins:UsePipGlobalIndexUrlPlugin']}
+
+setup_kwargs = {
+    'name': 'poetry-plugin-use-pip-global-index-url',
+    'version': '0.2.0',
+    'description': 'Poetry plugin that sets the global index with the value obtained from pip.',
+    'long_description': 'None',
+    'author': 'Jacob Henner',
+    'author_email': 'code@ventricle.us',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
