d08092f6c5efbd906cf9fd6ea8e6f90e33f7d02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:49 +0000

    Add package ocp-project-plugin/0.0.0.0.3/ocp_project_plugin-0.0.0.0.3.tar.gz

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.3.tar.gz/ocp_project_plugin-0.0.0.0.3/ocp_project_plugin/__init__.py
@@ -0,0 +1,18 @@
+from extras.plugins import PluginConfig
+
+__version__ = "0.0.0.0.3"
+
+
+class OCPProjectConfig(PluginConfig):
+    name = "ocp_project_plugin"
+    verbose_name = "Netbox Storage"
+    description = "Netbox Storage"
+    min_version = "3.4.0"
+    version = __version__
+    author = "Tim Rhomberg"
+    author_email = "timrhomberg@hotmail.com"
+    required_settings = []
+    base_url = "ocp-project-plugin"
+
+
+config = OCPProjectConfig
