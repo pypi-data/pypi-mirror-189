65176b3f7053b9375fdde5c4a8640d1c1c5b58a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:22 +0000

    Add package ocp-project-plugin/0.0.0.0.51/ocp_project_plugin-0.0.0.0.51.tar.gz

---
+++ packages/ocp-project-plugin/ocp_project_plugin-0.0.0.0.51.tar.gz/ocp_project_plugin-0.0.0.0.51/ocp_project_plugin/utils/gitlab.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from git import Repo
+
+PLUGIN_SETTINGS = settings.PLUGINS_CONFIG.get('ocp_project_plugin', dict())
+GITLAB_PROJECT_URL = PLUGIN_SETTINGS.get('gitlab_project_url', '')
+VALUES_PATH = PLUGIN_SETTINGS.get('values_path', '')
+
+
+def clone_repo():
+    repo_instance = Repo.clone_from(
+        GITLAB_PROJECT_URL, '/repo/project_repo')
+
+
+def add_ocp_project():
+    print()
+
+
+class GitlabProjectManager:
+    def __init__(self, context):
+        super().__init__(context)
