56dfb2e9490b5cfe9494f6c2ac8f875a77275bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:24 +0000

    Add package wassup-my-g/0.0.1/wassup-my-g-0.0.1.tar.gz

---
+++ packages/wassup-my-g/wassup-my-g-0.0.1.tar.gz/wassup-my-g-0.0.1/__init__.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+import os 
+import codecs
+setup (
+    name="wassup-my-g",
+    version="0.0.1",
+    author="Shefki Berisha",
+    author_email="mastibeatz16@gmail.com",
+    description="The most useless package ever.",
+    long_description="Why would you use this? 💀",
+    pythonrequires=">=3.8.0"
+)
