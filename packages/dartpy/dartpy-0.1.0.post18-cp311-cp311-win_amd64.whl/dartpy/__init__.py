fb4e7d6f71927eaf3b6c683fba5398b2da08ca50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:14 +0000

    Add package dartpy/0.1.0.post18/dartpy-0.1.0.post18-cp311-cp311-win_amd64.whl

---
+++ packages/dartpy/dartpy-0.1.0.post18-cp311-cp311-win_amd64.whl/dartpy/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2011-2023, The DART development contributors
+# All rights reserved.
+#
+# The list of contributors can be found at:
+#   https://github.com/dartsim/dart/blob/main/LICENSE
+#
+# This file is provided under the "BSD-style" License
+
+import importlib
+
+math = importlib.import_module("dart_math")
