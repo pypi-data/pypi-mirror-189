62a9c06e9a968c6b17b109c6f506c4095e137a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:24 +0000

    Add package wfdanielpackagetest/0.2.0/wfdanielpackagetest-0.2.0.tar.gz

---
+++ packages/wfdanielpackagetest/wfdanielpackagetest-0.2.0.tar.gz/wfdanielpackagetest-0.2.0/wfdanielpackagetest/dani.py
@@ -0,0 +1,23 @@
+import logging
+import qatamagochi
+import qafunnypet
+
+class Dani():
+
+    def __init__(self, message=None):
+        self.message = message
+        print(f"Hi dumb {self.message}")
+
+    def use_loggin(self):
+        if self.message is None:
+            print("Youre logged foreing")
+        else:
+            logging.warning(f"Youre logged {self.message}")
+
+    def new_tamagochi(self, name, tamagochi_name):
+        tamagochi = qatamagochi.Qatamagochi(name, tamagochi_name)
+        tamagochi.speak()
+
+    def new_pet(self, name, age):
+        pet = qafunnypet.Qafunnypet(name, age)
+        pet.present()
