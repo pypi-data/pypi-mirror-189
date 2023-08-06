cd0a38cf4fd9e053844a28099f61adfbc9bb1797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:28 +0000

    Add package eotransform-xarray/1.11.0/eotransform_xarray-1.11.0-py3-none-any.whl

---
+++ packages/eotransform-xarray/eotransform_xarray-1.11.0-py3-none-any.whl/eotransform_xarray/transformers/__init__.py
@@ -0,0 +1,19 @@
+from abc import ABC
+from typing import Union, Tuple
+
+from eotransform.protocol.transformer import Transformer
+from xarray import DataArray, Dataset
+
+XArrayData = Union[DataArray, Dataset]
+
+
+class TransformerOfDataArray(Transformer[DataArray, DataArray], ABC):
+    ...
+
+
+class TransformerOfXArrayData(Transformer[XArrayData, XArrayData], ABC):
+    ...
+
+
+class AggregatorOfDataArrays(Transformer[Tuple[DataArray, ...], DataArray], ABC):
+    ...
