944497d1307a2a4e5feb2557f43bf580db6aa171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:55 +0000

    Add package cloudeventful/2.2.3/cloudeventful-2.2.3-py3-none-any.whl

---
+++ packages/cloudeventful/cloudeventful-2.2.3-py3-none-any.whl/cloudeventful/_cloud_event_doc.py
@@ -0,0 +1,11 @@
+"""Module providing data class to represent registered event models."""
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class CloudEventDoc(BaseModel):
+    """Document describing what data models are published where."""
+
+    api_version: str
+    data_models: dict[str, dict[str, Any]] = {}
