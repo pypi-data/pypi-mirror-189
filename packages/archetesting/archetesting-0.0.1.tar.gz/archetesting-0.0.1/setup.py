8c95f7f37d82b8acee6b68363dc019fd564a6fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:04 +0000

    Add package archetesting/0.0.1/archetesting-0.0.1.tar.gz

---
+++ packages/archetesting/archetesting-0.0.1.tar.gz/archetesting-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+'Development Status :: 5 - Production/Stable',
+'Intended Audience :: Education',
+'Operating System :: Microsoft :: Windows :: Windows 10',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3'
+
+
+]
+
+setup(
+
+name = 'archetesting',
+version='0.0.1',
+description='This is the testing library',
+long_description='This is the testing library this long enough for testing',
+url='',
+author='Arche',
+author_email='rkdyava@gmail.com',
+classifiers=classifiers,
+keywords='add',
+packages=find_packages(),
+install_requires=['']
+
+
+
+
+
+)
