849d3853dc6a8034a4de3248bc4af9c19957d1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:33 +0000

    Add package piplyr/0.0.5/piplyr-0.0.5.tar.gz

---
+++ packages/piplyr/piplyr-0.0.5.tar.gz/piplyr-0.0.5/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	# Here is the module name.
+	name="piplyr",
+
+	# version of the module
+	version="0.0.5",
+
+	# Name of Author
+	author="Seyed Pozveh",
+
+	# your Email address
+	author_email="s.pozveh@gmail.com",
+
+	# #Small Description about module
+	description="Pandas with dplyr style",
+
+	# long_description=long_description,
+
+	# Specifying that we are using markdown file for description
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+
+	# Any link to reach this module, ***if*** you have any webpage or github profile
+	# url="https://github.com/economer/",
+	packages=find_packages(),
+
+
+	# if module has dependencies i.e. if your package rely on other package at pypi.org
+	# then you must add there, in order to download every requirement of package
+
+
+
+		# install_requires=[
+		# "pandas",
+	    # "sqlite3",
+	 #],
+
+
+	license="MIT",
+
+	# classifiers like program is suitable for python3, just leave as it is.
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
