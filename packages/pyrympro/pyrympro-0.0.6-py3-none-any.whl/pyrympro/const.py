76fca4a616f141ecf84aad68f60c174814e8ead9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:47 +0000

    Add package pyrympro/0.0.6/pyrympro-0.0.6-py3-none-any.whl

---
+++ packages/pyrympro/pyrympro-0.0.6-py3-none-any.whl/pyrympro/const.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+BASE_URL_OLD = "https://api.city-mind.com"
+BASE_URL_NEW = "https://api-ctm.city-mind.com"
+
+CONSUMER_URL_OLD = f"{BASE_URL_OLD}/consumer"
+CONSUMER_URL_NEW = f"{BASE_URL_NEW}/consumer"
+
+CONSUMPTION_URL = f"{BASE_URL_NEW}/consumption"
+
+class Endpoint(Enum):
+  LOGIN = f"{CONSUMER_URL_OLD}/login"
+  ACCOUNT_INFO = f"{CONSUMER_URL_OLD}/me"
+  LAST_READ = f"{CONSUMPTION_URL}/last-read"
+  CONSUMPTION_FORECAST = f"{CONSUMPTION_URL}/forecast/{{meter_id}}"
+  DAILY_CONSUMPTION = f"{CONSUMPTION_URL}/daily/lastbillingCycle/{{meter_id}}/{{start_date}}/{{end_date}}"
