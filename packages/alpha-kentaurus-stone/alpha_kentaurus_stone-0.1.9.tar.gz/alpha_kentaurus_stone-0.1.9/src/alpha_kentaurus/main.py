ffd0268a83c65d4d572cc091810f2facdeacafc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:36 +0000

    Add package alpha-kentaurus-stone/0.1.9/alpha_kentaurus_stone-0.1.9.tar.gz

---
+++ packages/alpha-kentaurus-stone/alpha_kentaurus_stone-0.1.9.tar.gz/alpha_kentaurus_stone-0.1.9/src/alpha_kentaurus/main.py
@@ -0,0 +1,4 @@
+def main():
+    print("Ah ah ah, you didn't say the magic word - https://www.youtube.com/watch?v=RfiQYRn7fBg")
+    print("Seriously though, this isn't what you're looking for.")
+    print("Please make sure not pulling from the public pypi.")
