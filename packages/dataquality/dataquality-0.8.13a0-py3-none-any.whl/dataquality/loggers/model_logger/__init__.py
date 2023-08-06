a45e8a5249e818664dd1b7ccfbdcfe1a66ff41d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:44 +0000

    Add package dataquality/0.8.13a0/dataquality-0.8.13a0-py3-none-any.whl

---
+++ packages/dataquality/dataquality-0.8.13a0-py3-none-any.whl/dataquality/loggers/model_logger/__init__.py
@@ -0,0 +1,15 @@
+from dataquality.loggers.model_logger import (
+    image_classification,
+    text_classification,
+    text_multi_label,
+    text_ner,
+)
+from dataquality.loggers.model_logger.base_model_logger import BaseGalileoModelLogger
+
+__all__ = [
+    "image_classification",
+    "text_classification",
+    "text_multi_label",
+    "text_ner",
+    "BaseGalileoModelLogger",
+]
