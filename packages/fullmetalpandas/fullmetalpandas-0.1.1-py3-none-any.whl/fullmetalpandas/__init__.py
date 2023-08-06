608ec1b238b97ab4f6576aab67bd4c9088c32490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:52 +0000

    Add package fullmetalpandas/0.1.1/fullmetalpandas-0.1.1-py3-none-any.whl

---
+++ packages/fullmetalpandas/fullmetalpandas-0.1.1-py3-none-any.whl/fullmetalpandas/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.1'
+
+from fullmetalpandas.sqldataframe import SqlDataFrame, read_sql
+from fullmetalpandas.sqltable import SqlTable, read_sqltable, read_sqltable_dataframe
+from fullmetalpandas.records import insert_record, insert_records
