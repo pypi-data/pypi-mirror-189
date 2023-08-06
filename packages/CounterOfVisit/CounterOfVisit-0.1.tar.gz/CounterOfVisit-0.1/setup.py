9b194706a4cb0e6495ab7d025ab3b29cb24c9857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:49 +0000

    Add package CounterOfVisit/0.1/CounterOfVisit-0.1.tar.gz

---
+++ packages/CounterOfVisit/CounterOfVisit-0.1.tar.gz/CounterOfVisit-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CounterOfVisit',
+    version='0.1',
+    packages=find_packages(),
+    description='Module for counting visits',
+    author='Johan Polsinelli',
+    author_email='polsinelli@gmx.com',
+    license='MIT'
+)
