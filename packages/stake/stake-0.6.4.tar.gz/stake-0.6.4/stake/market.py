470f621df7706343c8658580e1d579d1cb4af222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:19 +0000

    Add package stake/0.6.4/stake-0.6.4.tar.gz

---
+++ packages/stake/stake-0.6.4.tar.gz/stake-0.6.4/stake/market.py
@@ -0,0 +1,40 @@
+"""Checks the market status."""
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+from stake.common import BaseClient, camelcase
+
+__all__ = ["MarketStatus"]
+
+
+class Status(BaseModel):
+    change_at: str = Field(alias="change_at")
+    next: str
+    current: str
+
+    class Config:
+        alias_generator = camelcase
+
+
+class MarketStatus(BaseModel):
+    message: str
+    unixtime: datetime
+    error: str
+    status: Status
+    elapsedtime: int
+    date: datetime
+    version_number: str
+
+    class Config:
+        alias_generator = camelcase
+
+
+class MarketClient(BaseClient):
+    async def get(self) -> MarketStatus:
+        data = await self._client.get(self._client.exchange.market_status)
+        return MarketStatus(**data["response"])
+
+    async def is_open(self) -> bool:
+        status = await self.get()
+        return status.status.current == "open"
