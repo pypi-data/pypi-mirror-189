93ae872b066da5c4f95a946e43d0c583dfb88073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:38 +0000

    Add package a-pandas-ex-fastloc/0.11/a_pandas_ex_fastloc-0.11.tar.gz

---
+++ packages/a-pandas-ex-fastloc/a_pandas_ex_fastloc-0.11.tar.gz/a_pandas_ex_fastloc-0.11/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.11'
+DESCRIPTION = "Up to 25 times faster than df.loc by combining np.select and numexpr.evaluate (works with utf-8)"
+
+# Setting up
+setup(
+    name="a_pandas_ex_fastloc",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/a_pandas_ex_fastloc',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['numexpr', 'numpy', 'pandas', 'regex', 'ujson'],
+    keywords=['numexpr', 'numpy', 'loc', 'iloc', 'pandas', 'series'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['numexpr', 'numpy', 'pandas', 'regex', 'ujson'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
