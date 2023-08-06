0c81b7e55e092c7e9f0a1facf335ab3473b05c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:12 +0000

    Add package dns.resolver/0.0.1/dns.resolver-0.0.1.tar.gz

---
+++ packages/dns.resolver/dns.resolver-0.0.1.tar.gz/dns.resolver-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+        name="dns.resolver",
+        version="0.0.1",
+        description="Security Research for Dangling Packages",
+        long_description="Security Research for Dangling Packages",
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages()
+)
