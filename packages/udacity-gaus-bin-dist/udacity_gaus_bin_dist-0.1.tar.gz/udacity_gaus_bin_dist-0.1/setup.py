769712115c442bc679813522dc6e49cc2df2d426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:12 +0000

    Add package udacity-gaus-bin-dist/0.1/udacity_gaus_bin_dist-0.1.tar.gz

---
+++ packages/udacity-gaus-bin-dist/udacity_gaus_bin_dist-0.1.tar.gz/udacity_gaus_bin_dist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='udacity_gaus_bin_dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Nils Mayat',
+      packages=['udacity_gaus_bin_dist'],
+      zip_safe=False)
