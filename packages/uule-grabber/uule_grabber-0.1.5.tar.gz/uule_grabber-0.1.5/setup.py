53b0a35ff1f5f40443753a3f7a50c46d644761c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:52 +0000

    Add package uule-grabber/0.1.5/uule_grabber-0.1.5.tar.gz

---
+++ packages/uule-grabber/uule_grabber-0.1.5.tar.gz/uule_grabber-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name="uule_grabber",
+    packages=["uule_grabber"],
+    version="0.1.5",
+    description="Generates UULE codes for Google Search",
+    long_description="Generates UULE codes for Google Search",
+    author="Kemal Ogun Isik",
+    author_email="ogunisik@gmail.com",
+    url="https://github.com/ogun/uule_grabber",
+    download_url="https://github.com/ogun/uule_grabber/archive/v0.1.5.tar.gz",
+    classifiers=[],
+)
