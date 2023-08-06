e79ddad5a82a2634a1c776640919cf23ebf12fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:08 +0000

    Add package mosaicml/0.12.1/mosaicml-0.12.1-py3-none-any.whl

---
+++ packages/mosaicml/mosaicml-0.12.1-py3-none-any.whl/composer/metrics/__init__.py
@@ -0,0 +1,31 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""A collection of common torchmetrics."""
+
+from composer.metrics.map import MAP
+from composer.metrics.metrics import CrossEntropy, Dice, LossMetric, MIoU
+from composer.metrics.nlp import (BinaryF1Score, HFCrossEntropy, InContextLearningLMAccuracy, InContextLearningMetric,
+                                  InContextLearningMultipleChoiceAccuracy, LanguageCrossEntropy, MaskedAccuracy,
+                                  Perplexity)
+
+__all__ = [
+    'MAP',
+    'MIoU',
+    'Dice',
+    'CrossEntropy',
+    'LossMetric',
+    'Perplexity',
+    'BinaryF1Score',
+    'HFCrossEntropy',
+    'LanguageCrossEntropy',
+    'MaskedAccuracy',
+    'InContextLearningLMAccuracy',
+    'InContextLearningMultipleChoiceAccuracy',
+    'InContextLearningMetric',
+]
+
+METRIC_DEFAULT_CTORS = {
+    'InContextLearningLMAccuracy': InContextLearningLMAccuracy,
+    'InContextLearningMultipleChoiceAccuracy': InContextLearningMultipleChoiceAccuracy
+}
