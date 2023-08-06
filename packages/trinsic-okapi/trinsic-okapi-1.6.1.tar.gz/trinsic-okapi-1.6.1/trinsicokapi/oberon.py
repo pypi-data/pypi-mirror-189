a86e3f6629992da3a28d6d7ab2d5a9d9c11c076f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:52 +0000

    Add package trinsic-okapi/1.6.1/trinsic-okapi-1.6.1.tar.gz

---
+++ packages/trinsic-okapi/trinsic-okapi-1.6.1.tar.gz/trinsic-okapi-1.6.1/trinsicokapi/oberon.py
@@ -0,0 +1,55 @@
+from trinsicokapi.proto.okapi.security.v1 import (
+    CreateOberonKeyRequest,
+    CreateOberonKeyResponse,
+    CreateOberonProofRequest,
+    CreateOberonProofResponse,
+    CreateOberonTokenRequest,
+    CreateOberonTokenResponse,
+    BlindOberonTokenResponse,
+    BlindOberonTokenRequest,
+    UnBlindOberonTokenRequest,
+    UnBlindOberonTokenResponse,
+    VerifyOberonProofResponse,
+    VerifyOberonProofRequest,
+    VerifyOberonTokenRequest,
+    VerifyOberonTokenResponse,
+)
+from trinsicokapi.wrapper import _typed_wrap_and_call
+
+
+def create_key(request: CreateOberonKeyRequest) -> CreateOberonKeyResponse:
+    return _typed_wrap_and_call("oberon_create_key", request, CreateOberonKeyResponse)
+
+
+def create_proof(request: CreateOberonProofRequest) -> CreateOberonProofResponse:
+    return _typed_wrap_and_call(
+        "oberon_create_proof", request, CreateOberonProofResponse
+    )
+
+
+def create_token(request: CreateOberonTokenRequest) -> CreateOberonTokenResponse:
+    return _typed_wrap_and_call(
+        "oberon_create_token", request, CreateOberonTokenResponse
+    )
+
+
+def blind_token(request: BlindOberonTokenRequest) -> BlindOberonTokenResponse:
+    return _typed_wrap_and_call("oberon_blind_token", request, BlindOberonTokenResponse)
+
+
+def verify_token(request: VerifyOberonTokenRequest) -> VerifyOberonTokenResponse:
+    return _typed_wrap_and_call(
+        "oberon_verify_token", request, VerifyOberonTokenResponse
+    )
+
+
+def unblind_token(request: UnBlindOberonTokenRequest) -> UnBlindOberonTokenResponse:
+    return _typed_wrap_and_call(
+        "oberon_unblind_token", request, UnBlindOberonTokenResponse
+    )
+
+
+def verify_proof(request: VerifyOberonProofRequest) -> VerifyOberonProofResponse:
+    return _typed_wrap_and_call(
+        "oberon_verify_proof", request, VerifyOberonProofResponse
+    )
