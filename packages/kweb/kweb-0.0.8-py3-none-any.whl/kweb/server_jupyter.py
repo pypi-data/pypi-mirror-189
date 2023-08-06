a306f8581db0b75ac01d06c91c72699b0b48dc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:45 +0000

    Add package kweb/0.0.8/kweb-0.0.8-py3-none-any.whl

---
+++ packages/kweb/kweb-0.0.8-py3-none-any.whl/kweb/server_jupyter.py
@@ -0,0 +1,24 @@
+import asyncio
+
+import uvicorn
+
+from kweb.main import app
+
+jupyter_server = None
+
+
+def _run() -> None:
+    config = uvicorn.Config(app)
+    jupyter_server = uvicorn.Server(config)
+    loop = asyncio.get_event_loop()
+    loop.create_task(jupyter_server.serve())
+
+
+def _server_is_running() -> bool:
+    return False if jupyter_server is None else jupyter_server.started
+
+
+def start() -> None:
+    """Start a jupyter_server if it's nor already started."""
+    if not _server_is_running():
+        _run()
