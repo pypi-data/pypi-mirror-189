41e9a8de7a92b140372ad900e6345f242b30f56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:39 +0000

    Add package smithed/0.8.0/smithed-0.8.0.tar.gz

---
+++ packages/smithed/smithed-0.8.0.tar.gz/smithed-0.8.0/smithed/weld/__init__.py
@@ -0,0 +1,9 @@
+from .main import print_pack_name, run_weld
+from .merge_policies import beet_default, setup
+
+__all__ = [
+    "beet_default",
+    "print_pack_name",
+    "run_weld",
+    "setup",
+]
