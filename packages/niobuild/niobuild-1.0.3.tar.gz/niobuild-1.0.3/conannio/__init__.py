ec5b57e01189d09477629cd90ce9b1c0556b4ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:00 +0000

    Add package niobuild/1.0.3/niobuild-1.0.3.tar.gz

---
+++ packages/niobuild/niobuild-1.0.3.tar.gz/niobuild-1.0.3/conannio/__init__.py
@@ -0,0 +1,7 @@
+from .simulate import Simulate, parse_args
+try:
+    from .nioconanfile import NioConanFile
+except:
+    pass
+
+Version = "v1.0.3"
---
+++ packages/niobuild/niobuild-1.0.3.tar.gz/niobuild-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from conannio import Version
+
+setup(
+    name="niobuild",
+    version=Version,
+    # install_requires=["conan==1.58.0"],
+    entry_points = {
+        'console_scripts': [
+            'niobuild = conannio.build:main'
+        ]
+    }
+)
