a6e963119717751669d50c2e587431830938bc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:06 +0000

    Add package screenpy-requests/4.0.2/screenpy_requests-4.0.2-py3-none-any.whl

---
+++ packages/screenpy-requests/screenpy_requests-4.0.2-py3-none-any.whl/screenpy_requests/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# █▀ █▀▀ █▀█ █▀▀ █▀▀ █▄░█ █▀█ █▄█   █▀█ █▀▀ █▀█ █░█ █▀▀ █▀ ▀█▀ █▀
+# ▄█ █▄▄ █▀▄ ██▄ ██▄ █░▀█ █▀▀ ░█░   █▀▄ ██▄ ▀▀█ █▄█ ██▄ ▄█ ░█░ ▄█
+
+"""
+                                ScreenPy Requests
+                                                                      FADE IN:
+INT. SITEPACKAGES DIRECTORY
+
+ScreenPy Requests is an extension for ScreenPy, enabling interaction with
+Requests.
+
+:copyright: (c) 2022–2023 by Perry Goy.
+:license: MIT, see LICENSE for more details.
+"""
+
+from .abilities import *  # noqa: import all for ease-of-use
+from .actions import *  # noqa: import all for ease-of-use
+from .questions import *  # noqa: import all for ease-of-use
