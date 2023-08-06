ef1b007ec38b840cfcd13897b8f66b7f88ca1b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:28 +0000

    Add package nautobot-chatops-ipfabric/3.0.1/nautobot_chatops_ipfabric-3.0.1-py3-none-any.whl

---
+++ packages/nautobot-chatops-ipfabric/nautobot_chatops_ipfabric-3.0.1-py3-none-any.whl/nautobot_chatops_ipfabric/__init__.py
@@ -0,0 +1,29 @@
+"""Plugin declaration for ipfabric."""
+try:
+    from importlib import metadata
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
+
+from nautobot.extras.plugins import PluginConfig
+
+
+class IPFabricConfig(PluginConfig):
+    """Plugin configuration for the ipfabric plugin."""
+
+    name = "nautobot_chatops_ipfabric"
+    verbose_name = "Nautobot ChatOps IPFabric"
+    version = __version__
+    author = "Network to Code, LLC"
+    description = "Nautobot Chatops IPFabric"
+    base_url = "nautobot-chatops-ipfabric"
+    required_settings = []
+    min_version = "1.0.0"
+    max_version = "1.9999"
+    default_settings = {}
+    caching_config = {}
+
+
+config = IPFabricConfig  # pylint:disable=invalid-name
