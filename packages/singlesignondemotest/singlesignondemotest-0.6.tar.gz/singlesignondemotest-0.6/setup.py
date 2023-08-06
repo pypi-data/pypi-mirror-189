8ce42a23172bdd0d158b4fef673ba13a2f6c75cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:22 +0000

    Add package singlesignondemotest/0.6/singlesignondemotest-0.6.tar.gz

---
+++ packages/singlesignondemotest/singlesignondemotest-0.6.tar.gz/singlesignondemotest-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='singlesignondemotest',
+    version='0.6',
+    author="lehonghieu1412159",
+    author_email='lehonghieu1412159@gmail.com',
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+    url='https://github.com/lehonghieu1512/single-sign-on-demo',
+    keywords='demo project',
+    install_requires=[
+          'django',
+          'oauthlib',
+          'pyOpenSSL',
+          'requests'
+      ],
+
+)
