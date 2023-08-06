e98183157c8ee867142490387851bbbfa7330265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:14 +0000

    Add package aiotieba/3.1.4/aiotieba-3.1.4-cp39-cp39-win_amd64.whl

---
+++ packages/aiotieba/aiotieba-3.1.4-cp39-cp39-win_amd64.whl/aiotieba/client/_classdef/__init__.py
@@ -0,0 +1,28 @@
+from .._core import TbCore
+from .._helper import ForumInfoCache, WebsocketResponse
+from .common import TypeMessage
+from .container import Containers
+from .contents import (
+    FragAt,
+    FragEmoji,
+    FragImage,
+    FragItem,
+    FragLink,
+    FragmentUnknown,
+    FragText,
+    FragTiebaPlus,
+    TypeFragAt,
+    TypeFragEmoji,
+    TypeFragImage,
+    TypeFragItem,
+    TypeFragLink,
+    TypeFragment,
+    TypeFragmentUnknown,
+    TypeFragText,
+    TypeFragTiebaPlus,
+)
+from .enums import Header, ReqUInfo
+from .forum import Forum
+from .user import UserInfo
+from .vimage import VirtualImage
+from .vote import VoteInfo
