c1c3bbc1f8fcb75b0401563939a29a95e49f6ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:10 +0000

    Add package wiffi/1.1.2/wiffi-1.1.2.tar.gz

---
+++ packages/wiffi/wiffi-1.1.2.tar.gz/wiffi-1.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wiffi",
+    version="1.1.2",
+    author="mampfes",
+    author_email="mampfes@users.noreply.github.com",
+    description="Python 3 package to interface devices from STALL WIFFI.",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mampfes/python-wiffi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
