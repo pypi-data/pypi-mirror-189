cbfff927e091d7c7baac15353be17db15594a4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:29 +0000

    Add package hive-scrutineer/1.3.18/hive_scrutineer-1.3.18-py3-none-any.whl

---
+++ packages/hive-scrutineer/hive_scrutineer-1.3.18-py3-none-any.whl/scrutineer/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+"""
+Hive Posts Scutineer
+~~~~~~~~~~~~~~~~
+
+Performance and quality analytics on Hive Posts.
+
+"""
+
+__version__ = "1.3.18"
+from .scrutineer import Scrutineer
+from .scrutineer import get_keywords
+from .scrutineer import get_bigrams
+
+
+__all__ = ["scrutineer"]
