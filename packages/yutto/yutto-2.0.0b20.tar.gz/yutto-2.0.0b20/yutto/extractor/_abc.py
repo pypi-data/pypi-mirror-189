88dc5a2f2b8184e7a83c6d70a2abbb74c1a555ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:29 +0000

    Add package yutto/2.0.0b20/yutto-2.0.0b20.tar.gz

---
+++ packages/yutto/yutto-2.0.0b20.tar.gz/yutto-2.0.0b20/yutto/extractor/_abc.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import argparse
+from abc import ABCMeta, abstractmethod
+from typing import Any, Coroutine, Optional, TypeVar
+
+import aiohttp
+
+from yutto._typing import EpisodeData
+
+T = TypeVar("T")
+
+
+class Extractor(metaclass=ABCMeta):
+    def resolve_shortcut(self, id: str) -> tuple[bool, str]:
+        matched = False
+        url = id
+        return (matched, url)
+
+    @abstractmethod
+    def match(self, url: str) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def __call__(
+        self, session: aiohttp.ClientSession, args: argparse.Namespace
+    ) -> list[Optional[Coroutine[Any, Any, Optional[EpisodeData]]]]:
+        raise NotImplementedError
+
+
+class SingleExtractor(Extractor):
+    async def __call__(
+        self, session: aiohttp.ClientSession, args: argparse.Namespace
+    ) -> list[Optional[Coroutine[Any, Any, Optional[EpisodeData]]]]:
+        return [await self.extract(session, args)]
+
+    @abstractmethod
+    async def extract(
+        self, session: aiohttp.ClientSession, args: argparse.Namespace
+    ) -> Optional[Coroutine[Any, Any, Optional[EpisodeData]]]:
+        raise NotImplementedError
+
+
+class BatchExtractor(Extractor):
+    async def __call__(
+        self, session: aiohttp.ClientSession, args: argparse.Namespace
+    ) -> list[Optional[Coroutine[Any, Any, Optional[EpisodeData]]]]:
+        return await self.extract(session, args)
+
+    @abstractmethod
+    async def extract(
+        self, session: aiohttp.ClientSession, args: argparse.Namespace
+    ) -> list[Optional[Coroutine[Any, Any, Optional[EpisodeData]]]]:
+        raise NotImplementedError
