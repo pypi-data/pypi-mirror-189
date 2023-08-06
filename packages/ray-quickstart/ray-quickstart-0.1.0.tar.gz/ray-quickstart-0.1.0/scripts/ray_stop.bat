97850f4d08c76e395da594e2944872be2a0c4ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:58 +0000

    Add package ray-quickstart/0.1.0/ray-quickstart-0.1.0.tar.gz

---
+++ packages/ray-quickstart/ray-quickstart-0.1.0.tar.gz/ray-quickstart-0.1.0/scripts/ray_stop.bat
@@ -0,0 +1,5 @@
+echo off
+
+cd C:\dev\git\ai
+REM "C:\Users\tuyen\.virtualenvs\ai-Gt1AetLj\Scripts\ray.exe" down config\ray_cluster.yaml -y
+C:\Users\tuyen\.virtualenvs\ai-Gt1AetLj\Scripts\ray.exe stop
