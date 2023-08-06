1e4cc30ab790cd962bd2fed92cac95c7e956ec07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:58 +0000

    Add package coldshard-cli/0.1.0/coldshard-cli-0.1.0.tar.gz

---
+++ packages/coldshard-cli/coldshard-cli-0.1.0.tar.gz/coldshard-cli-0.1.0/README.md
@@ -0,0 +1,5 @@
+# ColdShard CLI
+This is the official CLI for ColdShard. This allows you to do whatever it is you would've done on our panel, *but in the command line* 🤯🎉!
+
+## Installation
+As of now, the CLI is not on PyPi.
---
+++ packages/coldshard-cli/coldshard-cli-0.1.0.tar.gz/coldshard-cli-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='coldshard-cli',
+    version='0.1.0',
+    packages=["cli", "cli.utils"],
+    install_requires=[
+        'Click',
+        "Requests",
+        "Inquirer"
+    ],
+    entry_points={
+        'console_scripts': [
+            'coldshard = cli.main:core',
+        ],
+    },
+)
